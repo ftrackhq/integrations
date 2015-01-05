5e2d89a99680800f485acb62daf3ce2bc85dce07
Martin Pengelly-Phillips <martin@ftrack.com>
2015-01-05 09:06:35 +0000

    Remove unnecessary global markers and clean up code to follow styleguide.

--- source/ftrack_connect_nuke_studio/__init__.py
+++ source/ftrack_connect_nuke_studio/__init__.py
@@ -2,13 +2,17 @@
 # :copyright: Copyright (c) 2014 ftrack
 
 import os
+
 from ftrack import Registry
+
 from ._version import __version__
 
-PROCESSOR_PLUGINS = Registry(os.environ.get('FTRACK_PROCESSOR_PLUGIN_PATH', '').split(os.pathsep))
+
+PROCESSOR_PLUGINS = Registry(
+    os.environ.get('FTRACK_PROCESSOR_PLUGIN_PATH', '').split(os.pathsep)
+)
+
 
 def setup():
-    global PROCESSOR_PLUGINS
+    '''Setup package.'''
     PROCESSOR_PLUGINS.discover()
-
-global PROCESSOR_PLUGINS
