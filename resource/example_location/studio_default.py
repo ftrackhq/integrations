c967a0b0537ef2a4618000a840ff171d115cfe7d
Martin Pengelly-Phillips <martin@ftrack.com>
2015-01-05 14:25:29 +0000

    Clean up default location and add a sensible temporary directory.

--- resource/example_location/studio_default.py
+++ resource/example_location/studio_default.py
@@ -1,22 +1,34 @@
+# :coding: utf-8
+# :copyright: Copyright (c) 2014 ftrack
+
 import os
+import tempfile
+
 import ftrack
 
 
-location_name = 'studio_default'
-project_location = os.getenv('PROJECT_ROOT')
-
-
 def register(registry, **kw):
+    '''Register location with *registry*.'''
+    location_name = 'studio_default'
 
-	ftrack.ensureLocation(location_name)
-	structure = ftrack.ClassicStructure()
-	accessor = ftrack.DiskAccessor(prefix=os.path.expanduser(project_location))
+    ftrack.ensureLocation(location_name)
+    structure = ftrack.ClassicStructure()
+    accessor = ftrack.DiskAccessor(
+        prefix=os.path.expanduser(
+            os.getenv(
+                'PROJECT_ROOT',
+                os.path.join(
+                    tempfile.gettempdir(), 'ftrack_connect_nuke_studio_example'
+                )
+            )
+        )
+    )
 
-	location = ftrack.Location(
-		location_name,
-		accessor=accessor,
-		structure=structure,
-		priority=1
-	    )
+    location = ftrack.Location(
+        location_name,
+        accessor=accessor,
+        structure=structure,
+        priority=1
+        )
 
-	registry.add(location)
+    registry.add(location)
