314d7aa7c2804977792f361c8a7e96c10c9b9080
Carl Claesson <carl.claesson@ftrack.com>
2015-01-27 17:30:07 +0100

    Refactored tag_drop_handler to cohere with style guide.

--- source/ftrack_connect_nuke_studio/ui/tag_drop_handler.py
+++ source/ftrack_connect_nuke_studio/ui/tag_drop_handler.py
@@ -2,6 +2,7 @@
 # :copyright: Copyright (c) 2014 ftrack
 
 import re
+
 import hiero
 from hiero.core.events import *
 import FnAssetAPI.logging
@@ -12,18 +13,24 @@ class TagDropHandler(object):
     kTextMimeType = "text/plain"
 
     def __init__(self):
-        ''' Initialize the class and register the handler.
-        '''
-        # nuke studio doesn't deal with drag and drop for text/plain data, so tell it to allow it
-        hiero.ui.registerBinViewCustomMimeDataType(TagDropHandler.kTextMimeType)
-        # register interest in the drop event now
-        registerInterest((EventType.kDrop, EventType.kTimeline), self.dropHandler)
+        ''' Initialize the class and register the handler.'''
+        # Nuke studio doesn't deal with drag and drop for text/plain data, so
+        # tell it to allow it
+        hiero.ui.registerBinViewCustomMimeDataType(
+            TagDropHandler.kTextMimeType
+        )
+
+        # Register interest drop event.
+        registerInterest(
+            (EventType.kDrop, EventType.kTimeline), self.dropHandler
+        )
 
     def dropHandler(self, event):
-        ''' Intercept the drop action on the timeline.
+        ''' Intercept the drop *event* on the timeline.
 
-        Filter out any non ftrack tag, and uses the tag.re field to extract the context from the clip name,
-        and set it back to the applied tag.
+        Filter out any non ftrack tag, and uses the tag.re field to extract
+        the context from the clip name, and set it back to the applied tag.
+
         '''
         currentTags = event.items
 
@@ -33,31 +40,44 @@ class TagDropHandler(object):
 
             # Filter out any non ftrack tag
             if not meta.hasKey('type') or meta.value('type') != 'ftrack':
-                FnAssetAPI.logging.debug('%s is not a valid track tag type' % tag_name)
+                FnAssetAPI.logging.debug(
+                    '{0} is not a valid track tag type'.format(tag_name)
+                )
                 continue
 
             clip_name = event.trackItem.name()
             if tag_name == 'project':
-                # print 'setting project to something'
                 project = event.trackItem.project().name()
-                FnAssetAPI.logging.debug('setting %s to %s on %s' % (tag_name, project, clip_name))
+                FnAssetAPI.logging.debug(
+                    'Setting {0} to {1} on {2}'.format(
+                        tag_name, project, clip_name
+                    )
+                )
                 meta.setValue('tag.value', project)
 
-            # here we got a tag with a regular expression
+            # Handle a tag with a regular expression.
             if meta.hasKey('tag.re'):
                 match = meta.value('tag.re')
                 if not match:
-                    # somehow the tag.re is empty
+                    # If the regular expression is empty skip it.
                     continue
 
                 result = re.match(match, clip_name)
                 if result:
                     result_value = result.groups()[-1]
-                    FnAssetAPI.logging.debug('setting %s to %s on %s' % (tag_name, result_value, clip_name))
+                    FnAssetAPI.logging.debug(
+                        'Setting {0} to {1} on {2}'.format(
+                            tag_name, result_value, clip_name
+                        )
+                    )
                     meta.setValue('tag.value', result_value)
 
     def unregister(self):
-        ''' Unregister the handler.
-        '''
-        unregisterInterest((EventType.kDrop, EventType.kTimeline), self.dropHandler)
-        hiero.ui.unregisterBinViewCustomMimeDataType(TagDropHandler.kTextMimeType)
+        ''' Unregister the handler.'''
+        unregisterInterest(
+            (EventType.kDrop, EventType.kTimeline), self.dropHandler
+        )
+
+        hiero.ui.unregisterBinViewCustomMimeDataType(
+            TagDropHandler.kTextMimeType
+        )
