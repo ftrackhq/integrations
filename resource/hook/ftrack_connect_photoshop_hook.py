78e8e8f90305755c7d5bbc206e2df60858cf610a
Carl Claesson <carl.claesson@ftrack.com>
2015-02-03 15:30:58 +0100

    Add dummy hook to ensure that old hooks are removed when upgrading connect

---
+++ resource/hook/ftrack_connect_photoshop_hook.py
@@ -0,0 +1,8 @@
+# :coding: utf-8
+# :copyright: Copyright (c) 2015 ftrack
+
+# This file is here to ensure the windows installer removes 
+# an old hook with the same file name when updating the package.
+#
+# It is empty on purpose.
+#
