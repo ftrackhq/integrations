86948673eb6f3c261724d07fec44e21b586336f8
Carl Claesson <carl.claesson@ftrack.com>
2015-02-10 12:48:33 +0100

    Added launch hook and updated setup to include data_files.

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@
 
 import os
 import re
+import glob
 
 from setuptools import setup, find_packages
 
 ROOT_PATH = os.path.dirname(os.path.realpath(__file__))
 SOURCE_PATH = os.path.join(ROOT_PATH, 'source')
 README_PATH = os.path.join(ROOT_PATH, 'README.rst')
-
+RESOURCE_PATH = os.path.join(ROOT_PATH, 'resource')
 
 # Read version from source.
 with open(os.path.join(
@@ -17,6 +18,52 @@ with open(os.path.join(
     VERSION = re.match(
         r'.*__version__ = \'(.*?)\'', _version_file.read(), re.DOTALL
     ).group(1)
+
+connect_foundry_dependency_link = (
+    'file://{0}#egg=ftrack-connect-foundry-0.1.0'
+    .format(os.environ['FTRACK_CONNECT_FOUNDRY'].replace('\\', '/'))
+)
+
+
+def get_files_from_folder(folder):
+    '''Get all files in a folder in resource folder.'''
+    plugin_directory = os.path.join(RESOURCE_PATH, folder)
+    plugin_data_files = []
+
+    for root, directories, files in os.walk(plugin_directory):
+        files_list = []
+        if files:
+            for filename in files:
+                files_list.append(
+                    os.path.join(root, filename)
+                )
+
+        if files_list:
+            destination_folder = root.replace(
+                RESOURCE_PATH, 'ftrack_connect_nuke_studio/resource'
+            )
+            plugin_data_files.append(
+                (destination_folder, files_list)
+            )
+
+    return plugin_data_files
+
+data_files = []
+
+for child in os.listdir(
+    RESOURCE_PATH
+):
+    if os.path.isdir(os.path.join(RESOURCE_PATH, child)) and child != 'hook':
+        data_files += get_files_from_folder(child)
+
+data_files += get_files_from_folder(RESOURCE_PATH)
+
+data_files.append(
+    (
+        'ftrack_connect_nuke_studio/hook',
+        glob.glob(os.path.join(RESOURCE_PATH, 'hook', '*.py'))
+    )
+)
 
 
 # Call main setup.
@@ -43,7 +90,11 @@ setup(
         'ftrack-connect >= 0.1.2, < 2',
         'ftrack-connect-foundry >= 0.1.0, < 2'
     ],
+    dependency_links=[
+        connect_foundry_dependency_link
+    ],
     tests_require=[
     ],
-    zip_safe=False
+    zip_safe=False,
+    data_files=data_files
 )
