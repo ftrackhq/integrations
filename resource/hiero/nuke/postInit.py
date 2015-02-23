2a3725d9adc26b6a7d38d258a85a06fca5f7ea12
langeli <lorenzo.angeli@efestolab.uk>
2015-02-23 20:15:08 +0100

    replace import

--- resource/hiero/nuke/postInit.py
+++ resource/hiero/nuke/postInit.py
@@ -1,5 +1,5 @@
 # This initializes the FnAssetAPI host
-import foundry.assetmgr
+import nuke.assetmgr
 import ftrack_connect_nuke_studio.plugin
 # Select a locally available manager
 nuke.assetmgr.start(ftrack_connect_nuke_studio.plugin.Plugin.getIdentifier())
