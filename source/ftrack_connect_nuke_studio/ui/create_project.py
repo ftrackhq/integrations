897268c0c9c3a4860e693808b52e1a683538675f
Mattias Lagergren <mattias.lagergren@ftrack.com>
2016-03-11 16:22:10 +0100

    Add more commits to avoid issue where order of operations is wrong in backend

--- source/ftrack_connect_nuke_studio/ui/create_project.py
+++ source/ftrack_connect_nuke_studio/ui/create_project.py
@@ -751,6 +751,11 @@ class ProjectTreeDialog(QtGui.QDialog):
                 for key, value in metadata.items():
                     current['metadata'][key] = value
 
+                self.logger.debug('Commit project.')
+                #: TODO: Remove this commit when the api issue with order of 
+                # operations is fixed.
+                self.session.commit()
+
             else:
 
                 # Gather all the useful informations from the track
@@ -848,6 +853,12 @@ class ProjectTreeDialog(QtGui.QDialog):
                     self.update_entity_reference.emit(
                         datum.track, datum.exists['taskid'], 'task'
                     )
+
+
+                self.logger.debug('Commit partial structure.')
+                #: TODO: Remove this commit when the api issue with order of 
+                # operations is fixed.
+                self.session.commit()
 
                 processors = gather_processors(datum.name, datum.type)
 
