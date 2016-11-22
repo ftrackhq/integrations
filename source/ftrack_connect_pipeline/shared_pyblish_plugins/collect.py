b813a9ca1280a895c1437f0be75d2f4f80df841f
Mattias Lagergren <mattias.lagergren@ftrack.com>
2016-11-22 12:41:20 +0100

    Move plugins

---
+++ source/ftrack_connect_pipeline/shared_pyblish_plugins/collect.py
@@ -0,0 +1,21 @@
+# :coding: utf-8
+# :copyright: Copyright (c) 2016 ftrack
+
+import logging
+
+import pyblish.api
+import ftrack_connect_pipeline.util
+
+logger = logging.getLogger(__file__)
+
+
+class FtrackPublishCollector(pyblish.api.ContextPlugin):
+    '''Prepare ftrack publish.'''
+
+    order = pyblish.api.CollectorOrder
+
+    def process(self, context):
+        '''Process *context* and add ftrack entity.'''
+        ftrack_entity = ftrack_connect_pipeline.util.get_ftrack_entity()
+        context.data['ftrack_entity'] = ftrack_entity
+        logger.debug('Collected ftrack entity {0}.'.format(ftrack_entity))
