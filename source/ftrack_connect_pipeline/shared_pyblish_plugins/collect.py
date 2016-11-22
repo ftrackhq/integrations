442b7a6a074fdd37e7cb71dd248e73cd06ebd242
Mattias Lagergren <mattias.lagergren@ftrack.com>
2016-11-22 13:04:06 +0100

    Change collector to use self.log instead off logger

--- source/ftrack_connect_pipeline/shared_pyblish_plugins/collect.py
+++ source/ftrack_connect_pipeline/shared_pyblish_plugins/collect.py
@@ -1,12 +1,7 @@
 # :coding: utf-8
 # :copyright: Copyright (c) 2016 ftrack
 
-import logging
-
 import pyblish.api
-import ftrack_connect_pipeline.util
-
-logger = logging.getLogger(__file__)
 
 
 class FtrackPublishCollector(pyblish.api.ContextPlugin):
@@ -16,8 +11,10 @@ class FtrackPublishCollector(pyblish.api.ContextPlugin):
 
     def process(self, context):
         '''Process *context* and add ftrack entity.'''
+        import ftrack_connect_pipeline.util
         ftrack_entity = ftrack_connect_pipeline.util.get_ftrack_entity()
         context.data['ftrack_entity'] = ftrack_entity
-        logger.debug('Collected ftrack entity {0}.'.format(ftrack_entity))
+        self.log.debug('Collected ftrack entity {0}.'.format(ftrack_entity))
+
 
 pyblish.api.register_plugin(FtrackPublishCollector)
