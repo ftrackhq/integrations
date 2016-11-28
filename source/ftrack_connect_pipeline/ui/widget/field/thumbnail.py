07f0109d4ad31ee9d5c3095acc535b668974f11a
lorenzo angeli <lorenzo.angeli@efestolab.uk>
2016-11-28 17:45:42 +0100

    rework thumbnail field to avoid multiple inheritance

--- source/ftrack_connect_pipeline/ui/widget/field/thumbnail.py
+++ source/ftrack_connect_pipeline/ui/widget/field/thumbnail.py
@@ -5,13 +5,20 @@
 from ftrack_connect_pipeline.ui.widget.field.base import BaseField
 from ftrack_connect_pipeline.ui.widget import thumbnail_drop_zone
 
+from QtExt import QtWidgets
 
-class ThumbnailField(BaseField, thumbnail_drop_zone.ThumbnailDropZone):
+
+class ThumbnailField(BaseField):
 
     def __init__(self):
         '''Initialize widget'''
         super(ThumbnailField, self).__init__()
-        self.updated.connect(self.notify_changed)
+        layout = QtWidgets.QHBoxLayout()
+        layout.setContentsMargins(0, 0, 0, 0)
+        self.setLayout(layout)
+        self.thumbnail = thumbnail_drop_zone.ThumbnailDropZone()
+        self.layout().addWidget(self.thumbnail)
+        self.thumbnail.updated.connect(self.notify_changed)
 
     def notify_changed(self, *args, **kwargs):
         '''Notify the world about the changes.'''
@@ -19,4 +26,4 @@ class ThumbnailField(BaseField, thumbnail_drop_zone.ThumbnailDropZone):
 
     def value(self):
         '''Return value.'''
-        return self.getFilePath()
+        return self.thumbnail.getFilePath()
