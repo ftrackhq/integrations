fcecafffe5e4f9702ff35dd6450b9974bf461f26
Mattias Lagergren <mattias.lagergren@ftrack.com>
2016-12-05 17:26:40 +0100

    Update register functions and test

--- source/ftrack_connect_pipeline/shared_pyblish_plugins/__init__.py
+++ source/ftrack_connect_pipeline/shared_pyblish_plugins/__init__.py
@@ -5,5 +5,4 @@
 def register():
     '''Register all shared pyblish plugins.'''
     # Importing the submodules will cause plugins to register.
-    import ftrack_connect_pipeline.shared_pyblish_plugins.collect
     import ftrack_connect_pipeline.shared_pyblish_plugins.integrate
