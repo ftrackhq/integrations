77411b15165a3e52f30188738aa1e3a5fdefdc51
lorenzo angeli <lorenzo.angeli@efestolab.uk>
2017-01-13 12:40:45 +0100

    fix reference for apply baseTheme under PySide2

--- source/ftrack_connect_pipeline/ui/theme.py
+++ source/ftrack_connect_pipeline/ui/theme.py
@@ -12,8 +12,8 @@ def applyFont(font=':/ftrack/font/main'):
 def applyTheme(widget, theme='light', baseTheme=None):
     '''Apply *theme* to *widget*.'''
     # Set base style.
-    if baseTheme and QtGui.QApplication.style().objectName() != baseTheme:
-        QtGui.QApplication.setStyle(baseTheme)
+    if baseTheme and QtWidgets.QApplication.style().objectName() != baseTheme:
+        QtWidgets.QApplication.setStyle(baseTheme)
 
     # Load stylesheet from resource file and apply.
     fileObject = QtCore.QFile(':/ftrack/style/{0}'.format(theme))
