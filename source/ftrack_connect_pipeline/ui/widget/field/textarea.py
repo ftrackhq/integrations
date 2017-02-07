aae3fc389c4e8a449d592c7cbf2b11fef80a4df0
Mattias Lagergren <mattias.lagergren@ftrack.com>
2017-02-07 09:58:01 +0100

    Bump copyright year

--- source/ftrack_connect_pipeline/ui/widget/field/textarea.py
+++ source/ftrack_connect_pipeline/ui/widget/field/textarea.py
@@ -1,5 +1,5 @@
 # :coding: utf-8
-# :copyright: Copyright (c) 2016 ftrack
+# :copyright: Copyright (c) 2017 ftrack
 
 from ftrack_connect_pipeline.ui.widget.field.base import BaseField
 from ftrack_connect_pipeline.ui.widget import textarea
