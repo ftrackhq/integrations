97fc79e617f1e4ec072b869c31619ecc45746edf
Lorenzo Angeli <lorenzo.angeli@ftrack.com>
2018-06-12 17:28:05 +0200

    remove dead coed

--- source/ftrack_connect_nuke_studio_beta/plugin.py
+++ source/ftrack_connect_nuke_studio_beta/plugin.py
@@ -3,10 +3,9 @@
 
 from __future__ import absolute_import
 
-import functools
 import logging
 
-from QtExt import QtGui, QtWidgets, is_webwidget_supported
+from QtExt import QtGui, QtWidgets
 import hiero.ui
 import hiero.core
 
@@ -15,41 +14,18 @@ import ftrack_connect.event_hub_thread
 
 logger = logging.getLogger(__name__)
 
-
-import ftrack_connect_nuke_studio_beta.ui.widget.info_view
 from ftrack_connect_nuke_studio_beta.processors import register_processors
-
 
 ftrack_connect.ui.theme.applyFont()
 
 
 def populate_ftrack(event):
     '''Populate the ftrack menu with items.'''
-    parent = hiero.ui.mainWindow()
     menu_bar = hiero.ui.menuBar()
 
     ftrack_menu = menu_bar.addMenu(
         QtGui.QPixmap(':ftrack/image/default/ftrackLogoColor'), 'ftrack'
     )
-
-    window_manager = hiero.ui.windowManager()
-
-    if is_webwidget_supported():
-        information_view = ftrack_connect_nuke_studio_beta.ui.widget.info_view.InfoView(
-            parent=parent
-        )
-        window_manager.addWindow(information_view)
-
-        information_view_action = QtWidgets.QAction(
-            ftrack_connect_nuke_studio_beta.ui.widget.info_view.InfoView.get_display_name(),
-            ftrack_menu
-        )
-
-        information_view_action.triggered.connect(
-            functools.partial(window_manager.showWindow, information_view)
-        )
-
-        ftrack_menu.addAction(information_view_action)
 
 
 # Trigger population of the ftrack menu.
@@ -58,4 +34,5 @@ hiero.core.events.registerInterest(
     'kStartup', populate_ftrack
 )
 
+# TODO: move to events!
 register_processors()
