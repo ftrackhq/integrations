e99630106d9ba686181cd1148a8f7d445137beb8
Lorenzo Angeli <lorenzo.angeli@ftrack.com>
2018-07-09 13:10:46 +0200

    fetch components

--- source/ftrack_connect_nuke_studio_beta/actions/ftrack.py
+++ source/ftrack_connect_nuke_studio_beta/actions/ftrack.py
@@ -6,6 +6,7 @@ from QtExt import QtWidgets, QtGui, QtCore
 from ftrack_connect_nuke_studio_beta.base import FtrackBase
 
 import hiero
+
 from hiero.ui.BuildExternalMediaTrack import (
     BuildTrack,
     BuildTrackFromExportTagAction,
@@ -15,6 +16,8 @@ from hiero.ui.BuildExternalMediaTrack import (
     TrackFinderByNameWithDialog
 )
 
+registry = hiero.core.taskRegistry
+
 
 class FtracBuildServerTrackDialog(QtWidgets.QDialog, FtrackBase):
     task_changed = QtCore.Signal()
@@ -22,53 +25,6 @@ class FtracBuildServerTrackDialog(QtWidgets.QDialog, FtrackBase):
     @staticmethod
     def common_items(items):
         return set.intersection(*map(set, items))
-
-    def itemProject(self, item):
-        if hasattr(item, 'project'):
-            return item.project()
-        elif hasattr(item, 'parent'):
-            return self.itemProject(item.parent())
-        else:
-            return None
-
-    def __init__(self, selection, parent=None):
-        if not parent:
-            parent = hiero.ui.mainWindow()
-        super(FtracBuildServerTrackDialog, self).__init__(parent)
-
-        self.logger = logging.getLogger(
-            __name__ + '.' + self.__class__.__name__
-        )
-        self.logger.setLevel(logging.DEBUG)
-
-        self._selection = selection
-
-        if self._selection:
-            self.project = self.itemProject(self._selection[0])
-
-        self.setWindowTitle("Build Track From server tasks")
-        self.setSizeGripEnabled(True)
-
-        layout = QtWidgets.QVBoxLayout()
-        formLayout = QtWidgets.QFormLayout()
-        self._tracknameField = QtWidgets.QLineEdit(BuildTrack.ProjectTrackNameDefault(selection))
-        self._tracknameField.setToolTip("Name of new track")
-
-        self.tasks_combobox = QtWidgets.QComboBox()
-        formLayout.addRow("Task:", self.tasks_combobox)
-
-        self.asset_type_combobox = QtWidgets.QComboBox()
-        formLayout.addRow("Asset Type:", self.asset_type_combobox)
-
-        self.compomnent_combobox = QtWidgets.QComboBox()
-        formLayout.addRow("Component :", self.compomnent_combobox)
-
-        layout.addLayout(formLayout)
-        self.setLayout(layout)
-
-        # populate data
-        self.populate_tasks()
-        self.populate_asset_types()
 
     @property
     def parsed_selection(self):
@@ -82,6 +38,137 @@ class FtracBuildServerTrackDialog(QtWidgets.QDialog, FtrackBase):
         self.logger.info(results)
 
         return results
+
+    def itemProject(self, item):
+        if hasattr(item, 'project'):
+            return item.project()
+        elif hasattr(item, 'parent'):
+            return self.itemProject(item.parent())
+        else:
+            return None
+
+    def __init__(self, selection, parent=None):
+        if not parent:
+            parent = hiero.ui.mainWindow()
+        super(FtracBuildServerTrackDialog, self).__init__(parent)
+        self.logger = logging.getLogger(
+            __name__ + '.' + self.__class__.__name__
+        )
+        self.logger.setLevel(logging.DEBUG)
+
+        num_tasks = registry.numTasks()
+        self._ftrack_tasks = [registry.taskName(index) for index in range(num_tasks)]
+
+        self.logger.info(self._ftrack_tasks)
+        self._selection = selection
+
+        if self._selection:
+            self.project = self.itemProject(self._selection[0])
+
+        self.setWindowTitle("Build Track From server tasks")
+        self.setSizeGripEnabled(True)
+
+        layout = QtWidgets.QVBoxLayout()
+        formLayout = QtWidgets.QFormLayout()
+        self._tracknameField = QtWidgets.QLineEdit(BuildTrack.ProjectTrackNameDefault(selection))
+        self._tracknameField.setToolTip("Name of new track")
+        formLayout.addRow("Track Name:", self._tracknameField)
+
+        self.tasks_combobox = QtWidgets.QComboBox()
+        self.tasks_combobox.currentIndexChanged.connect(self.get_components)
+        formLayout.addRow("Task:", self.tasks_combobox)
+
+        self.asset_type_combobox = QtWidgets.QComboBox()
+        self.asset_type_combobox.currentIndexChanged.connect(self.get_components)
+
+        formLayout.addRow("Asset Type:", self.asset_type_combobox)
+
+        self.component_combobox = QtWidgets.QComboBox()
+        self.component_combobox.currentIndexChanged.connect(self.get_components)
+        formLayout.addRow("Component :", self.component_combobox)
+
+        layout.addLayout(formLayout)
+        self.setLayout(layout)
+
+        # build button
+
+        self.build_button = QtWidgets.QPushButton('Build')
+        self.build_button.clicked.connect(self.build_tracks)
+        formLayout.addRow("Build Track", self.build_button)
+
+        # populate data
+        self.populate_tasks()
+        self.populate_asset_types()
+        self.populate_components()
+
+    def build_tracks(self):
+        components = self.get_components()
+        self.logger.info('LOCATION: {}'.format(self.ftrack_location['name']))
+        for component in components:
+            if self.ftrack_location not in component['component_locations']:
+                continue
+
+            self.logger.info(component['name'])
+            path = self.ftrack_location.get_filesystem_path(component)
+            self.logger.info(path)
+
+    def get_components(self, index=None):
+        all_components = []
+        task = self.tasks_combobox.currentText()
+        asset_type = self.asset_type_combobox.currentText()
+        component = self.component_combobox.currentText()
+
+        if not all([task, asset_type, component]):
+            self.build_button.setDisabled(not bool(all_components))
+            return all_components
+
+        for (project, sequence, shot) in self.parsed_selection:
+
+            query = (
+                'Component where name is {} '  # component name 
+                'and version.asset.type.name is "{}" '  # asset type
+                'and version.task.name is "{}" '  # task name
+                'and version.asset.parent.name is "{}" '  # shot
+                'and version.asset.parent.parent.name is "{}" '  # sequence
+                'and version.asset.parent.project.name is "{}"'  # project
+                ''.format(component, asset_type, task, shot, sequence, project)
+            )
+            self.logger.info('query: {0}'.format(query))
+
+            final_component = self.session.query(query
+            ).first()
+
+            if not final_component:
+                continue
+
+            all_components.append(final_component)
+
+        # if no result is found, disable builds
+        self.build_button.setDisabled(not bool(all_components))
+        self.logger.info(all_components)
+        return all_components
+
+
+    def populate_components(self):
+        all_component_names = []
+        for (project, sequence, shot) in self.parsed_selection:
+            components = self.session.query(
+                'select name, version.asset.parent.name, version.asset.parent.parent.name from Component '
+                'where version.asset.parent.name is "{}" and version.asset.parent.parent.name is "{}" and version.asset.parent.project.name is "{}"'.format(
+                    shot, sequence, project
+                )
+            ).all()
+
+            if not components:
+                continue
+
+            # WE SHOULD FILTER HERE BASED ON REGISTERED FTRACK TASKS.
+            all_component_names.append([component['name'] for component in components])
+
+        common_components = self.common_items(all_component_names)
+        self.logger.info('component_combobox : {}'.format(common_components))
+        for name in sorted(common_components):
+            self.component_combobox.addItem(name)
 
     def populate_asset_types(self):
         all_asset_types_names = []
