34e2bfba6f075dadf3c068e1de8f267457c0c642
Lorenzo Angeli <lorenzo.angeli@ftrack.com>
2018-07-17 17:19:45 +0200

    code cleanup better queries

--- source/ftrack_connect_nuke_studio_beta/overrides/version_scanner.py
+++ source/ftrack_connect_nuke_studio_beta/overrides/version_scanner.py
@@ -16,28 +16,19 @@ def register_versioning_overrides():
 
   # We want to redefine some of the methods of VersionScanner, but may still need to use the default functionality within our custom methods.
   # Therefore, we need to save a reference to the default methods, and then we will be able to call them from ours.
-  if not hasattr(VersionScanner, "_default_findVersionFiles"):
+  if not hasattr(VersionScanner, '_default_findVersionFiles'):
     VersionScanner._default_findVersionFiles = VersionScanner.findVersionFiles
     VersionScanner.findVersionFiles = ftrack_find_version_files
 
-  VersionScanner._entityVersions = []
+  if not hasattr(VersionScanner, '_default_insertClips'):
+    VersionScanner._default_insertClips = VersionScanner.insertClips
+    VersionScanner.insertClips = ftrack_insert_clips
+
+  VersionScanner._ftrack_component_reference = []
 
 
-def ftrack_insert_versions(self, binitem, versionFiles):
-  newClips = []
-  for newFilename in versionFiles:
-    newClip = self.createClip(newFilename)
-
-    if newClip is not None:
-      newClips.append(newClip)
-
-  return self.insertClips(binitem, newClips)
-
-
-def ftrack_find_version_files(scannerInstance, version):
-  logger.debug('ftrack_find_version_files: {}'.format(str(version)))
-  clip = version.item()
-  location = session.pick_location()
+def get_ftrack_tag(clip):
+  # tags are stored in the source clip
   existingTag = None
 
   for tag in clip.tags():
@@ -45,26 +36,63 @@ def ftrack_find_version_files(scannerInstance, version):
       existingTag = tag
       break
 
-  if not existingTag:
-    return []
+  logger.info('Checking tags  : {} - {}'.format(clip, existingTag))
 
-  component_id = existingTag.metadata()['component_id']
+  if not existingTag:
+      return False
+
+  return existingTag
+
+
+def ftrack_insert_clips(scannerInstance, binItem, clips):
+  entities = []
+  nonEntities = []
+
+  newVersions = []
+
+  for c in clips:
+    has_tags = get_ftrack_tag(c)
+    if not has_tags:
+      nonEntities.append(c)
+
+  scannerInstance._ftrack_component_reference = []
+
+  newVersions.extend(scannerInstance._default_insertClips(binItem, nonEntities))
+  return newVersions
+
+
+def ftrack_find_version_files(scannerInstance, version):
+  clip = version.item()
+  ftrack_tag = get_ftrack_tag(clip)
+  logger.debug('ftrack_find_version_files: {}: {}'.format(version, ftrack_tag))
+
+  if not ftrack_tag:
+    return VersionScanner._default_findVersionFiles(scannerInstance, version)
+
+  location = session.pick_location()
+  component_id = ftrack_tag.metadata()['component_id']
   component = session.get('Component', component_id)
   component_name = component['name']
-  versions = component['version']['asset']['versions']
-  components = []
-  for version in versions:
-    logger.info('VERSION: {}'.format(version['version']))
+  asset = component['version']['asset']
+  logger.info(asset)
+  paths = []
 
-    components = session.query(
-      'Component where name is "{0}" and version.id is "{0}"'.format(
-        component_name, version['id']
-      )
-    ).all()
-    for component in components:
-      component_avaialble = session.pick_location(component)
+  unsorted_compomnents = session.query(
+    'select name, version.version from Component where version.asset.id is {} and name is {}'.format(
+      asset['id'], component_name)
+  ).all()
+  sorted_components = sorted(unsorted_compomnents, key=lambda k: int(k['version']['version']))
 
-      if component_avaialble:
-        components.append(component)
+  logger.info(sorted_components)
 
-  return components
+  paths = []
+  for component in sorted_components:
+    component_avaialble = session.pick_location(component)
+
+    if component_avaialble:
+      logger.info('VERSION: {} component: {}'.format(component['version']['version'], component))
+
+      path = location.get_filesystem_path(component)
+      paths.append(path)
+
+  return paths
