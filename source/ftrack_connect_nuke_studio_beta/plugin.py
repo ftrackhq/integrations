fcd3b426d72103ad00d963a2b37cacbd672f339c
Lorenzo Angeli <lorenzo.angeli@ftrack.com>
2018-08-28 17:08:39 +0200

    disable implicit use of FnAssetAPI register ReadNode override and set ftrack tab example

--- source/ftrack_connect_nuke_studio_beta/plugin.py
+++ source/ftrack_connect_nuke_studio_beta/plugin.py
@@ -14,6 +14,7 @@ from ftrack_connect_nuke_studio_beta.actions.build_track import FtrackBuildTrack
 from ftrack_connect_nuke_studio_beta.tags.tag_drop_handler import TagDropHandler
 from ftrack_connect_nuke_studio_beta.tags.tag_manager import TagManager
 from ftrack_connect_nuke_studio_beta.overrides.version_scanner import register_versioning_overrides
+from ftrack_connect_nuke_studio_beta.overrides.nodes import register_nodes_overrides
 from ftrack_connect_nuke_studio_beta.processors import register_processors
 import ftrack_connect_nuke_studio_beta.resource
 
@@ -41,4 +42,5 @@ if (not hiero.core.isHieroPlayer()) and isinstance(QtCore.QCoreApplication.insta
 
     register_processors()
     register_versioning_overrides()
+    register_nodes_overrides()
 
