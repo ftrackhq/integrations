682d5be5af17526db05cc0730290c4ef1c02d80a
Lorenzo Angeli <lorenzo.angeli@ftrack.com>
2018-10-09 14:10:34 +0200

    update run.sh for docker build

--- docker/run.sh
+++ docker/run.sh
@@ -6,14 +6,13 @@ git clone --branch ${FTRACK_LEGACY_PYTHON_API_VERSION} https://bitbucket.org/ftr
 git clone --branch ${FTRACK_CONNECT_PACKAGE_VERSION} https://bitbucket.org/ftrack/ftrack-connect-package.git ${BUILD_DIR};
 
 # Build connect package.
-echo Building Connect Package Version: ${FTRACK_CONNECT_PACKAGE_VERSION} 
+echo Building Connect Package Version: ${FTRACK_CONNECT_PACKAGE_VERSION} in /${OUT_FOLDER}/ftrack-connect-package-${FTRACK_CONNECT_PACKAGE_VERSION}.tar.gz
+
 cd ${BUILD_DIR} && python2.7 setup.py build
 
 # Package result code.
 cd ${BUILD_DIR}/build/ && tar -zcvf /${OUT_FOLDER}/ftrack-connect-package-${FTRACK_CONNECT_PACKAGE_VERSION}.tar.gz exe.linux-x86_64-2.7 --transform 's/exe.linux-x86_64-2.7/ftrack-connect-package/' 
-
-echo Build available: /${OUT_FOLDER}/ftrack-connect-package-${FTRACK_CONNECT_PACKAGE_VERSION}.tar.gz
-
+echo 
 if [ -v $UPLOAD_BUILD ]; then
     # Install awscli for amazon upload.
     yum install -y libyaml-devel
@@ -25,6 +24,6 @@ if [ -v $UPLOAD_BUILD ]; then
         /${OUT_FOLDER}/ftrack-connect-package-${FTRACK_CONNECT_PACKAGE_VERSION}.tar.gz \
         s3://ftrack-deployment/ftrack-connect/ftrack-connect-package-${FTRACK_CONNECT_PACKAGE_VERSION}.tar.gz
 else
-    echo 'BUILD UPLOAD DISABLED, please run with :  -v `pwd`/volume:/build , to extract the built files.'
+    echo 'BUILD UPLOAD DISABLED, please run: docker cp <result_docker_image>:/build/ftrack-connect-package-master.tar.gz .
 fi
 
