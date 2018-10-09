f398400fb7f0c88a89c53a76e8c2a7ceaf705984
Lorenzo Angeli <lorenzo.angeli@ftrack.com>
2018-10-09 12:12:42 +0200

    update run.sh to expose built dir fix variable lookup

--- docker/run.sh
+++ docker/run.sh
@@ -6,17 +6,20 @@ git clone --branch ${FTRACK_LEGACY_PYTHON_API_VERSION} https://bitbucket.org/ftr
 git clone --branch ${FTRACK_CONNECT_PACKAGE_VERSION} https://bitbucket.org/ftrack/ftrack-connect-package.git ${BUILD_DIR};
 
 # Build connect package.
-echo Building Connect Package Version: ${FTRACK_CONNECT_PACKAGE_VERSION}
+echo Building Connect Package Version: ${FTRACK_CONNECT_PACKAGE_VERSION} 
 cd ${BUILD_DIR} && python2.7 setup.py build
 
 # Package result code.
 cd ${BUILD_DIR}/build/ && tar -zcvf /${OUT_FOLDER}/ftrack-connect-package-${FTRACK_CONNECT_PACKAGE_VERSION}.tar.gz exe.linux-x86_64-2.7 --transform 's/exe.linux-x86_64-2.7/ftrack-connect-package/' 
 
-if [ -v UPLOAD_BUILD ]; then
+echo Build available: /${OUT_FOLDER}/ftrack-connect-package-${FTRACK_CONNECT_PACKAGE_VERSION}.tar.gz
+
+if [ -v $UPLOAD_BUILD ]; then
     # Install awscli for amazon upload.
     yum install -y libyaml-devel
     python2.7 -m pip install awscli
-
+    
+    echo "RUN "
     # Copy result file to amazon storage.
     aws s3 cp \
         /${OUT_FOLDER}/ftrack-connect-package-${FTRACK_CONNECT_PACKAGE_VERSION}.tar.gz \
