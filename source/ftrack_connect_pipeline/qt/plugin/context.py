92be60bf49b2c6888bf95c7b3e3c2a48eb61ef19
Lorenzo Angeli <lorenzo.angeli@ftrack.com>
2019-01-16 15:48:41 +0100

    start rewire load context

--- source/ftrack_connect_pipeline/qt/plugin/context.py
+++ source/ftrack_connect_pipeline/qt/plugin/context.py
@@ -5,7 +5,16 @@ from ftrack_connect.ui.widget import entity_selector
 from ftrack_connect.ui.widget import asset_options
 from ftrack_connect.ui.widget import context_selector
 from ftrack_connect.ui.widget import list_assets_table
+from ftrack_connect.ui.widget import component_table
 
+# dummy wrap to make old class work
+class ConnectorWrapper(object):
+
+    def __init__(self, session):
+        self.session = session
+
+    def getConnectorName(self):
+        return "foobar"
 
 
 class PublishContextWidget(SimpleWidget):
@@ -53,30 +62,43 @@ class PublishContextWidget(SimpleWidget):
         return result
 
 
-
 class LoadContextWidget(SimpleWidget):
 
     def __init__(self, parent=None, session=None, data=None, name=None, description=None, options=None, plugin_topic=None):
+        self._connector_wrapper = ConnectorWrapper(session)
         super(LoadContextWidget, self).__init__(parent=parent, session=session, data=data, name=name, description=description, options=options, plugin_topic=plugin_topic)
+        self.widget_options = {}
 
-        self.browseTasksWidget = None
-        self.listAssetsTableWidget  = None
-
-    def _build_context_id_selector(self, value):
+    def build(self):
+        self.listAssetsTableWidget = list_assets_table.ListAssetsTableWidget(self)
         self.browseTasksWidget = context_selector.ContextSelector(
             currentEntity=None, parent=self
         )
-        self.layout().addWidget(self.browseTasksWidget, stretch=0)
-        self.widget_options['context_id'] = self.browseTasksWidget
+        self.componentTableWidget = component_table.ComponentTableWidget(self, self._connector_wrapper)
+        self.browseTasksWidget.entityChanged.connect(self.clickedIdSignal)
+        self.listAssetsTableWidget.assetVersionSelectedSignal[str].connect(
+            self.clickedAssetVSignal
+        )
 
-    def _build_asset_selector(self, value):
-        self.listAssetsTableWidget = list_assets_table.ListAssetsTableWidget(self)
+        super(LoadContextWidget, self).build()
+
+        self.layout().addWidget(self.browseTasksWidget, stretch=0)
         self.layout().addWidget(self.listAssetsTableWidget, stretch=4)
-        self.widget_options['asset_list'] = self.listAssetsTableWidget
+        self.layout().addWidget(self.componentTableWidget, stretch=4)
+
+    def clickedIdSignal(self, ftrackId):
+        '''Handle click signal.'''
+        self.listAssetsTableWidget.initView(ftrackId)
+
+    def clickedAssetVSignal(self, assetVid):
+        '''Set asset version to *assetVid*.'''
+        self.componentTableWidget.setAssetVersion(assetVid)
+
+    def _build_component_selector(self, value):
+        self.widget_options['component_list'] = self.componentTableWidget
 
     def build_options(self, options):
-        self._build_context_id_selector(options['context_id'])
-        self._build_asset_selector(options['asset_list'])
+        self._build_component_selector(options['component_list'])
 
     def extract_options(self):
         result = {}
