abc8b8f85404a09e8b5b7263de7074de8354780f
Lorenzo Angeli <lorenzo.angeli@ftrack.com>
2019-01-22 16:06:35 +0100

    temporary disable components and disable interactive

--- source/ftrack_connect_pipeline/qt/load.py
+++ source/ftrack_connect_pipeline/qt/load.py
@@ -14,11 +14,10 @@ class QtPipelineLoaderWidget(BaseQtPipelineWidget):
         super(QtPipelineLoaderWidget, self).__init__(parent=None)
         self.setWindowTitle('Standalone Pipeline Loader')
         self.stage_type = constants.LOAD
-        self._iteractive = True
+        self._iteractive = False
 
         self.mapping = OrderedDict([
             (constants.CONTEXT,    (constants.CONTEXT_PLUGIN_TOPIC, self._on_run_context)),
-            (constants.COMPONENTS, (constants.COMPONENTS_PLUGIN_TOPIC, self._on_run_components)),
             (constants.IMPORTERS,  (constants.IMPORTERS_PLUGIN_TOPIC, self._on_run_importers))
         ])
 
@@ -38,9 +37,8 @@ class QtPipelineLoaderWidget(BaseQtPipelineWidget):
 
         self.run_async(event_list)
 
-    def _on_run_importers(self, widgets):
+    def _on_run_components(self, widgets):
         context_data = self.merge_dict(self._stages_results[constants.CONTEXT])
-
         self.logger.debug('context data:{}'.format(context_data))
 
         event_list = []
@@ -48,6 +46,25 @@ class QtPipelineLoaderWidget(BaseQtPipelineWidget):
             options = widget.extract_options()
             topic = widget.plugin_topic
             options.update(context_data)
+
+            event_list.append(
+                {
+                    'topic': topic,
+                    'options': options,
+                    'type': constants.COMPONENTS
+                }
+            )
+
+        self.run_async(event_list)
+
+    def _on_run_importers(self, widgets):
+        component_data = self.merge_dict(self._stages_results[constants.COMPONENTS])
+
+        event_list = []
+        for widget in widgets:
+            options = widget.extract_options()
+            topic = widget.plugin_topic
+            options.update(component_data)
 
             event_list.append(
                 {
