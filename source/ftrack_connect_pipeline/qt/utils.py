0634dd20035f1cd3e3eb4f24a6248d95d9d4681d
Lorenzo Angeli <lorenzo.angeli@ftrack.com>
2019-01-23 15:54:38 +0100

    code cleanup

--- source/ftrack_connect_pipeline/qt/utils.py
+++ source/ftrack_connect_pipeline/qt/utils.py
@@ -8,7 +8,7 @@ class NewApiEventHubThread(QtCore.QThread):
     '''Listen for events from ftrack's event hub.'''
 
     def start(self, session):
-        '''Start thread for *session*.'''
+        '''Start thread for *_session*.'''
         self._session = session
         super(NewApiEventHubThread, self).start()
 
@@ -29,17 +29,18 @@ class StageManager(QtCore.QObject):
             __name__ + '.' + self.__class__.__name__
         )
 
+        self._current_stage = None
         self._stage_type = stage_type
         self._stages_results = {}
-        self.session = session
-        self.mapping = stages_mapping
+        self._session = session
+        self._stages_mapping = stages_mapping
         self._widget_stack = widgets
 
         self.stage_done.connect(self._on_stage_done)
         self.stage_start.connect(self._on_stage_start)
 
     # event handling
-    def on_handle_async_reply(self, event):
+    def _on_handle_async_reply(self, event):
         event_data = event['data']
         event_task_name = event_data.keys()[0]
         event_task_value = event_data.values()[0]
@@ -59,17 +60,17 @@ class StageManager(QtCore.QObject):
             'Sending event list {} to host'.format(event_list)
         )
 
-        self.session.event_hub.publish(
+        self._session.event_hub.publish(
             ftrack_api.event.base.Event(
                 topic=constants.PIPELINE_RUN_TOPIC,
                 data={'event_list': event_list}
             ),
-            on_reply=self.on_handle_async_reply
+            on_reply=self._on_handle_async_reply
         )
 
     def _on_stage_start(self, ):
         self.logger.debug('Starting stage: {}'.format(self.current_stage))
-        fn = self.mapping[self.current_stage][1]
+        fn = self.stages[self.current_stage][1]
         widgets = self._widget_stack[self.current_stage]
         fn(widgets)
 
@@ -83,10 +84,12 @@ class StageManager(QtCore.QObject):
         self.stage_start.emit()
 
     def process_stages(self):
-        for stage in self.mapping.keys():
-            self._stages_results.setdefault(stage, [])
-
+        self._reset_results()
         self.stage_start.emit()
+
+    def _reset_results(self):
+        for stage in self.stages.keys():
+            self.results.setdefault(stage, [])
 
     def reset_stages(self):
         self._current_stage = None
@@ -101,12 +104,12 @@ class StageManager(QtCore.QObject):
 
     @property
     def stages(self):
-        return self.mapping
+        return self._stages_mapping
 
     @property
     def previous_stage(self):
         self.logger.info('current_stage :{}'.format(self.current_stage))
-        current_stage_idx = self.mapping.keys().index(self.current_stage)
+        current_stage_idx = self.stages.keys().index(self.current_stage)
 
         previous_stage_idx = current_stage_idx - 1
 
@@ -114,33 +117,33 @@ class StageManager(QtCore.QObject):
             # we reached the end, no more steps to perform !
             return
 
-        previous_stage = self.mapping.keys()[previous_stage_idx]
+        previous_stage = self.stages.keys()[previous_stage_idx]
         self.logger.info('previous_stage :{}'.format(previous_stage))
         return previous_stage
 
     @property
     def next_stage(self):
         self.logger.info('current_stage :{}'.format(self.current_stage))
-        current_stage_idx = self.mapping.keys().index(self.current_stage)
+        current_stage_idx = self.stages.keys().index(self.current_stage)
 
         next_stage_idx = current_stage_idx + 1
 
-        if next_stage_idx >= len(self.mapping.keys()):
+        if next_stage_idx >= len(self.stages.keys()):
             # we reached the end, no more steps to perform !
             return
 
-        next_stage = self.mapping.keys()[next_stage_idx]
+        next_stage = self.stages.keys()[next_stage_idx]
         self.logger.info('next_stage :{}'.format(next_stage))
         return next_stage
 
     @property
     def current_stage(self):
-        return self._current_stage or self.mapping.keys()[0]
+        return self._current_stage or self.stages.keys()[0]
 
     @current_stage.setter
     def current_stage(self, stage):
-        if stage not in self.mapping.keys():
-            self.logger.warning('Stage {} not in {}'.format(stage, self.mapping.keys()))
+        if stage not in self.stages.keys():
+            self.logger.warning('Stage {} not in {}'.format(stage, self.stages.keys()))
             return
 
         self._current_stage = stage
