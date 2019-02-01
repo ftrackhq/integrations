0524002b1623c4340bfb5f1772760af93a8a0a7b
Lorenzo Angeli <lorenzo.angeli@ftrack.com>
2019-02-01 16:13:26 +0100

    put usage in use.

--- resource/maya_plugin/userSetup.py
+++ resource/maya_plugin/userSetup.py
@@ -6,12 +6,21 @@ import logging
 import threading
 
 from ftrack_connect_pipeline import host
-
+from ftrack_connect_pipeline import usage
 import ftrack_connect_pipeline_maya #  import to configure logging
 
 import maya.cmds as mc
 
 logger = logging.getLogger('userSetup')
+
+
+def load_and_int():
+    # TODO : later we need to bring back here all the maya initialiations from ftrack-connect-maya
+    # such as frame start / end etc....
+
+    usage.send_event(
+        'USED-FTRACK-CONNECT-PIPELINE-MAYA'
+    )
 
 
 def register_hub_host():
@@ -22,3 +31,4 @@ def register_hub_host():
 
 
 mc.evalDeferred("register_hub_host()")
+mc.evalDeferred("load_and_init()")
