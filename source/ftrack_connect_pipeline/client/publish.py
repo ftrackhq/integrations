6545ff3908df7afba36a7e5582ae33eb72b5558e
Lorenzo Angeli <lorenzo.angeli@ftrack.com>
2019-03-15 13:37:51 +0100

    group plugins

--- source/ftrack_connect_pipeline/client/publish.py
+++ source/ftrack_connect_pipeline/client/publish.py
@@ -34,19 +34,42 @@ class QtPipelinePublishWidget(BaseQtPipelineWidget):
 
     def build(self):
         self.context_layout = QtWidgets.QVBoxLayout()
-        components_layout = QtWidgets.QVBoxLayout()
+        self.components_layout = QtWidgets.QVBoxLayout()
         self.publisher_layout = QtWidgets.QVBoxLayout()
 
         self.task_layout.addLayout(self.context_layout)
-        self.task_layout.addLayout(components_layout)
+        self.task_layout.addLayout(self.components_layout)
         self.task_layout.addLayout(self.publisher_layout)
 
         self.components_widget = QtWidgets.QTabWidget()
-        components_layout.addWidget(self.components_widget)
+        self.components_layout.addWidget(self.components_widget)
+
+        self.publisher_layout = QtWidgets.QVBoxLayout()
+        self.task_layout.addLayout(self.publisher_layout)
 
     def post_build(self):
         super(QtPipelinePublishWidget, self).post_build()
         self.combo.currentIndexChanged.connect(self._on_publisher_changed)
+
+    def _build_publish_widget(self, publish_plugins):
+        publish_group_widget = QtWidgets.QGroupBox('publish')
+        publish_layout = QtWidgets.QVBoxLayout()
+        publish_group_widget.setLayout(publish_layout)
+        for publish_plugin in publish_plugins:
+            publish_widget = self.fetch_widget(publish_plugin, 'publish')
+            publish_layout.addWidget(publish_widget)
+
+        return publish_group_widget
+
+    def _build_context_widget(self, context_plugins):
+        context_group_widget = QtWidgets.QGroupBox('context')
+        context_layout = QtWidgets.QVBoxLayout()
+        context_group_widget.setLayout(context_layout)
+        for context_plugin in context_plugins:
+            context_widget = self.fetch_widget(context_plugin, 'context')
+            context_layout.addWidget(context_widget)
+
+        return context_group_widget
 
     def _build_stages_widget(self, component_stages):
         component_widget = QtWidgets.QWidget()
@@ -79,46 +102,17 @@ class QtPipelinePublishWidget(BaseQtPipelineWidget):
 
         contexts = package_publisher['context']
         components = package_publisher['components']
+        publishers = package_publisher['publish']
 
-        for context in contexts:
-            context_plugin = context
-            context_widget = self.fetch_widget(context_plugin, 'context')
-            self.context_layout.addWidget(context_widget)
+        context_widget = self._build_context_widget(contexts)
+        self.context_layout.addWidget(context_widget)
 
         for component_name, component_stages in components.items():
             stages_widget = self._build_stages_widget(component_stages)
             self.components_widget.addTab(stages_widget, component_name)
 
-
-        # self._current_asset_type = package['type']
-        #
-        # stages = asset_schema[self.stages_manager.type]['plugins']
-        # for stage in stages:
-        #     for current_stage, current_plugins in stage.items():
-        #         base_topic = self.stages_manager.stages.get(current_stage)
-        #         if not base_topic:
-        #             self.logger.warning('stage {} cannot be evaluated'.format(current_stage))
-        #             continue
-        #
-        #         box = QtWidgets.QGroupBox(current_stage)
-        #         plugin_layout = QtWidgets.QVBoxLayout()
-        #         box.setLayout(plugin_layout)
-        #
-        #         self.stages_manager.widgets.setdefault(current_stage, [])
-        #
-        #         for plugin in current_plugins:
-        #             widget = self.fetch_widget(plugin, current_stage)
-        #             if widget:
-        #                 widget_is_visible = plugin.get('visible', True)
-        #                 if not widget_is_visible:
-        #                     widget.setVisible(False)
-        #
-        #                 plugin_layout.addWidget(widget)
-        #                 self.stages_manager.widgets[current_stage].append(
-        #                     (widget, plugin)
-        #                 )
-        #
-        #         self.task_layout.addWidget(box)
+        publish_widget = self._build_publish_widget(publishers)
+        self.publisher_layout.addWidget(publish_widget)
 
 
 if __name__ == '__main__':
