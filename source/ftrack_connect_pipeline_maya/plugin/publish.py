78e3f49d401e7daadaf244c7467956f7d7262388
Lorenzo Angeli <lorenzo.angeli@ftrack.com>
2019-03-21 17:36:44 +0100

    constants cleanup

--- source/ftrack_connect_pipeline_maya/plugin/publish.py
+++ source/ftrack_connect_pipeline_maya/plugin/publish.py
@@ -7,33 +7,33 @@ from ftrack_connect_pipeline_maya.plugin import BaseMayaPlugin, BaseMayaWidget
 
 # PLUGINS
 class CollectorMayaPlugin(BaseMayaPlugin):
-    plugin_type = constants.COLLECTORS
+    plugin_type = constants.COLLECT
 
 
 class ValidatorMayaPlugin(BaseMayaPlugin):
-    plugin_type = constants.VALIDATORS
+    plugin_type = constants.VALIDATE
 
 
 class ExtractorMayaPlugin(BaseMayaPlugin):
-    plugin_type = constants.EXTRACTORS
+    plugin_type = constants.OUTPUT
 
 
 class PublisherMayaPlugin(BaseMayaPlugin):
-    plugin_type = constants.PUBLISHERS
+    plugin_type = constants.PUBLISH
 
 
 # WIDGET
 class CollectorMayaWidget(BaseMayaWidget):
-    plugin_type = constants.COLLECTORS
+    plugin_type = constants.COLLECT
 
 
 class ValidatorMayaWidget(BaseMayaWidget):
-    plugin_type = constants.VALIDATORS
+    plugin_type = constants.VALIDATE
 
 
 class ExtractorMayaWidget(BaseMayaWidget):
-    plugin_type = constants.EXTRACTORS
+    plugin_type = constants.OUTPUT
 
 
 class PublisherMayaWidget(BaseMayaWidget):
-    plugin_type = constants.PUBLISHERS
+    plugin_type = constants.PUBLISH
