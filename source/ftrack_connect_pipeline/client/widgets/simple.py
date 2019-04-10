fa34cfdcb0a41864b35a9bf1ad0d7c46d3c82b54
Lorenzo Angeli <lorenzo.angeli@ftrack.com>
2019-04-10 18:28:12 +0200

    fix manual merge

--- source/ftrack_connect_pipeline/client/widgets/simple.py
+++ source/ftrack_connect_pipeline/client/widgets/simple.py
@@ -1,39 +1,71 @@
 # :coding: utf-8
 # :copyright: Copyright (c) 2019 ftrack
 
+from functools import partial
 from qtpy import QtWidgets, QtCore
 from ftrack_connect_pipeline.client.widgets import BaseWidget
 
 
 class SimpleWidget(BaseWidget):
 
-    def extract_options(self):
-        result = {}
-        for label, widget in self.widget_options.items():
-            result[label] = widget()
-
-        return result
-
-    def build_options(self, options):
-
-        for key, value in options.items():
-            option_layout = QtWidgets.QHBoxLayout()
-            option_layout.setContentsMargins(5, 1, 5, 1)
-            option_layout.setAlignment(QtCore.Qt.AlignTop)
-
-            label = QtWidgets.QLabel(key)
-
-            value = QtWidgets.QLineEdit(str(value))
-
-            self.widget_options[key] = value.text
-
-            option_layout.addWidget(label)
-            option_layout.addWidget(value)
-            self.layout().addLayout(option_layout)
-
     def __init__(self, parent=None, session=None, data=None, name=None, description=None, options=None):
+        self._type_mapping = {
+            str: self._build_str_widget,
+            int: self._build_int_widget,
+            float: self._build_float_widget,
+            list: self._build_list_widget,
+        }
         super(SimpleWidget, self).__init__(parent=parent, session=session, data=data, name=name, description=description, options=options)
 
+    def _register_widget(self, name, widget):
+        '''Register *widget* with *name* and add it to main layout.'''
+        widget_layout = QtWidgets.QHBoxLayout()
+        widget_layout.setContentsMargins(0, 0, 0, 0)
+        widget_layout.setAlignment(QtCore.Qt.AlignTop)
+        label = QtWidgets.QLabel(name)
 
+        widget_layout.addWidget(label)
+        widget_layout.addWidget(widget)
+        self.layout().addLayout(widget_layout)
 
+    def _build_str_widget(self, key, value):
+        '''build a string widget out of options *key* and *value* '''
+        widget = QtWidgets.QLineEdit(str(value))
+        self._register_widget(key, widget)
+        update_fn = partial(self.set_option_result, key=key)
+        widget.textChanged.connect(update_fn)
+        self.set_option_result(value, key)
 
+    def _build_int_widget(self, key, value):
+        '''build an integer widget out of options *key* and *value* '''
+        widget = QtWidgets.QSpinBox()
+        widget.setValue(value)
+        self._register_widget(key, widget)
+        update_fn = partial(self.set_option_result, key=key)
+        widget.valueChanged.connect(update_fn)
+        self.set_option_result(value, key)
+
+    def _build_float_widget(self, key, value):
+        widget= QtWidgets.QDoubleSpinBox()
+        widget.setValue(value)
+        self._register_widget(key, widget)
+        update_fn = partial(self.set_option_result, key=key)
+        widget.valueChanged.connect(update_fn)
+        self.set_option_result(value, key)
+
+    def _build_list_widget(self, key, values):
+        widget = QtWidgets.QComboBox()
+        widget.addItems(values)
+        self._register_widget(key, widget)
+        update_fn = partial(self.set_option_result, key=key)
+        widget.editTextChanged.connect(update_fn)
+        if len(values) > 0:
+            self.set_option_result(values[0], key)
+
+    def build(self):
+        super(SimpleWidget, self).build()
+
+        for key, value in self.options.items():
+            value_type = type(value)
+            widget_fn = self._type_mapping.get(value_type, self._build_str_widget)
+            widget_fn(key, value)
