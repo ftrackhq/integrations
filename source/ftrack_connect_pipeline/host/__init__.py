7c4c4f42f4d7e3043a48eeeb1e16c6abb8e53b12
Lorenzo Angeli <lorenzo.angeli@ftrack.com>
2019-04-11 15:11:04 +0200

    do not broadcast host if is in local mode.

--- source/ftrack_connect_pipeline/host/__init__.py
+++ source/ftrack_connect_pipeline/host/__init__.py
@@ -5,7 +5,7 @@ import uuid
 import functools
 from ftrack_connect_pipeline.host.definition import DefintionManager
 from ftrack_connect_pipeline.host.runner import PublisherRunner
-from ftrack_connect_pipeline import event, constants
+from ftrack_connect_pipeline import event, constants, utils
 import logging
 
 logger = logging.getLogger(__name__)
@@ -27,15 +27,19 @@ def initialise(session, host, ui):
     definition_manager = DefintionManager(session, host, hostid)
     package_results = definition_manager.packages.result()
     PublisherRunner(session, package_results, host, ui, hostid)
-    logger.info('initialising host: {}'.format(hostid))
 
-    handle_event = functools.partial(provide_hostid, hostid)
-    session.event_hub.subscribe(
-        'topic={}'.format(
-            constants.PIPELINE_DISCOVER_HOST
-        ),
-        handle_event
-    )
+    is_remote_event = utils.remote_event_mode()
+
+    if is_remote_event:
+        logger.info('initialising host: {}'.format(hostid))
+
+        handle_event = functools.partial(provide_hostid, hostid)
+        session.event_hub.subscribe(
+            'topic={}'.format(
+                constants.PIPELINE_DISCOVER_HOST
+            ),
+            handle_event
+        )
 
     return hostid
 
