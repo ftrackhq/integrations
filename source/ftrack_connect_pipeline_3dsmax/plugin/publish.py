67327c1b9f95087a4e3c04d590f2c5fc092ff49d
l_angeli <lorenzo.angeli@ftrack.com>
2019-06-05 14:39:56 +0200

    ensure mixin with base classes

--- source/ftrack_connect_pipeline_3dsmax/plugin/publish.py
+++ source/ftrack_connect_pipeline_3dsmax/plugin/publish.py
@@ -1,39 +1,39 @@
 # :coding: utf-8
 # :copyright: Copyright (c) 2019 ftrack
 
-from ftrack_connect_pipeline import constants
+from ftrack_connect_pipeline import plugin, constants
 from ftrack_connect_pipeline_3dsmax.plugin import BaseMaxPlugin, BaseMaxWidget
 
 
 # PLUGINS
-class CollectorMaxPlugin(BaseMaxPlugin):
+class CollectorMaxPlugin(BaseMaxPlugin, plugin.CollectorPlugin):
     plugin_type = constants.COLLECT
 
 
-class ValidatorMaxPlugin(BaseMaxPlugin):
+class ValidatorMaxPlugin(BaseMaxPlugin, plugin.ValidatorPlugin):
     plugin_type = constants.VALIDATE
 
 
-class ExtractorMaxPlugin(BaseMaxPlugin):
+class ExtractorMaxPlugin(BaseMaxPlugin, plugin.OutputPlugin):
     plugin_type = constants.OUTPUT
 
 
-class PublisherMaxPlugin(BaseMaxPlugin):
+class PublisherMaxPlugin(BaseMaxPlugin, plugin.PublisherPlugin):
     plugin_type = constants.PUBLISH
 
 
 # WIDGET
-class CollectorMaxWidget(BaseMaxWidget):
+class CollectorMaxWidget(BaseMaxWidget, plugin.CollectorWidget):
     plugin_type = constants.COLLECT
 
 
-class ValidatorMaxWidget(BaseMaxWidget):
+class ValidatorMaxWidget(BaseMaxWidget, plugin.ValidatorWidget):
     plugin_type = constants.VALIDATE
 
 
-class ExtractorMaxWidget(BaseMaxWidget):
+class ExtractorMaxWidget(BaseMaxWidget, plugin.OutputWidget):
     plugin_type = constants.OUTPUT
 
 
-class PublisherMaxWidget(BaseMaxWidget):
+class PublisherMaxWidget(BaseMaxWidget, plugin.PublisherWidget):
     plugin_type = constants.PUBLISH
