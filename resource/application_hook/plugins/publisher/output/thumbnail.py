aef91e60e80f38e88f340c9f537a936ccb0881eb
l_angeli <lorenzo.angeli@ftrack.com>
2019-06-06 15:31:33 +0200

    add thumbnail implementation from old takeScreenshot method

--- resource/application_hook/plugins/publisher/output/thumbnail.py
+++ resource/application_hook/plugins/publisher/output/thumbnail.py
@@ -1,9 +1,9 @@
 # :coding: utf-8
 # :copyright: Copyright (c) 2019 ftrack
 
-import tempfile
-
-import pymxs
+import os
+import uuid
+import MaxPlus
 
 from ftrack_connect_pipeline_3dsmax import plugin
 
@@ -12,7 +12,23 @@ class ExtractThumbnailPlugin(plugin.ExtractorMaxPlugin):
     plugin_name = 'thumbnail'
 
     def run(self, context=None, data=None, options=None):
-        pass
+        view = MaxPlus.ViewportManager.GetActiveViewport()
+        bm = MaxPlus.Factory.CreateBitmap()
+        storage = MaxPlus.Factory.CreateStorage(7)
+        info = storage.GetBitmapInfo()
+        bm.SetStorage(storage)
+        bm.DeleteStorage()
+        res = view.GetDIB(info, bm)
+        if not res:
+            return
+
+        filename = '{0}.jpg'.format(uuid.uuid4().hex)
+        outpath = os.path.join(MaxPlus.PathManager.GetTempDir(), filename)
+        info.SetName(outpath)
+        bm.OpenOutput(info)
+        bm.Write(info)
+        bm.Close(info)
+        return outpath
 
 
 def register(api_object, **kw):
