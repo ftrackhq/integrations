54c294cf43d1214a91182ea673c1f25ed8f80cb9
Lluis Casals <lluis.casals@ftrack.com>
2019-12-04 11:10:43 +0100

    added test folder

--- tests/pipeline_test.py
+++ tests/pipeline_test.py
@@ -1,20 +1,24 @@
-from ftrack_connect_pipeline.session import get_shared_session
+#from ftrack_connect_pipeline.session import get_shared_session
 from ftrack_connect_pipeline import client, host
 from ftrack_connect_pipeline import event
 import os
+import ftrack_api
+#os.environ['FTRACK_EVENT_PLUGIN_PATH'] = '/Users/lluisftrack/work/brokenC/ftrack/repos/ftrack-connect-pipeline-definition/resource/application_hook'
+
+
+#session = get_shared_session('/Users/lluisftrack/work/brokenC/ftrack/repos/ftrack-connect-pipeline-definition/resource/application_hook')
+
+
 os.environ['FTRACK_EVENT_PLUGIN_PATH'] = '/Users/lluisftrack/work/brokenC/ftrack/repos/ftrack-connect-pipeline-definition/resource/application_hook'
+logging.getLogger(__name__)
+session = ftrack_api.Session()
+print session
 
-
-session = get_shared_session()
-event_thread = event.NewApiEventHubThread()
-event_thread.start(session)
 
 
 baseHost = host.initialise(session, host='api', ui=None)
 baseClient = client.BasePipelineClient(ui=None, host='api', hostid=baseHost)
 print baseClient.packages
-
-
 
 
 
