ed9923ae5899af85e4715441865b828bb5c4a597
Lluis Casals <lluis.casals@ftrack.com>
2019-12-09 15:10:25 +0100

    code cleaned

--- resource/application_hook/plugins/publisher/publishers/publish_result.py
+++ resource/application_hook/plugins/publisher/publishers/publish_result.py
@@ -72,21 +72,14 @@ class FtrackPublishPlugin(plugin.PublisherPlugin):
 
         results = {}
 
-        self.logger.debug("data lluisTest -------> {}".format(data))
-        self.logger.debug("data.items lluisTest -------> {}".format(data.items()))
-
         for component_name, component_path in data.items():
-            self.logger.debug("publishing component -------> {}".format(component_name))
             publish_component_fn = self.component_functions.get(
                 component_name, self.create_component
             )
             publish_component_fn(asset_version, component_name, component_path)
             results[component_name] = True
-            self.logger.debug("finish publishing component -------> {}".format(component_name))
 
-        self.logger.debug("finish publishing all components ")
         self.session.commit()
-        self.logger.debug("finish commit ")
 
         self.logger.debug("publishing: {} to {} as {}".format(data, context, asset_object))
 
--- source/ftrack_connect_pipeline/host/runner/publish.py
+++ source/ftrack_connect_pipeline/host/runner/publish.py
@@ -179,8 +179,6 @@ class PublisherRunner(object):
         statuses = []
         results = []
 
-        self.logger.debug("publish_plugins --->  {} publish_data --->  {} context_data ---> {}".format(publish_plugins, publish_data, context_data))
-
         for plugin in publish_plugins:
             status, result = self._run_plugin(
                 plugin, constants.PUBLISHERS,
@@ -236,7 +234,6 @@ class PublisherRunner(object):
                 for key, value in output.items():
                     publish_data[key] = value
 
-        self.logger.debug("publish data --> {} ".format(publish_data))
         publish_status, publish_result = self.run_publish(
             publish_plugins, publish_data, context_result
         )
