7e91e8506e4aaf34d680be80aa594efb9de4aee3
Lluis Casals <lluis.casals@ftrack.com>
2019-12-09 14:11:13 +0100

    updated constants

--- source/ftrack_connect_pipeline_maya/plugin/publish.py
+++ source/ftrack_connect_pipeline_maya/plugin/publish.py
@@ -7,33 +7,33 @@ from ftrack_connect_pipeline_maya.plugin import BaseMayaPlugin, BaseMayaWidget
 
 # PLUGINS
 class CollectorMayaPlugin(BaseMayaPlugin, plugin.CollectorPlugin):
-    plugin_type = constants.COLLECT
+    plugin_type = constants.COLLECTORS
 
 
 class ValidatorMayaPlugin(BaseMayaPlugin, plugin.ValidatorPlugin):
-    plugin_type = constants.VALIDATE
+    plugin_type = constants.VALIDATORS
 
 
 class OutputMayaPlugin(BaseMayaPlugin, plugin.OutputPlugin):
-    plugin_type = constants.OUTPUT
+    plugin_type = constants.OUTPUTS
 
 
 class PublisherMayaPlugin(BaseMayaPlugin, plugin.PublisherPlugin):
-    plugin_type = constants.PUBLISH
+    plugin_type = constants.PUBLISHERS
 
 
 # WIDGET
 class CollectorMayaWidget(BaseMayaWidget, plugin.CollectorWidget):
-    plugin_type = constants.COLLECT
+    plugin_type = constants.COLLECTORS
 
 
 class ValidatorMayaWidget(BaseMayaWidget, plugin.ValidatorWidget):
-    plugin_type = constants.VALIDATE
+    plugin_type = constants.VALIDATORS
 
 
 class OutputMayaWidget(BaseMayaWidget, plugin.OutputWidget):
-    plugin_type = constants.OUTPUT
+    plugin_type = constants.OUTPUTS
 
 
 class PublisherMayaWidget(BaseMayaWidget, plugin.PublisherWidget):
-    plugin_type = constants.PUBLISH
+    plugin_type = constants.PUBLISHERS
