56f896c0d508e1bb38a42d0065960c0e7ac8c53a
Lluis Casals <lluis.casals@ftrack.com>
2019-12-11 14:13:48 +0100

    changed schema constants

--- source/ftrack_connect_pipeline/constants/schema.py
+++ source/ftrack_connect_pipeline/constants/schema.py
@@ -5,6 +5,6 @@ from ftrack_connect_pipeline.constants import _BASE_
 
 
 # Publish stages.
-LOADER_SCHEMA = 'loader-schema'
-PACKAGE_SCHEMA = 'package-schema'
-PUBLISHER_SCHEMA = 'publisher-schema'
+LOADER_SCHEMA = 'loader.schema'
+PACKAGE_SCHEMA = 'package.schema'
+PUBLISHER_SCHEMA = 'publisher.schema'
