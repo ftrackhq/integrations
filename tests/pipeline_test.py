00490c05cab67b8d53b9df21095351b91bd43c46
Lorenzo Angeli <lorenzo.angeli@ftrack.com>
2019-12-12 11:38:55 +0100

    cleanup paths

--- tests/pipeline_test.py
+++ tests/pipeline_test.py
@@ -2,14 +2,15 @@ import os
 from ftrack_connect_pipeline import client, host, constants
 from ftrack_connect_pipeline.session import get_shared_session
 
+CWD = os.path.dirname(__name__)
+
 event_paths = [
-    os.path.abspath(os.path.join('ftrack-connect-pipeline-definition', 'resource', 'application_hook')),
-    os.path.abspath(os.path.join('ftrack-connect-pipeline', 'resource', 'application_hook'))
+    os.path.abspath(os.path.join(CWD, '..', '..', 'ftrack-connect-pipeline-definition', 'resource', 'application_hook')),
+    os.path.abspath(os.path.join(CWD, '..', '..', 'ftrack-connect-pipeline', 'resource', 'application_hook'))
 ]
 
-paths = os.pathsep.join(event_paths)
-print paths
-os.environ['FTRACK_EVENT_PLUGIN_PATH'] = paths
+print event_paths
+os.environ['FTRACK_EVENT_PLUGIN_PATH'] = os.pathsep.join(event_paths)
 
 session = get_shared_session()
 host_id = host.initialise(session, host=constants.HOST, ui=constants.UI)
