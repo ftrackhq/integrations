a9af058f6b523394608669801a183f306da80eab
Lorenzo Angeli <lorenzo.angeli@ftrack.com>
2019-12-18 12:28:59 +0100

    add filesystem collector plugin

---
+++ resource/application_hook/plugins/publisher/collector/filesystem.py
@@ -0,0 +1,17 @@
+# :coding: utf-8
+# :copyright: Copyright (c) 2019 ftrack
+
+import os
+from ftrack_connect_pipeline import plugin
+
+
+class FilesystemCollectPlugin(plugin.CollectorPlugin):
+    plugin_name = 'filesystem'
+
+    def run(self, context=None, data=None, options=None):
+        return [options['path']]
+
+
+def register(api_object, **kw):
+    plugin = FilesystemCollectPlugin(api_object)
+    plugin.register()
