471a5d8d2d68396b2942005fe19a6ea123dab34f
Lluis Casals <lluis.casals@ftrack.com>
2019-12-20 13:29:14 +0100

    added todo in load.py from plugin

--- source/ftrack_connect_pipeline/plugin/load.py
+++ source/ftrack_connect_pipeline/plugin/load.py
@@ -1,6 +1,8 @@
 # :coding: utf-8
 # :copyright: Copyright (c) 2019 ftrack
 
+#TODO: THIS SHOULD BE DEPECATED
+
 from ftrack_connect_pipeline import constants
 from ftrack_connect_pipeline.plugin import BasePlugin
 
