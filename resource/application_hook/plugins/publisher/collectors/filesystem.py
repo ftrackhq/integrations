afd483a188299b316eaa841101c71fec25ce1ad3
Lorenzo Angeli <lorenzo.angeli@ftrack.com>
2020-01-08 15:38:48 +0100

    cleanup docs

--- resource/application_hook/plugins/publisher/collectors/filesystem.py
+++ resource/application_hook/plugins/publisher/collectors/filesystem.py
@@ -9,17 +9,17 @@ class FilesystemCollectPlugin(plugin.CollectorPlugin):
     plugin_name = 'filesystem'
 
     def run(self, context=None, data=None, options=None):
-        '''*context* Dictionary with the asset_name, context_id, asset_type, comment and status_id of the asset that
-        we are working on. Example: 'context': {u'asset_name': 'PipelineAsset',
-                                                u'context_id': u'529af752-2274-11ea-a019-667d1218a15f',
-                                                'asset_type': u'geo',
-                                                 u'comment': 'A new hope',
-                                                 u'status_id': u'44dd9fb2-4164-11df-9218-0019bb4983d8'}
-        *data* list of data coming from previous collector or empty list
-        *options* Dictionary of options added from the ui or manually added.
-        Predefined available option: *option['path']*
-        Return type: List
-        Returns: List of paths of objects to collect '''
+        '''Run the current plugin with , *context* , *data* and *options*.
+
+        *context* provides a mapping with the asset_name, context_id, asset_type,
+        comment and status_id of the asset that we are working on.
+
+        *data* a list of data coming from previous collector and
+        *options* a dictionary of options passed from ourside.
+
+        Returns a List of paths of objects to collect
+
+        '''
         return [options['path']]
 
 
