1e5b37721ee5eaa33271fb4f6536c1d773d23be9
Lorenzo Angeli <lorenzo.angeli@ftrack.com>
2020-01-15 14:57:39 +0100

    add qt resources in plugins

--- tests/widgets_pipeline_test.py
+++ tests/widgets_pipeline_test.py
@@ -9,10 +9,9 @@ CWD = os.path.dirname(__name__)
 
 event_paths = [
     os.path.abspath(os.path.join('ftrack-connect-pipeline-definition', 'resource', 'application_hook')),
-    os.path.abspath(os.path.join('ftrack-connect-pipeline', 'resource', 'application_hook'))
+    os.path.abspath(os.path.join('ftrack-connect-pipeline', 'resource', 'application_hook')),
+    os.path.abspath(os.path.join('ftrack-connect-pipeline-qt', 'resource', 'application_hook')),
 ]
-
-# os.environ['FTRACK_EVENT_PLUGIN_PATH'] = os.pathsep.join(event_paths)
 
 # create event manager
 session = get_shared_session(plugin_paths=event_paths)
