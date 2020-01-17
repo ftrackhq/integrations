c9c70bd68896d264d3b2dffd50235489ec10f640
Lorenzo Angeli <lorenzo.angeli@ftrack.com>
2020-01-17 15:02:00 +0100

    code cleanup

--- tests/pipeline_test.py
+++ tests/pipeline_test.py
@@ -1,6 +1,5 @@
 import os
 from ftrack_connect_pipeline import client, host, constants, event
-from ftrack_connect_pipeline.session import get_shared_session
 import ftrack_api
 
 CWD = os.path.dirname(__name__)
@@ -9,11 +8,6 @@ event_paths = [
     os.path.abspath(os.path.join('ftrack-connect-pipeline-definition', 'resource', 'application_hook')),
     os.path.abspath(os.path.join('ftrack-connect-pipeline', 'resource', 'application_hook'))
 ]
-
-# os.environ['FTRACK_EVENT_PLUGIN_PATH'] = os.pathsep.join(event_paths)
-
-# create event manager
-# session = get_shared_session(plugin_paths=event_paths)
 
 session = ftrack_api.Session(auto_connect_event_hub=False, plugin_paths=event_paths)
 event_manager = event.EventManager(
