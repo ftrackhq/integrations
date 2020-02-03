915f93400a461d43bbf0e14b4c5783ddba61e153
Lorenzo Angeli <lorenzo.angeli@ftrack.com>
2020-02-03 11:53:50 +0100

    reorder imports

--- source/ftrack_connect_pipeline_qt/client/publish.py
+++ source/ftrack_connect_pipeline_qt/client/publish.py
@@ -4,7 +4,9 @@
 
 import os
 import sys
+import logging
 
+logger = logging.getLogger('ftrack_connect_pipeline_qt')
 
 deps_paths = os.environ.get('PYTHONPATH', '').split(os.pathsep)
 for path in deps_paths:
@@ -15,27 +17,40 @@ import ftrack_api
 
 QtWidgets.QApplication.setStyle('plastique')
 
-from ftrack_connect_pipeline_qt.client import QtClient
-from ftrack_connect_pipeline import constants, event
-from ftrack_connect_pipeline_qt import constants as qt_contants
-
-
-class QtPublisherClient(QtClient):
-    '''
-    Base load widget class.
-    '''
-    def __init__(self, event_manager, ui, parent=None):
-
-        super(QtPublisherClient, self).__init__(event_manager, ui, parent=parent)
-        self.setWindowTitle('Standalone Pipeline Publisher')
-
 
 if __name__ == '__main__':
+    print 'CALLING !!!!'
+
+    app = QtWidgets.QApplication.instance()
+    print app
+    if not app:
+        app = QtWidgets.QApplication(sys.argv)
+
+    from ftrack_connect_pipeline_qt.client import QtClient
+
+    from ftrack_connect_pipeline import constants, event, host
+    from ftrack_connect_pipeline_qt import constants as qt_constants
+
+    class QtPublisherClient(QtClient):
+        '''
+        Base load widget class.
+        '''
+
+        def __init__(self, event_manager, ui, parent=None):
+            super(QtPublisherClient, self).__init__(event_manager, ui,
+                                                    parent=parent)
+            self.setWindowTitle('Standalone Pipeline Publisher')
+            self.logger.info('start qt publisher')
+
+
+    logger.info('start publisher.....')
     session = ftrack_api.Session(auto_connect_event_hub=False)
     event_manager = event.EventManager(
         session=session, mode=constants.LOCAL_EVENT_MODE
     )
-    app = QtWidgets.QApplication(sys.argv)
-    wid = QtPublisherClient(event_manager, ui=[qt_contants.UI])
+
+    host.Host(event_manager)
+
+    wid = QtPublisherClient(event_manager, ui=[qt_constants.UI])
     wid.show()
     sys.exit(app.exec_())
