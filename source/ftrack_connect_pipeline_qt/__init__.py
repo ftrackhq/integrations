6aded7775b7ad646c12415588ec30f10fcbecfba
Lorenzo Angeli <lorenzo.angeli@ftrack.com>
2020-02-04 12:17:54 +0100

    improve logging

--- source/ftrack_connect_pipeline_qt/__init__.py
+++ source/ftrack_connect_pipeline_qt/__init__.py
@@ -1,4 +1,4 @@
 from ._version import __version__
 
 from ftrack_connect_pipeline.configure_logging import configure_logging
-configure_logging('ftrack_connect_pipeline_qt')
+configure_logging('ftrack_connect_pipeline_qt', extra_modules=['python_jsonschema_objects'])
