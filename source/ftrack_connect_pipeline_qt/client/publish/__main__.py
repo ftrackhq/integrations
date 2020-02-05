60e3d0138c2da09d10726fb267492be952029657
Lorenzo Angeli <lorenzo.angeli@ftrack.com>
2020-02-05 09:01:49 +0100

    create qapp if instance is not available

--- source/ftrack_connect_pipeline_qt/client/publish/__main__.py
+++ source/ftrack_connect_pipeline_qt/client/publish/__main__.py
@@ -11,14 +11,16 @@ for path in deps_paths:
     sys.path.append(path)
 
 
-QtWidgets.QApplication.setStyle('plastique')
-
-import ftrack_api
-from ftrack_connect_pipeline import constants, event, host
-from ftrack_connect_pipeline_qt.client.publish import QtPublisherClient
-
 if __name__ == '__main__':
+
     app = QtWidgets.QApplication.instance()
+    if not app:
+        app = QtWidgets.QApplication(sys.argv)
+
+    import ftrack_api
+    from ftrack_connect_pipeline import constants, event, host
+    from ftrack_connect_pipeline_qt.client.publish import QtPublisherClient
+
     session = ftrack_api.Session(auto_connect_event_hub=False)
     event_manager = event.EventManager(
         session=session, mode=constants.LOCAL_EVENT_MODE
