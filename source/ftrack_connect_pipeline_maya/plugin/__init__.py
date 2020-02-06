bd47b96a794479b84db96049c237e99d4dc5eae8
Lorenzo Angeli <lorenzo.angeli@ftrack.com>
2020-02-06 17:04:37 +0100

    remove execute in main thread

--- source/ftrack_connect_pipeline_maya/plugin/__init__.py
+++ source/ftrack_connect_pipeline_maya/plugin/__init__.py
@@ -12,14 +12,10 @@ from ftrack_connect_pipeline_maya import constants as maya_constants
 class BaseMayaPlugin(plugin.BasePlugin):
     host = maya_constants.HOST
 
-    def _run(self, event):
-        super_fn = super(BaseMayaPlugin, self)._run
-        result = maya.utils.executeInMainThreadWithResult(super_fn, event)
-        return result
-
-
-# class BaseMayaPlugin(plugin.BasePlugin, _BaseMaya):
-#     type = 'plugin'
+    # def _run(self, event):
+    #     super_fn = super(BaseMayaPlugin, self)._run
+    #     result = maya.utils.executeInMainThreadWithResult(super_fn, event)
+    #     return result
 
 
 class BaseMayaPluginWidget(BaseMayaPlugin, pluginWidget.BasePluginWidget):
