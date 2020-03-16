8bdcb6da2dd3c651757192706bb7d20e4446f768
Lorenzo Angeli <lorenzo.angeli@ftrack.com>
2020-03-16 10:08:31 +0100

    add camera widget

---
+++ resource/application_hook/plugins/publisher/collectors/widget/camera_collector_ui.py
@@ -0,0 +1,63 @@
+# :coding: utf-8
+# :copyright: Copyright (c) 2019 ftrack
+
+from functools import partial
+
+from ftrack_connect_pipeline_maya import plugin
+from ftrack_connect_pipeline_qt.client.widgets.options import BaseOptionsWidget
+
+from Qt import QtWidgets
+
+import maya.cmds as mcd
+
+
+class CameraCollectorWidget(BaseOptionsWidget):
+
+    def __init__(
+        self, parent=None, session=None, data=None, name=None,
+        description=None, options=None, context=None
+    ):
+
+        self.maya_cameras = mcd.ls(type='camera')
+
+        super(CameraCollectorWidget, self).__init__(
+            parent=parent,
+            session=session, data=data, name=name,
+            description=description, options=options,
+            context=context)
+
+    def build(self):
+        '''build function , mostly used to create the widgets.'''
+        super(CameraCollectorWidget, self).build()
+        self.cameras = QtWidgets.QComboBox()
+        self.cameras.setToolTip(self.description)
+        self.layout().addWidget(self.cameras)
+
+        if not self.maya_cameras:
+            self.cameras.setDisabled(True)
+            self.cameras.addItem('No Suitable Cameras found.')
+        else:
+            self.cameras.addItems(self.maya_cameras)
+
+    def post_build(self):
+        super(CameraCollectorWidget, self).post_build()
+        update_fn = partial(self.set_option_result, key='camera_name')
+
+        self.cameras.editTextChanged.connect(update_fn)
+        self.set_option_result(self.maya_cameras[0], key='camera_name')
+
+
+class CameraCollectorPluginWidget(plugin.CollectorMayaWidget):
+    plugin_name = 'camera'
+
+    def run(self, context=None, data=None, name=None, description=None, options=None):
+        return CameraCollectorWidget(
+            context=context,
+            session=self.session, data=data, name=name,
+            description=description, options=options
+        )
+
+
+def register(api_object, **kw):
+    plugin = CameraCollectorPluginWidget(api_object)
+    plugin.register()
