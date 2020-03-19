786bf60a20b3c540d1ca2dd61f32c7204df735d3
Lorenzo Angeli <lorenzo.angeli@ftrack.com>
2020-03-19 16:57:57 +0100

    add alembic start looking into fbx

---
+++ resource/application_hook/plugins/publisher/output/fbx_output.py
@@ -0,0 +1,122 @@
+# :coding: utf-8
+# :copyright: Copyright (c) 2019 ftrack
+
+import tempfile
+
+import maya.cmds as cmd
+import maya.mel as mel
+
+import maya
+
+from ftrack_connect_pipeline_maya import plugin
+
+
+
+class OutputMayaAlembicPlugin(plugin.OutputMayaPlugin):
+
+    plugin_name = 'fbx'
+
+    def extract_options(self, options):
+
+        return {
+            'FBXExportScaleFactor': int(options.get('FBXExportScaleFactor', 1)),
+            'FBXExportUpAxis': str(options.get('FBXExportUpAxis', 'y')),
+            'FBXExportFileVersion': str(options.get('FBXExportFileVersion', 'FBX201600')),
+            'FBXExportSmoothMesh':bool(options.get('FBXExportSmoothMesh', False)),
+            'FBXExportInAscii': bool(options.get('FBXExportScaleFactor', False)),
+            'FBXExportAnimationOnly': bool(options.get('FBXExportAnimationOnly', False)),
+            'FBXExportInstances': bool(options.get('FBXExportInstances', False)),
+            'FBXExportApplyConstantKeyReducer': bool(options.get('FBXExportApplyConstantKeyReducer', False)),
+            'FBXExportBakeComplexAnimation': bool(options.get('FBXExportBakeComplexAnimation', False)),
+            'FBXExportBakeResampleAnimation': bool(options.get('FBXExportBakeResampleAnimation', False)),
+            'FBXExportCameras': bool(options.get('FBXExportCameras', True)),
+            'FBXExportLights': bool(options.get('FBXExportLights', True)),
+            'FBXExportConstraints': bool(options.get('FBXExportConstraints', False)),
+            'FBXExportEmbeddedTextures': bool(options.get('FBXExportEmbeddedTextures', False)),
+
+        }
+
+    def run(self, context=None, data=None, options=None):
+        # ensure to load the alembic plugin
+        cmd.loadPlugin('AbcExport.so', qt=1)
+
+        component_name = options['component_name']
+        new_file_path = tempfile.NamedTemporaryFile(
+            delete=False,
+            suffix='.fbx'
+        ).name
+
+        options = self.extract_options(options)
+
+        self.logger.debug(
+            'Calling output options: data {}. options {}'.format(
+                data, options
+            )
+        )
+        # fbx basic options
+        mel.eval('FBXResetExport')
+        mel.eval('FBXExportConvertUnitString "cm"')
+        mel.eval('FBXExportGenerateLog -v 0')
+
+        # fbx user options
+        scale = options.get('FBXExportScaleFactor')
+        mel.eval('FBXExportScaleFactor {}'.format(scale))
+
+        up_axis = options.get('FBXExportUpAxis')
+        mel.eval('FBXExportUpAxis {}'.format(up_axis))
+
+        version = options.get('FBXExportFileVersion')
+        mel.eval('FBXExportFileVersion {}'.format(version))
+
+        smooth_mesh = options.get('FBXExportSmoothMesh')
+        mel.eval('FBXExportSmoothMesh -v {}'.format(int(smooth_mesh)))
+
+        ascii_export = options.get('FBXExportInAscii')
+        mel.eval('FBXExportInAscii -v {}'.format(int(ascii_export)))
+
+        anim_only = options.get('FBXExportAnimationOnly')
+        mel.eval('FBXExportAnimationOnly -v {}'.format(int(anim_only)))
+
+        intances = options.get('FBXExportInstances')
+        mel.eval('FBXExportInstances -v {}'.format(int(intances)))
+
+        constraint_reducer = options.get('FBXExportApplyConstantKeyReducer')
+        mel.eval('FBXExportApplyConstantKeyReducer -v {}'.format(int(constraint_reducer)))
+
+        bake_complex_anim = options.get('FBXExportBakeComplexAnimation')
+        mel.eval('FBXExportBakeComplexAnimation -v {}'.format(int(bake_complex_anim)))
+
+        bake_resample_anim = options.get('FBXExportBakeResampleAnimation')
+        mel.eval('FBXExportBakeResampleAnimation -v {}'.format(
+            int(bake_resample_anim)))
+
+        export_camera = options.get('FBXExportCameras')
+        mel.eval('FBXExportCameras -v {}'.format(int(export_camera)))
+
+        export_constraints = options.get('FBXExportConstraints')
+        mel.eval('FBXExportConstraints -v {}'.format(int(export_constraints)))
+
+        embedded_textures = options.get('FBXExportEmbeddedTextures')
+        mel.eval(
+            'FBXExportEmbeddedTextures -v {}'.format(int(embedded_textures)))
+
+        export_lights = options.get('FBXExportLights')
+        mel.eval('FBXExportLights -v {}'.format(int(export_lights)))
+
+        # fbx export command
+        fbx_export_cmd = 'FBXExport -f "{}"'.format(new_file_path)
+
+        if iAObj.options.get('fbxExportMode') == 'Selection':
+            fbx_export_cmd += ' -s'
+
+        mel.eval(fbx_export_cmd)
+
+        if selectednodes:
+            mc.select(selectednodes)
+
+        return {component_name: new_file_path}
+
+
+def register(api_object, **kw):
+    ma_plugin = OutputMayaAlembicPlugin(api_object)
+    ma_plugin.register()
---
+++ resource/application_hook/plugins/publisher/output/widget/alembic_options.py
@@ -0,0 +1,76 @@
+# :coding: utf-8
+# :copyright: Copyright (c) 2019 ftrack
+
+from functools import partial
+
+from ftrack_connect_pipeline_maya import plugin
+from ftrack_connect_pipeline_qt.client.widgets.options import BaseOptionsWidget
+
+from Qt import QtWidgets
+
+import maya.cmds as mcd
+
+
+class AlembicOptionsWidget(BaseOptionsWidget):
+
+    def __init__(
+        self, parent=None, session=None, data=None, name=None,
+        description=None, options=None, context=None
+    ):
+
+        self.options_cb = []
+
+        super(AlembicOptionsWidget, self).__init__(
+            parent=parent,
+            session=session, data=data, name=name,
+            description=description, options=options,
+            context=context)
+
+    def build(self):
+        '''build function , mostly used to create the widgets.'''
+        super(AlembicOptionsWidget, self).build()
+
+        frames_option = [
+            'frameStart',
+            'frameEnd',
+        ]
+
+        bool_options = [
+            'alembicAnimation',
+            'alembicUvwrite',
+            'alembicWorldspace',
+            'alembicWritevisibility',
+            'alembicEval'
+        ]
+
+        self.option_group = QtWidgets.QGroupBox('Alembic Output Options')
+        self.option_group.setToolTip(self.description)
+
+        self.option_layout = QtWidgets.QVBoxLayout()
+        self.option_group.setLayout(self.option_layout)
+
+        self.layout().addWidget(self.option_group)
+        for option in bool_options:
+            option_check = QtWidgets.QCheckBox(option)
+
+            self.options_cb.append(option_check)
+            self.option_layout.addWidget(option_check)
+
+            update_fn = partial(self.set_option_result, key=option)
+            option_check.stateChanged.connect(update_fn)
+
+
+class AlembicOptionsPluginWidget(plugin.OutputMayaWidget):
+    plugin_name = 'alembic_options'
+
+    def run(self, context=None, data=None, name=None, description=None, options=None):
+        return AlembicOptionsWidget(
+            context=context,
+            session=self.session, data=data, name=name,
+            description=description, options=options
+        )
+
+
+def register(api_object, **kw):
+    plugin = AlembicOptionsPluginWidget(api_object)
+    plugin.register()
