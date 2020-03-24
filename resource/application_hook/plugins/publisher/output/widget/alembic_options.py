f0b0bf5f21aebd914d753edc15055b6f8e5efb5b
Lorenzo Angeli <lorenzo.angeli@ftrack.com>
2020-03-24 09:49:52 +0100

    update options

--- resource/application_hook/plugins/publisher/output/widget/alembic_options.py
+++ resource/application_hook/plugins/publisher/output/widget/alembic_options.py
@@ -4,21 +4,21 @@
 from functools import partial
 
 from ftrack_connect_pipeline_maya import plugin
-from ftrack_connect_pipeline_qt.client.widgets.options import BaseOptionsWidget
+from ftrack_connect_pipeline_qt.client.widgets.options.dynamic import DynamicWidget
 
 from Qt import QtWidgets
 
 import maya.cmds as mcd
 
 
-class AlembicOptionsWidget(BaseOptionsWidget):
+class AlembicOptionsWidget(DynamicWidget):
 
     def __init__(
         self, parent=None, session=None, data=None, name=None,
         description=None, options=None, context=None
     ):
 
-        self.options_cb = []
+        self.options_cb = {}
 
         super(AlembicOptionsWidget, self).__init__(
             parent=parent,
@@ -53,11 +53,18 @@ class AlembicOptionsWidget(BaseOptionsWidget):
         for option in bool_options:
             option_check = QtWidgets.QCheckBox(option)
 
-            self.options_cb.append(option_check)
+            self.options_cb[option] = option_check
             self.option_layout.addWidget(option_check)
 
             update_fn = partial(self.set_option_result, key=option)
             option_check.stateChanged.connect(update_fn)
+
+    def post_build(self):
+        super(AlembicOptionsWidget, self).post_build()
+
+        for option, widget in self.options_cb.items():
+            update_fn = partial(self.set_option_result, key=option)
+            widget.stateChanged.connect(update_fn)
 
 
 class AlembicOptionsPluginWidget(plugin.OutputMayaWidget):
