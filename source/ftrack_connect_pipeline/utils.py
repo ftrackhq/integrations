aaaad3d046ba738a71e557a1ddbc7b44ad32d656
Lluis Casals <lluis.casals@ftrack.com>
2020-03-27 08:41:20 +0100

    added asynchronous decorator on create_reviwable and create_thumbnail functions

--- source/ftrack_connect_pipeline/utils.py
+++ source/ftrack_connect_pipeline/utils.py
@@ -21,3 +21,32 @@ def get_current_context():
     )
 
     return context_id
+
+
+def asynchronous(method):
+    '''Decorator to make a method asynchronous using its own thread.'''
+
+    def wrapper(*args, **kwargs):
+        '''Thread wrapped method.'''
+
+        def exceptHookWrapper(*args, **kwargs):
+            '''Wrapp method and pass exceptions to global excepthook.
+
+            This is needed in threads because of
+            https://sourceforge.net/tracker/?func=detail&atid=105470&aid=1230540&group_id=5470
+            '''
+            try:
+                method(*args, **kwargs)
+            except (KeyboardInterrupt, SystemExit):
+                raise
+            except:
+                sys.excepthook(*sys.exc_info())
+
+        thread = threading.Thread(
+            target=exceptHookWrapper,
+            args=args,
+            kwargs=kwargs
+        )
+        thread.start()
+
+    return wrapper
