84e335a61136e3955dba9ae38e8766fd9085ff90
Lluis Casals <lluis.casals@ftrack.com>
2020-04-29 10:22:44 +0200

    delete unused host functions

--- source/ftrack_connect_pipeline_maya/host/__init__.py
+++ source/ftrack_connect_pipeline_maya/host/__init__.py
@@ -38,31 +38,5 @@ def get_ftrack_menu(menu_name = 'ftrack_pipeline'):
     return menu
 
 
-def mark_menu(hostid, event):
-    '''mark menu as connected or disconnnected.'''
-    client_hostid = event['data']['pipeline']['hostid']
-    menu = get_ftrack_menu()
-    if client_hostid == hostid:
-        logger.info('client connected')
-        mc.menu(menu, e=True, l='ftrack_pipeline (connected)')
-        # TODO: Mark somehow the menu to be connected
-    else:
-        logger.info('client disconnected')
-        mc.menu(menu, e=True, l='ftrack_pipeline')
-        # TODO: remove marked menu
-
-
-def notify_connected_client(session, hostid):
-    '''event handler to notify connected clients.'''
-    event_handler = functools.partial(mark_menu, hostid)
-
-    session.event_hub.subscribe(
-        'topic={}'.format(
-            constants.PIPELINE_CONNECT_CLIENT
-        ),
-        event_handler
-    )
-
-
 class MayaHost(Host):
     host = [qt_constants.HOST, maya_constants.HOST]
