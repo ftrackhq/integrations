d1d9a9866f448998fc3e32f2467fe04da8f40240
Lluis Casals <lluis.casals@ftrack.com>
2020-05-13 11:33:28 +0200

    implementing asset base class

---
+++ source/ftrack_connect_pipeline/constants/asset/v1.py
@@ -0,0 +1,13 @@
+# :coding: utf-8
+# :copyright: Copyright (c) 2019 ftrack
+
+ASSET_ID = ''
+ASSET_NAME = ''
+ASSET_TYPE = 'assetType'
+VERSION_ID = 'assetId'
+VERSION_NUMBER = 'assetVersion'
+CONTEXT_ID = ''
+COMPONENT_PATH = 'assetPath'
+COMPONENT_NAME = 'assetTake'
+COMPONENT_ID = 'assetComponentId'
+
---
+++ source/ftrack_connect_pipeline/constants/asset/v2.py
@@ -0,0 +1,12 @@
+# :coding: utf-8
+# :copyright: Copyright (c) 2019 ftrack
+
+ASSET_ID = 'asset_id'
+ASSET_NAME = 'asset_name'
+ASSET_TYPE = 'asset_type'
+VERSION_ID = 'version_id'
+VERSION_NUMBER = 'version_number'
+CONTEXT_ID = 'context_id'
+COMPONENT_PATH = 'asset_path'
+COMPONENT_NAME = 'asset_take'
+COMPONENT_ID = 'asset_component_id'
