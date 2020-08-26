30b08936e64f24601510b402c6b698d4fc274ea4
Lluis Casals <lluis.casals@ftrack.com>
2020-08-26 15:28:00 +0200

    clean code

--- source/ftrack_connect_pipeline_maya/constants/asset/__init__.py
+++ source/ftrack_connect_pipeline_maya/constants/asset/__init__.py
@@ -7,4 +7,3 @@ FTRACK_PLUGIN_TYPE = 'ftrackAssetNode'
 #TODO: Check if we have a circular import here as we also import asset ve on the init
 from ftrack_connect_pipeline.constants.asset import *
 from ftrack_connect_pipeline_maya.constants.asset.v2 import *
-from ftrack_connect_pipeline.constants.asset.versions_mapping import *
