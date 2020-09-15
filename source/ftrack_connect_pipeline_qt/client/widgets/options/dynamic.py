3c545a3e79f86f45cb5025bc5a51af4106abf271
Lluis Casals <lluis.casals@ftrack.com>
2020-09-15 15:35:22 +0200

    combobox signal changed

--- source/ftrack_connect_pipeline_qt/client/widgets/options/dynamic.py
+++ source/ftrack_connect_pipeline_qt/client/widgets/options/dynamic.py
@@ -82,7 +82,7 @@ class DynamicWidget(BaseOptionsWidget):
         widget.addItems(values)
         self._register_widget(key, widget)
         update_fn = partial(self.set_option_result, key=key)
-        widget.editTextChanged.connect(update_fn)
+        widget.currentTextChanged.connect(update_fn)
         if len(values) > 0:
             self.set_option_result(values[0], key)
 
