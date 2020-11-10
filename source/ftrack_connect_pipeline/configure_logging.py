ba155ce426d59c59b011cf53987b84ff624dc98e
Lluis Casals <lluis.casals@ftrack.com>
2020-11-10 12:27:47 +0100

    configure logging fixed

--- source/ftrack_connect_pipeline/configure_logging.py
+++ source/ftrack_connect_pipeline/configure_logging.py
@@ -48,8 +48,8 @@ def configure_logging(logger_name, level=None, format=None, extra_modules=None):
 
     log_directory = get_log_directory()
     logfile = os.path.join(
-        log_directory, '{0}.log'.format(logger_name)
-    ).encode('utf8')
+        log_directory, '{0}.log'.format(logger_name).encode('utf8')
+    )
 
     # Sanitise the variable, checking the type.
     if not isinstance(extra_modules, (list, tuple, type(None))):
@@ -73,7 +73,7 @@ def configure_logging(logger_name, level=None, format=None, extra_modules=None):
         'handlers': {
             'console': {
                 'class': 'logging.StreamHandler',
-                'level': logging._levelNames[level],
+                'level': logging.getLevelName(level),
                 'formatter': 'file',
                 'stream': 'ext://sys.stdout',
             },
@@ -102,7 +102,7 @@ def configure_logging(logger_name, level=None, format=None, extra_modules=None):
     }
 
     for module in modules:
-        current_level = logging._levelNames[level]
+        current_level = logging.getLevelName(level)
         logging_settings['loggers'].setdefault(
             module, {'level': current_level}
         )
