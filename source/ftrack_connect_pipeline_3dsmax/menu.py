e56dc0b03d81c38c59160205b4cbc3a046ffad48
Lluis Casals <lluis.casals@ftrack.com>
2020-11-10 14:38:44 +0100

    fix open dialog for previous versions of 3dsmax

--- source/ftrack_connect_pipeline_3dsmax/menu.py
+++ source/ftrack_connect_pipeline_3dsmax/menu.py
@@ -1,18 +1,22 @@
 # :coding: utf-8
 # :copyright: Copyright (c) 2014-2020 ftrack
 
-
-import logging
-import functools
-import shiboken2
 from Qt import QtWidgets
 
-from pymxs import runtime as rt
-
 try:
-    import MaxPlus
+    # https://help.autodesk.com/view/3DSMAX/2020/ENU/?guid=__developer_creating_python_uis_html
+    #This works for max >= 2020
+    import shiboken2
+    MAX_MIN_VERSION = '2020'
 except ImportError:
-    pass
+    # MaxPlus is the recommended way to use in max <= 2019 even that we could
+    # use the following: from PySide2 import shiboken2 #but seems to be slower.
+    # https://help.autodesk.com/view/3DSMAX/2019/ENU/?guid=__developer_using_pyside_html
+    # This works for max <= 2020
+    import MaxPlus
+    MAX_MIN_VERSION = '2019'
+
+from pymxs import runtime as rt
 
 created_dialogs = dict()
 
@@ -31,15 +35,14 @@ class OpenDialog(object):
         dialog_name = dialog_class
 
         if dialog_name not in created_dialogs:
-            #This is working on 2020 but I hit the simbol error on getting the context twice
-            # https://help.autodesk.com/view/3DSMAX/2020/ENU/?guid=__developer_creating_python_uis_html
-            main_window_qwdgt = QtWidgets.QWidget.find(rt.windows.getMAXHWND())
-            main_window = shiboken2.wrapInstance(
-                shiboken2.getCppPointer(main_window_qwdgt)[0],
-                QtWidgets.QMainWindow
-            )
-            # # TODO: mantain this to be able to create the dialog on versions < 2020
-            # main_window = MaxPlus.GetQMaxMainWindow()
+            if MAX_MIN_VERSION == '2020':
+                main_window_qwdgt = QtWidgets.QWidget.find(rt.windows.getMAXHWND())
+                main_window = shiboken2.wrapInstance(
+                    shiboken2.getCppPointer(main_window_qwdgt)[0],
+                    QtWidgets.QMainWindow
+                )
+            else:
+                main_window = MaxPlus.GetQMaxMainWindow()
             ftrack_dialog = dialog_class
             created_dialogs[dialog_name] = ftrack_dialog(
                 event_manager, parent=main_window
