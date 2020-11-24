ad77751f3c750647e2a69f25bebf48964e07ce4b
lorenzo <lorenzo.angeli@ftrack.com>
2020-11-24 16:53:01 +0100

    refactor hook

--- hook/listen_maya_launch.py
+++ hook/listen_maya_launch.py
@@ -2,101 +2,69 @@
 # :copyright: Copyright (c) 2016 ftrack
 
 import os
+import sys
 import ftrack_api
-import ftrack_connect.application
 import logging
 from functools import partial
 
-logger = logging.getLogger('ftrack_connect_pipeline_maya.listen_maya_launch')
 
-plugin_base_dir = os.path.normpath(
-    os.path.join(
-        os.path.abspath(
-            os.path.dirname(__file__)
-        ),
-        '..'
-    )
-)
-
-maya_connect_plugins_path = os.path.join(
-    plugin_base_dir, 'resource', 'plug_ins'
-)
-
-maya_script_path = os.path.join(
-    plugin_base_dir, 'resource', 'scripts'
-)
-
-python_dependencies = os.path.join(
-    plugin_base_dir, 'dependencies'
-)
+logger = logging.getLogger('ftrack_connect_pipeline_maya.discover')
 
 
 def on_application_launch(session, event):
     '''Handle application launch and add environment to *event*.'''
 
-    logger.debug('Adding maya pipeline environments.')
+
+    plugin_base_dir = os.path.normpath(
+        os.path.join(
+            os.path.abspath(
+                os.path.dirname(__file__)
+            ),
+            '..'
+        )
+    )
+
+    maya_plugins_path = os.path.join(
+        plugin_base_dir, 'resource', 'plug_ins'
+    )
+
+    maya_script_path = os.path.join(
+        plugin_base_dir, 'resource', 'scripts'
+    )
+
+    python_dependencies = os.path.join(
+        plugin_base_dir, 'dependencies'
+    )
+    sys.path.append(python_dependencies)
 
     # logger.debug('Adding ftrackShotId')
     entity = event['data']['context']['selection'][0]
-    task = session.get('Task', entity['entityId'])
-    taskParent = task['parent']
+    task = session.get('Context', entity['entityId'])
 
-    try:
-        event['data']['options']['env']['FS'] = str(
-            taskParent['custom_attributes'].get('fstart', '1')
-        )
-    except Exception:
-        event['data']['options']['env']['FS'] = '1'
-
-    try:
-        event['data']['options']['env']['FE'] = str(
-            taskParent['custom_attributes'].get('fend', '1')
-        )
-    except Exception:
-        event['data']['options']['env']['FE'] = '1'
-
-    event['data']['options']['env']['FTRACK_CONTEXTID'] = task['id']
-
-    # Add dependencies in pythonpath
-    ftrack_connect.application.prependPath(
-        python_dependencies,
-        'PYTHONPATH',
-        event['data']['options']['env']
-    )
-
-    # Maya scripts
-    ftrack_connect.application.prependPath(
-        maya_script_path,
-        'PYTHONPATH',
-        event['data']['options']['env']
-    )
-
-    ftrack_connect.application.prependPath(
-        maya_script_path,
-        'MAYA_SCRIPT_PATH',
-        event['data']['options']['env']
-    )
-
-    # Maya plugins
-    ftrack_connect.application.prependPath(
-        maya_connect_plugins_path,
-        'MAYA_PLUG_IN_PATH',
-        event['data']['options']['env']
-    )
-
-    # Discover plugins from definitions
+    # # Discover plugins from definitions
     definitions_plugin_hook = event['data']['options']['env'].get(
         'FTRACK_DEFINITION_PLUGIN_PATH'
     )
     plugin_hook = os.path.join(definitions_plugin_hook, 'maya')
-    # Add plugins to events path.
-    ftrack_connect.application.appendPath(
-        plugin_hook,
-        'FTRACK_EVENT_PLUGIN_PATH',
-        event['data']['options']['env']
-    )
 
-    return event['data']['options']
+    from ftrack_connect_pipeline_maya import _version as integration_version
+
+    data = {
+        'integration': {
+            "name": 'ftrack-connect-maya',
+            'version': integration_version.__version__,
+            'env': {
+                'PYTHONPATH.prepend': os.path.pathsep.join([python_dependencies, maya_script_path]),
+                'MAYA_SCRIPT_PATH': maya_script_path,
+                'MAYA_PLUG_IN_PATH.prepend': maya_plugins_path,
+                'FTRACK_CONTEXTID.set': task['id'],
+                'FTRACK_EVENT_PLUGIN_PATH.prependPath': plugin_hook,
+                'FS.set': task['parent']['custom_attributes'].get('fstart', '1.0'),
+                'FE.set': task['parent']['custom_attributes'].get('fend', '100.0')
+            }
+        }
+    }
+    return data
 
 
 def register(session):
