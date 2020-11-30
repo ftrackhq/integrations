6e82aa13fb212777c5610c8eaceff713aeea6b8a
lorenzo <lorenzo.angeli@ftrack.com>
2020-11-30 12:41:39 +0100

    rename module remove usage module and its use refactor hook

--- source/ftrack_connect_pipeline_nuke/plugin/__init__.py
+++ source/ftrack_connect_pipeline_nuke/plugin/__init__.py
@@ -1,7 +1,7 @@
 # :coding: utf-8
 # :copyright: Copyright (c) 2019 ftrack
 
-
+import nuke
 from ftrack_connect_pipeline import plugin
 from ftrack_connect_pipeline_qt import plugin as pluginWidget
 from ftrack_connect_pipeline_nuke import constants as nuke_constants
@@ -9,11 +9,6 @@ from ftrack_connect_pipeline_nuke import constants as nuke_constants
 
 class BaseNukePlugin(plugin.BasePlugin):
     host = nuke_constants.HOST
-
-    def __init__(self, session):
-        super(BaseNukePlugin, self).__init__(session)
-        import nuke
-        self.nuke = nuke
 
     def _run(self, event):
         super_fn = super(BaseNukePlugin, self)._run
