a98f2ae85b9938f2562fbe472944ebd72af105b3
henriknorin <henrik.norin@ftrack.com>
2021-03-24 13:37:33 +0100

    Removed clique dep

--- setup.py
+++ setup.py
@@ -132,7 +132,6 @@ setup(
         'sphinx >= 1.2.2, < 2',
         'sphinx_rtd_theme >= 0.1.6, < 2',
         'lowdown >= 0.1.0, < 2',
-        'clique'
     ],
     install_requires=[
         'ftrack-python-api',
