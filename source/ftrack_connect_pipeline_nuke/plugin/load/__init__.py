c8bc1963c43bb95dfe21466b8274a8ff8f14f8c3
Lluis Casals <lluis.casals@ftrack.com>
2021-03-29 11:59:19 +0200

    finalizers added and importer modified

--- source/ftrack_connect_pipeline_nuke/plugin/load/__init__.py
+++ source/ftrack_connect_pipeline_nuke/plugin/load/__init__.py
@@ -4,5 +4,7 @@
 from ftrack_connect_pipeline_nuke.plugin.load.collector import *
 from ftrack_connect_pipeline_nuke.plugin.load.context import *
 from ftrack_connect_pipeline_nuke.plugin.load.finalizer import *
+from ftrack_connect_pipeline_nuke.plugin.load.pre_finalizer import *
+from ftrack_connect_pipeline_nuke.plugin.load.post_finalizer import *
 from ftrack_connect_pipeline_nuke.plugin.load.importer import *
 from ftrack_connect_pipeline_nuke.plugin.load.post_import import *
