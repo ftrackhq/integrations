4925e5b7729aeb3fa637ac6dadd9672f5d0e3df8
Lluis Casals <lluis.casals@ftrack.com>
2021-03-29 11:58:17 +0200

    finalizers added and importer modified

--- source/ftrack_connect_pipeline_3dsmax/plugin/load/__init__.py
+++ source/ftrack_connect_pipeline_3dsmax/plugin/load/__init__.py
@@ -4,5 +4,7 @@
 from ftrack_connect_pipeline_3dsmax.plugin.load.collector import *
 from ftrack_connect_pipeline_3dsmax.plugin.load.context import *
 from ftrack_connect_pipeline_3dsmax.plugin.load.finalizer import *
+from ftrack_connect_pipeline_3dsmax.plugin.load.pre_finalizer import *
+from ftrack_connect_pipeline_3dsmax.plugin.load.post_finalizer import *
 from ftrack_connect_pipeline_3dsmax.plugin.load.importer import *
 from ftrack_connect_pipeline_3dsmax.plugin.load.post_import import *
---
+++ source/ftrack_connect_pipeline_3dsmax/plugin/load/post_finalizer.py
@@ -0,0 +1,32 @@
+# :coding: utf-8
+# :copyright: Copyright (c) 2019 ftrack
+
+from ftrack_connect_pipeline import plugin
+from ftrack_connect_pipeline_qt import plugin as pluginWidget
+from ftrack_connect_pipeline_3dsmax.plugin import (
+    BaseMaxPlugin, BaseMaxPluginWidget
+)
+
+
+class LoaderPostFinalizerMaxPlugin(plugin.LoaderPostFinalizerPlugin, BaseMaxPlugin):
+    ''' Class representing a Post Finalizer Plugin
+
+        .. note::
+
+            _required_output is a dictionary containing the 'context_id',
+            'asset_name', 'asset_type', 'comment' and 'status_id' of the
+            current asset
+    '''
+
+
+class LoaderPostFinalizerMaxWidget(
+    pluginWidget.LoaderPostFinalizerWidget, BaseMaxPluginWidget
+):
+    ''' Class representing a Post Finalizer Widget
+
+        .. note::
+
+            _required_output is a dictionary containing the 'context_id',
+            'asset_name', 'asset_type', 'comment' and 'status_id' of the
+            current asset
+    '''
