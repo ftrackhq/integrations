684130854efcfb44af3bb2ff2526598181d17fe9
Lluis Casals <lluis.casals@ftrack.com>
2021-03-31 14:44:06 +0200

    fixed asset manager plugin type and category of the widgets

--- source/ftrack_connect_pipeline_nuke/host/engine/asset_manager.py
+++ source/ftrack_connect_pipeline_nuke/host/engine/asset_manager.py
@@ -32,13 +32,9 @@ class NukeAssetManagerEngine(AssetManagerEngine):
         result = []
         message = None
 
-        #TODO: we should fix plugin_type in all the assset manager methods,
-        # to be able to run without plugin type if there isn't any plugin and in
-        # case we have a plugin, then run with asset_manager.action plugin type.
-
         result_data = {
             'plugin_name': None,
-            'plugin_type': 'action',
+            'plugin_type': constants.PLUGIN_AM_ACTION_TYPE,
             'method': 'discover_assets',
             'status': status,
             'result': result,
@@ -92,7 +88,7 @@ class NukeAssetManagerEngine(AssetManagerEngine):
         result = []
         message = None
 
-        plugin_type = None
+        plugin_type = constants.PLUGIN_AM_ACTION_TYPE
         plugin_name = None
         if plugin:
             plugin_type = '{}.{}'.format('asset_manager', plugin['type'])
@@ -100,7 +96,7 @@ class NukeAssetManagerEngine(AssetManagerEngine):
 
         result_data = {
             'plugin_name': plugin_name,
-            'plugin_type': 'action',
+            'plugin_type': plugin_type,
             'method': 'remove_asset',
             'status': status,
             'result': result,
@@ -212,7 +208,7 @@ class NukeAssetManagerEngine(AssetManagerEngine):
         result = []
         message = None
 
-        plugin_type = None
+        plugin_type = constants.PLUGIN_AM_ACTION_TYPE
         plugin_name = None
         if plugin:
             plugin_type = '{}.{}'.format('asset_manager', plugin['type'])
@@ -220,7 +216,7 @@ class NukeAssetManagerEngine(AssetManagerEngine):
 
         result_data = {
             'plugin_name': plugin_name,
-            'plugin_type': 'action',
+            'plugin_type': plugin_type,
             'method': 'select_asset',
             'status': status,
             'result': result,
