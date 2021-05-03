c9ed2cb757abca1c21ad291b6b2c6d1d26fddc1c
lorenzo <lorenzo.angeli@ftrack.com>
2021-05-03 12:09:58 +0200

    reverse version limits

--- resource/hook/discover_integration.py
+++ resource/hook/discover_integration.py
@@ -57,13 +57,13 @@ def register(session, **kw):
     session.event_hub.subscribe(
         'topic=ftrack.connect.application.launch'
         ' and (data.application.identifier=nuke-studio* or data.application.identifier=hiero*)'
-        ' and data.application.version < 13',
+        ' and data.application.version >= 13',
         handle_event
     )
 
     session.event_hub.subscribe(
         'topic=ftrack.connect.application.discover'
         ' and (data.application.identifier=nuke-studio* or data.application.identifier=hiero*)'
-        ' and data.application.version < 13',
+        ' and data.application.version >= 13',
         handle_event
     )
