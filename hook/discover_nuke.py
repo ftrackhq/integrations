4ef8edae84472b8c74fb2bedc12e3fb86ade3dd3
Lorenzo Angeli <lorenzo.angeli@ftrack.com>
2021-06-14 17:26:47 +0200

    refactor hook

--- hook/discover_nuke.py
+++ hook/discover_nuke.py
@@ -5,63 +5,61 @@ import os
 import sys
 import ftrack_api
 import logging
-from functools import partial
+import functools
 
 logger = logging.getLogger('ftrack_connect_pipeline_nuke.listen_nuke_launch')
 
-
-def on_application_launch(session, event):
-
-    plugin_base_dir = os.path.normpath(
-        os.path.join(
-            os.path.abspath(
-                os.path.dirname(__file__)
-            ),
-            '..'
-        )
+plugin_base_dir = os.path.normpath(
+    os.path.join(
+        os.path.abspath(
+            os.path.dirname(__file__)
+        ),
+        '..'
     )
+)
 
-    nuke_script_path = os.path.join(
-        plugin_base_dir, 'resource', 'scripts'
-    )
+python_dependencies = os.path.join(
+    plugin_base_dir, 'dependencies'
+)
 
-    python_dependencies = os.path.join(
+sys.path.append(python_dependencies)
 
-        plugin_base_dir, 'dependencies'
-
-    )
-
-    sys.path.append(python_dependencies)
-
-    entity = event['data']['context']['selection'][0]
-
-    task = session.get('Context', entity['entityId'])
-
-    # Discover plugins from definitions
-
-    definitions_plugin_hook = os.getenv("FTRACK_DEFINITION_PLUGIN_PATH")
-
-    plugin_hook = os.path.join(definitions_plugin_hook, 'nuke')
-
-    # from ftrack_connect_pipeline_maya import _version as integration_version
-
+def on_discover_nuke_pipeline(session, event):
+    from ftrack_connect_pipeline_nuke import __version__ as integration_version
 
     data = {
         'integration': {
             "name": 'ftrack-connect-pipeline-nuke',
-            'version': '0.0.0',
-            'env': {
-                'FTRACK_EVENT_PLUGIN_PATH.prepend': plugin_hook,
-                'PYTHONPATH.prepend': os.path.pathsep.join([python_dependencies, nuke_script_path]),
-                'NUKE_PATH': nuke_script_path,
-                'FTRACK_CONTEXTID.set': task['id'],
-                'FS.set': task['parent']['custom_attributes'].get('fstart', '1.0'),
-                'FE.set': task['parent']['custom_attributes'].get('fend', '100.0')
-            }
+            'version': integration_version
         }
     }
 
     return data
+
+def on_launch_nuke_pipeline(session, event):
+    pipeline_nuke_base_data = on_discover_nuke_pipeline(session, event)
+
+    nuke_script_path = os.path.join(plugin_base_dir, 'resource', 'scripts')
+
+    definitions_plugin_hook = os.getenv("FTRACK_DEFINITION_PLUGIN_PATH")
+    plugin_hook = os.path.join(definitions_plugin_hook, 'nuke')
+
+    pipeline_nuke_base_data['integration']['env']  = {
+                'FTRACK_EVENT_PLUGIN_PATH.prepend': plugin_hook,
+                'PYTHONPATH.prepend': os.path.pathsep.join([python_dependencies, nuke_script_path]),
+                'NUKE_PATH': nuke_script_path
+    }
+
+    selection = event['data'].get('context', {}).get('selection', [])
+
+    if selection:
+        task = session.get('Context', selection[0]['entityId'])
+        pipeline_nuke_base_data['integration']['env']['FTRACK_CONTEXTID.set'] =  task['id']
+        pipeline_nuke_base_data['integration']['env']['FS.set'] = task['parent']['custom_attributes'].get('fstart', '1.0')
+        pipeline_nuke_base_data['integration']['env']['FE.set'] = task['parent']['custom_attributes'].get('fend', '100.0')
+        pipeline_nuke_base_data['integration']['env']['FPS.set'] = task['parent']['custom_attributes'].get('fps', '24.0')
+
+    return pipeline_nuke_base_data
 
 
 def register(session):
@@ -69,18 +67,26 @@ def register(session):
     if not isinstance(session, ftrack_api.session.Session):
         return
 
-    handle_event = partial(on_application_launch, session)
+    handle_discovery_event = functools.partial(
+        on_discover_nuke_pipeline,
+        session
+    )
+
+    session.event_hub.subscribe(
+        'topic=ftrack.connect.application.discover'
+        ' and data.application.identifier=nuke*'
+        ' and data.application.version >= 13.0',
+        handle_discovery_event, priority=40
+    )
+
+    handle_launch_event = functools.partial(
+        on_launch_nuke_pipeline,
+        session
+    )    
 
     session.event_hub.subscribe(
         'topic=ftrack.connect.application.launch and '
         'data.application.identifier=nuke*'
         ' and data.application.version >= 13.0',
-        handle_event, priority=40
-
-    )
-    session.event_hub.subscribe(
-        'topic=ftrack.connect.application.discover'
-        ' and data.application.identifier=nuke*'
-        ' and data.application.version >= 13.0',
-        handle_event, priority=40
+        handle_launch_event, priority=40
     )
