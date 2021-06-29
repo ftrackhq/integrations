b89b979270389dfc4254a1e124149ac3d32754b4
Lorenzo Angeli <lorenzo.angeli@ftrack.com>
2021-06-29 12:06:14 +0200

    update setup with setuptools_scm replace pip_main with subprocess

--- setup.py
+++ setup.py
@@ -4,16 +4,15 @@
 
 import os
 import re
+import sys
+import subprocess
 import shutil
+import setuptools_scm
 
 from setuptools import setup, find_packages
 from setuptools.command.test import test as TestCommand
 import setuptools
 
-from pkg_resources import parse_version
-import pip
-
-from pip.__main__ import _main as pip_main
 
 ROOT_PATH = os.path.dirname(os.path.realpath(__file__))
 SOURCE_PATH = os.path.join(ROOT_PATH, 'source')
@@ -28,14 +27,8 @@ BUILD_PATH = os.path.join(
     ROOT_PATH, 'build'
 )
 
-
-# Read version from source.
-with open(os.path.join(
-    SOURCE_PATH, 'ftrack_connect_pipeline', '_version.py')
-) as _version_file:
-    VERSION = re.match(
-        r'.*__version__ = \'(.*?)\'', _version_file.read(), re.DOTALL
-    ).group(1)
+release = setuptools_scm.get_version(version_scheme='post-release')
+VERSION = '.'.join(release.split('.')[:3])
 
 STAGING_PATH = os.path.join(
     BUILD_PATH, 'ftrack-connect-pipeline-{}'.format(VERSION)
@@ -66,11 +59,9 @@ class BuildPlugin(setuptools.Command):
             os.path.join(STAGING_PATH, 'hook')
         )
 
-        pip_main(
+        subprocess.check_call(
             [
-                'install',
-                '.',
-                '--target',
+                sys.executable, '-m', 'pip', 'install','.','--target',
                 os.path.join(STAGING_PATH, 'dependencies')
             ]
         )
@@ -83,11 +74,6 @@ class BuildPlugin(setuptools.Command):
             'zip',
             STAGING_PATH
         )
-
-
-
-
-
 
 
 # Custom commands.
@@ -106,10 +92,17 @@ class PyTest(TestCommand):
         raise SystemExit(errno)
 
 
+version_template = '''
+# :coding: utf-8
+# :copyright: Copyright (c) 2017-2020 ftrack
+
+__version__ = {version!r}
+'''
+
+
 # Configuration.
 setup(
     name='ftrack-connect-pipeline',
-    version=VERSION,
     description='Ftrack core pipeline integration framework.',
     long_description=open(README_PATH).read(),
     keywords='ftrack',
@@ -121,11 +114,18 @@ setup(
     package_dir={
         '': 'source'
     },
+    use_scm_version={
+        'write_to': 'source/ftrack_connect_pipeline/_version.py',
+        'write_to_template': version_template,
+        'version_scheme': 'post-release'
+    },
     python_requires='<3.8',
     setup_requires=[
         'sphinx >= 1.8.5, < 4',
         'sphinx_rtd_theme >= 0.1.6, < 2',
-        'lowdown >= 0.1.0, < 2'
+        'lowdown >= 0.1.0, < 2',
+        'setuptools>=45.0.0',
+        'setuptools_scm'
     ],
     install_requires=[
         'ftrack-python-api >= 1, < 3',
