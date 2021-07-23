1cbc34536dae7c2481c90e4861e4454a5c7ebc53
Lluis Casals <lluis.casals@ftrack.com>
2021-07-23 12:57:30 +0200

    Fix bug on finalizer, correctly cleaning up the paths now

--- source/ftrack_connect_pipeline_nuke/plugin/publish/finalizer.py
+++ source/ftrack_connect_pipeline_nuke/plugin/publish/finalizer.py
@@ -4,6 +4,7 @@ import os
 import re
 import clique
 
+from ftrack_connect_pipeline import constants
 from ftrack_connect_pipeline import plugin
 from ftrack_connect_pipeline_qt import plugin as pluginWidget
 from ftrack_connect_pipeline_nuke.plugin import (
@@ -54,15 +55,21 @@ class PublisherFinalizerNukePlugin(plugin.PublisherFinalizerPlugin, BaseNukePlug
         data = event['data']['settings']['data']
 
         cleanup_files = []
-        for component_name, component_path in data.items():
-            if re.search(
-                    '(?<=\.)((%+\d+d)|(#+)|(%d)|(\d+))(?=\.)', component_path
-            ):
-                sequence_path = clique.parse(component_path)
-                seq_paths = list(sequence_path)
-                cleanup_files.extend(seq_paths)
-            else:
-                cleanup_files.append(component_path)
+        for step in data:
+            if step['type'] == constants.COMPONENT:
+                component_name = step['name']
+                for stage in step['result']:
+                    for plugin in stage['result']:
+                        for component_path in plugin['result']:
+                            if re.search(
+                                    '(?<=\.)((%+\d+d)|(#+)|(%d)|(\d+))(?=\.)',
+                                    component_path
+                            ):
+                                sequence_path = clique.parse(component_path)
+                                seq_paths = list(sequence_path)
+                                cleanup_files.extend(seq_paths)
+                            else:
+                                cleanup_files.append(component_path)
         for path in cleanup_files:
             if os.path.exists(path):
                 os.remove(path)
