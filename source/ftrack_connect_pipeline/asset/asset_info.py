aae6498c35af8f2fe90d0690c0658f9249588d55
Lluis Casals <lluis.casals@ftrack.com>
2021-11-19 12:12:04 +0100

    fix bug on asset info

--- source/ftrack_connect_pipeline/asset/asset_info.py
+++ source/ftrack_connect_pipeline/asset/asset_info.py
@@ -370,7 +370,7 @@ class FtrackAssetInfo(dict):
         string convert it to asset_info. (This maya for example)
         '''
         if not value:
-            return None
+            return value
         new_value = []
         for dependency in value:
             if type(dependency) == str:
