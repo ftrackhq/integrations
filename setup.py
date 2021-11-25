e1bb9f811e5b748620ad6ae3d3d5d36836c1305e
henriknorin <henrik.norin@ftrack.com>
2021-11-25 13:08:09 +0100

    Added qtpy and qtawesome (Material icons support) deps

--- setup.py
+++ setup.py
@@ -230,8 +230,10 @@ setup(
         'sphinx >= 1.8.5, < 4',
         'sphinx_rtd_theme >= 0.1.6, < 2',
         'lowdown >= 0.1.0, < 2',
-        'setuptools>=44.0.0',
-        'setuptools_scm'
+        'setuptools >= 44.0.0',
+        'setuptools_scm',
+        'qtpy',
+        'qtawesome'
     ],
     install_requires=[
         'qt.py >=1.0.0, < 2'
