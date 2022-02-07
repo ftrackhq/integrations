d94d26875dcbf2da6bd090d3968af55542937bb8
Lorenzo Angeli <lorenzo.angeli@ftrack.com>
2022-02-07 12:17:05 +0100

    look into docs update

--- setup.py
+++ setup.py
@@ -100,6 +100,9 @@ setup(
     packages=find_packages(SOURCE_PATH),
     package_dir={'': 'source'},
     setup_requires=[
+        'sphinx >= 1.8.5, < 4',
+        'sphinx_rtd_theme >= 0.1.6, < 2',
+        'lowdown >= 0.1.0, < 2',
         'setuptools>=45.0.0',
         'setuptools_scm'
     ],
