065161f012490cc4ae56e62061b06305147781da
Lluis Ftrack <lluis.casals@ftrack.com>
2022-03-22 12:50:15 +0100

    cleanup code and PARENT_NAME constant added to asset info

--- source/ftrack_connect_pipeline/constants/asset/__init__.py
+++ source/ftrack_connect_pipeline/constants/asset/__init__.py
@@ -2,7 +2,7 @@
 # :copyright: Copyright (c) 2014-2020 ftrack
 
 #: Name of the ftrack object to identify the loaded assets
-FTRACK_OBJECT_NAME = '{}_ftrackdata'
+FTRACK_OBJECT_NAME = '{}_{}_ftrackdata'
 #: LOAD_AS_NODE_ONLY if true when loading an asset only the ftrack node will be
 # loaded in the scene, and the asset will apear as unloaded. If false, the
 # ftrack node will be created as well but the asset will automatically be added
--- source/ftrack_connect_pipeline/constants/asset/v2.py
+++ source/ftrack_connect_pipeline/constants/asset/v2.py
@@ -9,6 +9,10 @@ ASSET_ID = 'asset_id'
 #: :class:`~ftrack_connect_pipeline.asset.asset_info.FtrackAssetInfo`
 #: and the DCC ftrack plugin.
 ASSET_NAME = 'asset_name'
+#: Parent name (Usually AssetBuild) constant identifier key for ftrack assets connected or used with
+#: :class:`~ftrack_connect_pipeline.asset.asset_info.FtrackAssetInfo`
+#: and the DCC ftrack plugin.
+PARENT_NAME = 'parent_name'
 #: Asset type constant identifier key for ftrack assets connected or used with
 #: :class:`~ftrack_connect_pipeline.asset.asset_info.FtrackAssetInfo`
 #: and the DCC ftrack plugin.
@@ -78,6 +82,7 @@ VERSION = '2.0'
 KEYS = [
     ASSET_ID,
     ASSET_NAME,
+    PARENT_NAME,
     ASSET_TYPE_NAME,
     VERSION_ID,
     VERSION_NUMBER,
