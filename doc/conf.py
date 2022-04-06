94daa16ad3fbad58b6cfb510d57cfcedd6e45a74
lorenzo angeli <lorenzo.angeli@ftrack.com>
2022-04-06 15:50:05 +0200

    add requirements fix docs build

--- doc/conf.py
+++ doc/conf.py
@@ -6,6 +6,7 @@
 import os
 import sys
 import re
+from pkg_resources import get_distribution, DistributionNotFound
 
 # -- General ------------------------------------------------------------------
 
@@ -37,19 +38,16 @@ master_doc = 'index'
 project = u'ftrack connect action launcher widget'
 copyright = u'2022, ftrack'
 
-# Version
-with open(
-    os.path.join(
-        os.path.dirname(__file__), '..', 'source',
-        'ftrack_connect_action_launcher_widget', '_version.py'
-    )
-) as _version_file:
-    _version = re.match(
-        r'.*__version__ = \'(.*?)\'', _version_file.read(), re.DOTALL
-    ).group(1)
+try:
+    release = get_distribution('ftrack-connect-action-launcher-widget').version
+    # take major/minor/patch
+    VERSION = '.'.join(release.split('.')[:3])
+except DistributionNotFound:
+    # package is not installed
+    VERSION = 'Unknown version'
 
-version = _version
-release = _version
+version = VERSION
+release = VERSION
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
