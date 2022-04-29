a7ab60ba29bec2a08adc1df12b534a8bda380eca
henriknorin <henrik.norin@ftrack.com>
2022-04-29 12:35:53 +0200

    Inherit Client; Removed Base from name

--- source/ftrack_connect_pipeline_qt/client/webview/__init__.py
+++ source/ftrack_connect_pipeline_qt/client/webview/__init__.py
@@ -8,23 +8,18 @@ from PySide2 import (
 )  # Qt.py does not provide QtWebEngineWidgets
 
 from ftrack_connect_pipeline.utils import get_current_context_id
+from ftrack_connect_pipeline.client import Client
 from ftrack_connect_pipeline_qt.ui.utility.widget import dialog, header
 from ftrack_connect_pipeline_qt.ui import theme
 
 
-class QtWebViewClientBase(dialog.Dialog):
+class QtWebViewClient(Client, dialog.Dialog):
     '''Web widget viewer base'''
 
-    @property
-    def session(self):
-        '''
-        Returns instance of :class:`ftrack_api.session.Session`
-        '''
-        return self._event_manager.session
+    def __init__(self, event_manager, parent=None):
 
-    def __init__(self, event_manger, parent=None):
-        self._event_manager = event_manger
-        super(QtWebViewClientBase, self).__init__(parent=parent)
+        dialog.Dialog.__init__(self, parent=parent)
+        Client.__init__(self, event_manager)
 
         self._context = None
 
@@ -74,13 +69,13 @@ class QtWebViewClientBase(dialog.Dialog):
     def show(self):
         if self._context is None:
             self.set_context_id(get_current_context_id())
-        super(QtWebViewClientBase, self).show()
+        super(QtWebViewClient, self).show()
 
     def get_url(self):
         raise NotImplementedError()
 
 
-class QtInfoWebViewClient(QtWebViewClientBase):
+class QtInfoWebViewClient(QtWebViewClient):
     def __init__(self, event_manger, unused_asset_model, parent=None):
         super(QtInfoWebViewClient, self).__init__(event_manger, parent=parent)
         self.setWindowTitle('Task info')
@@ -89,7 +84,7 @@ class QtInfoWebViewClient(QtWebViewClientBase):
         return self.session.get_widget_url('info', entity=self._context)
 
 
-class QtTasksWebViewClient(QtWebViewClientBase):
+class QtTasksWebViewClient(QtWebViewClient):
     def __init__(self, event_manger, unused_asset_model, parent=None):
         super(QtTasksWebViewClient, self).__init__(event_manger, parent=parent)
         self.setWindowTitle('My Tasks')
