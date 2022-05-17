fa6cfe6a3465ba011877f43d39f77a6a9f65b739
henriknorin <henrik.norin@ftrack.com>
2022-05-17 11:27:17 +0200

    Log viewer client definition_filter set

--- source/ftrack_connect_pipeline/client/log_viewer.py
+++ source/ftrack_connect_pipeline/client/log_viewer.py
@@ -2,6 +2,7 @@
 # :copyright: Copyright (c) 2014-2022 ftrack
 
 from ftrack_connect_pipeline import client
+from ftrack_connect_pipeline import constants as core_constants
 from ftrack_connect_pipeline.constants import asset as asset_const
 
 
@@ -9,6 +10,9 @@ class LogViewerClient(client.Client):
     '''
     Log Viewer Client Base Class
     '''
+
+    definition_filter = core_constants.LOG_VIEWER
+    '''Use only definitions that matches the definition_filter'''
 
     def __init__(self, event_manager):
         '''
