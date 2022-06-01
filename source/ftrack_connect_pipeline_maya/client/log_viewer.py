09ca514d91904f98d032bfa144529cfe24f522d2
Lluis Ftrack <lluis.casals@ftrack.com>
2022-06-01 12:56:05 +0200

    rename classes

--- source/ftrack_connect_pipeline_maya/client/log_viewer.py
+++ source/ftrack_connect_pipeline_maya/client/log_viewer.py
@@ -8,11 +8,11 @@ from ftrack_connect_pipeline_qt.client import log_viewer
 from ftrack_connect_pipeline_maya.utils.custom_commands import get_main_window
 
 
-class MayaLogViewerDialog(log_viewer.QtLogViewerClient):
+class MayaQtLogViewerClientWidget(log_viewer.QtLogViewerClientWidget):
     '''Maya log viewer dialog'''
 
     def __init__(self, event_manager, unused_asset_list_model, parent=None):
-        super(MayaLogViewerDialog, self).__init__(
+        super(MayaQtLogViewerClientWidget, self).__init__(
             event_manager, parent=(parent or get_main_window())
         )
 
@@ -20,5 +20,5 @@ class MayaLogViewerDialog(log_viewer.QtLogViewerClient):
         self.setWindowFlags(QtCore.Qt.Tool)
 
     def show(self):
-        super(MayaLogViewerDialog, self).conditional_rebuild()
-        super(MayaLogViewerDialog, self).show()
+        super(MayaQtLogViewerClientWidget, self).conditional_rebuild()
+        super(MayaQtLogViewerClientWidget, self).show()
--- source/ftrack_connect_pipeline_maya/client/save.py
+++ source/ftrack_connect_pipeline_maya/client/save.py
@@ -1,10 +1,10 @@
 # :coding: utf-8
 # :copyright: Copyright (c) 2014-2022 ftrack
-from ftrack_connect_pipeline_qt.client.save import QtSaveClient
+from ftrack_connect_pipeline_qt.client.save import QtSaveClientWidget
 from ftrack_connect_pipeline_maya.utils import custom_commands as maya_utils
 
 
-class QtMayaSaveClient(QtSaveClient):
+class MayaQtSaveClientWidget(QtSaveClientWidget):
     '''Client for doing an incremental save of Maya scene locally'''
 
     dcc_utils = maya_utils
