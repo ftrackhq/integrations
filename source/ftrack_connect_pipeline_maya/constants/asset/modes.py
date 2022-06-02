25fe25d4fb984b5c21c103d125236567d82fbe34
henrik_norin <henrik.norin@ftrack.com>
2022-06-02 10:16:57 +0200

    Remove open mode

--- source/ftrack_connect_pipeline_maya/constants/asset/modes.py
+++ source/ftrack_connect_pipeline_maya/constants/asset/modes.py
@@ -9,7 +9,6 @@ REFERENCE_MODE = 'Reference'
 OPEN_MODE = 'Open'
 
 LOAD_MODES = {
-    OPEN_MODE: maya_utils.open_file,
     IMPORT_MODE: maya_utils.import_file,
     REFERENCE_MODE: maya_utils.reference_file,
 }
