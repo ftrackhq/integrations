cde0689aee34392cfe7e26ac3c0a0c3661ff6737
henriknorin <henrik.norin@ftrack.com>
2022-07-07 12:07:29 +0200

    Make change context inherit widget so raise works

--- source/ftrack_connect_pipeline_qt/client/change_context/__init__.py
+++ source/ftrack_connect_pipeline_qt/client/change_context/__init__.py
@@ -2,6 +2,8 @@
 # :copyright: Copyright (c) 2014-2022 ftrack
 import logging
 
+from Qt import QtWidgets
+
 from ftrack_connect_pipeline import constants as core_constants
 from ftrack_connect_pipeline.client import Client
 
@@ -13,7 +15,7 @@ from ftrack_connect_pipeline_qt.ui.utility.widget import (
 )
 
 
-class QtChangeContextClientWidget(Client):
+class QtChangeContextClientWidget(Client, QtWidgets.QWidget):
     '''Client for changing the current working context within the host/DCC'''
 
     def __init__(self, event_manager, parent=None):
@@ -23,7 +25,8 @@ class QtChangeContextClientWidget(Client):
         :param event_manager:  :class:`~ftrack_connect_pipeline.event.EventManager` instance
         :param parent: The parent dialog or frame
         '''
-        super(QtChangeContextClientWidget, self).__init__(event_manager)
+        QtWidgets.QWidget.__init__(self, parent=parent)
+        Client.__init__(self, event_manager)
         self.logger = logging.getLogger(
             __name__ + '.' + self.__class__.__name__
         )
