10bf43f60464b33706f6f863aeb8bc17b0092c3c
henrik_norin <henrik.norin@ftrack.com>
2022-11-10 13:50:57 +0100

    Added save function to utils

--- {{cookiecutter.repo_name}}/source/{{cookiecutter.package_name}}/utils/custom_commands.py
+++ {{cookiecutter.repo_name}}/source/{{cookiecutter.package_name}}/utils/custom_commands.py
@@ -3,6 +3,10 @@
 import threading
 from functools import wraps
 
+from ftrack_connect_pipeline.utils import (
+    get_save_path,
+)
+
 def init_{{cookiecutter.host_type}}(context_id=None, session=None):
     '''
     Initialise timeline in {{cookiecutter.host_type_capitalized}} based on shot/asset build settings.
@@ -42,3 +46,34 @@ def import_file(path, options):
 def reference_file(path, options):
     '''Native reference file function '''
     # return cmds.file(path, o=True, f=True)
+
+def save(context_id, session, temp=True, save=True):
+    '''Save scene locally in temp or with the next version number based on latest version
+    in ftrack.'''
+
+    save_path, message = get_save_path(
+        context_id, session, extension='.mb', temp=temp
+    )
+
+    if save_path is None:
+        return (False, message)
+
+    # Save Maya scene to this path
+    #cmds.file(rename=save_path)
+    if save:
+        #cmds.file(save=True)
+        message = 'Saved {{cookiecutter.host_type_capitalized}}  scene @ "{}"'.format(save_path)
+    else:
+        message = 'Renamed {{cookiecutter.host_type_capitalized}}  scene @ "{}"'.format(save_path)
+    #if not temp:
+        # Add to recent files
+        #mm.eval("source addRecentFile;")
+        #mm.eval(
+        #    'addRecentFile("{}.mb","{}");'.format(
+        #        save_path.replace('\\', '/'), 'mayaBinary'
+        #    )
+        #)
+
+    result = save_path
+
+    return result, message
