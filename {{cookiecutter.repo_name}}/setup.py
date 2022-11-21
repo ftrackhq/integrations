ba14eb9b31e9a34d40041995efeaceadf0e3671e
Lluis Ftrack <lluis.casals@ftrack.com>
2022-11-21 10:15:04 +0100

    update readme to md file

--- {{cookiecutter.repo_name}}/setup.py
+++ {{cookiecutter.repo_name}}/setup.py
@@ -15,7 +15,7 @@ import setuptools
 
 ROOT_PATH = os.path.dirname(os.path.realpath(__file__))
 SOURCE_PATH = os.path.join(ROOT_PATH, 'source')
-README_PATH = os.path.join(ROOT_PATH, 'README.rst')
+README_PATH = os.path.join(ROOT_PATH, 'README.md')
 
 RESOURCE_PATH = os.path.join(ROOT_PATH, 'resource')
 
