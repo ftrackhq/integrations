9d347e0623d17d4fa70dccaaff1920ac06645d37
Henrik Norin <henrik.norin@ftrack.com>
2023-03-15 11:14:33 +0100

    Added find_image_sequence to utils

--- source/ftrack_connect_pipeline/utils.py
+++ source/ftrack_connect_pipeline/utils.py
@@ -1,6 +1,7 @@
 # :coding: utf-8
 # :copyright: Copyright (c) 2014-2023 ftrack
 import os
+import clique
 import appdirs
 import tempfile
 import six
@@ -165,3 +166,34 @@ def get_save_path(context_id, session, extension=None, temp=True):
             message = 'Could not evaluate local save path!'
 
     return result, message
+
+
+def find_image_sequence(file_path):
+    '''Try to find a continous image sequence in the *file_path*, supplied either as
+    an explicit single file within the sequence or a folder. Will return the clique
+    parsable expression.
+    '''
+
+    is_dir = False
+    if not file_path or not os.path.exists(os.path.dirname(file_path)):
+        return None
+
+    if os.path.isdir(file_path):
+        is_dir = True
+        folder_path = file_path
+    else:
+        folder_path = os.path.dirname(file_path)
+    # Search folder for images sequence
+    collections, remainder = clique.assemble(
+        os.listdir(folder_path), minimum_items=1
+    )
+    if not collections:
+        return None
+
+    if is_dir:
+        return os.path.join(folder_path, collections[0].format())
+    for collection in collections:
+        if collection.match(os.path.basename(file_path)):
+            return os.path.join(folder_path, collection.format())
+
+    return None
