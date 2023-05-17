990622beafe237a1c452bb3712f9ac6fcb57249d
henriknorin-ftrack <henrik.norin@backlight.co>
2023-05-17 14:28:54 +0200

    Test build matrix

--- projects/framework-unreal/setup.py
+++ projects/framework-unreal/setup.py
@@ -99,12 +99,14 @@ class PyTest(TestCommand):
         raise SystemExit(errno)
 
 
-version_template = '''
-# :coding: utf-8
-# :copyright: Copyright (c) 2014-2023 ftrack
-
-__version__ = {version!r}
-'''
+def get_version():
+    '''Expect version written to source/framework_core/_version.py'''
+    version_path = os.path.join(SOURCE_PATH, 'framework_unreal', '_version.py')
+    with open(version_path, 'r') as file_handle:
+        for line in file_handle.readlines():
+            if line.find('__version__') > -1:
+                return re.findall(r'\'(.*)\'', line)[0].strip()
+    raise ValueError('Could not find version in {0}'.format(version_path))
 
 # Configuration.
 setup(
@@ -119,7 +121,7 @@ setup(
     packages=find_packages(SOURCE_PATH),
     package_dir={'': 'source'},
     package_data={"": ["{}/**/*.*".format(RESOURCE_PATH), "{}/**/*.py".format(HOOK_PATH)]},
-    version="1.1.0b1",
+    version=get_version(),
     python_requires='<3.10',
     setup_requires=[
         'sphinx >= 1.8.5, < 4',
