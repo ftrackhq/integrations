4b6f7add552c5c5c30923fe00c438a25d6f51670
henriknorin-ftrack <henrik.norin@backlight.co>
2023-05-31 11:11:53 +0200

    Add wheels to NKS setup requires

--- projects/nuke-studio/setup.py
+++ projects/nuke-studio/setup.py
@@ -56,7 +56,8 @@ SETUP_REQUIRES = [
     'sphinx >= 1.8.5, < 4',
     'sphinx_rtd_theme >= 0.1.6, < 1',
     'lowdown >= 0.1.0, < 1',
-    'setuptools>=45.0.0'
+    'setuptools>=45.0.0',
+    'wheel'
 ]
 
 
