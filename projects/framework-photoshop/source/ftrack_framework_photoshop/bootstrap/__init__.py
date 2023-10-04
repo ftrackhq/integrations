03cd0619277d72ac2901e1c1c88e9b5e8aaa80b6
Lluis Ftrack <lluis.casals@ftrack.com>
2023-10-04 10:12:57 +0200

    cleanup bootsrap

--- projects/framework-photoshop/source/ftrack_framework_photoshop/bootstrap/__init__.py
+++ projects/framework-photoshop/source/ftrack_framework_photoshop/bootstrap/__init__.py
@@ -3,52 +3,28 @@
 
 import os
 import logging
-import time
 
 from ftrack_framework_core.configure_logging import configure_logging
 
 
 configure_logging(
     'ftrack_framework_photoshop',
+    extra_modules=["ftrack_qt"],
     propagate=False,
 )
 
+
 logger = logging.getLogger('ftrack_framework_photoshop.bootstrap')
 
-use_uxp = (os.environ.get('FTRACK_PHOTOSHOP_UXP') or '').lower() in [
-    'true',
-    '1',
-]
+photoshop_session_id = os.environ.get('FTRACK_INTEGRATION_SESSION_ID')
+assert (
+    photoshop_session_id
+), 'Photoshop integration requires a FTRACK_INTEGRATION_SESSION_ID passed as environment variable!'
 
-# Init QApplication
-if use_uxp:
-    from ftrack_framework_photoshop.app.uxp_app import UXPPhotoshopApplication
+photoshop_version = os.environ.get('FTRACK_PHOTOSHOP_VERSION')
+assert (
+    photoshop_version
+), 'Photoshop integration requires FTRACK_PHOTOSHOP_VERSION passed as environment variable!'
 
-    app = UXPPhotoshopApplication()
-else:
-    from ftrack_framework_photoshop.app.cep_app import CEPPhotoshopApplication
-
-    app = CEPPhotoshopApplication()
-
-# Run until it's closed, or CTRL+C
-active_time = 0
-
-while True:
-    app.processEvents()
-    time.sleep(0.01)
-    active_time += 10
-    # Failsafe check if PS is still alive
-    if active_time % 1000 == 0:
-        print('.', end='', flush=True)
-    if active_time % (60 * 1000) == 0:
-        # Check if Photoshop still is with us
-        if not app.check_responding():
-            if not app.check_alive():
-                logger.warning(
-                    'Photoshop is not responding and process gone, shutting down!'
-                )
-                app.terminate()
-            else:
-                logger.warning(
-                    'Photoshop is not responding but process is still there, panel temporarily closed?'
-                )
+# TODO: Spawn standalone QApplication
+raise Exception('Photoshop CEP integration not implemented yet!')
