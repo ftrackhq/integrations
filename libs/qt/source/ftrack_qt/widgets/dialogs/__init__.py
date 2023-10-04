779f6bb9161f32350de7a01e419ca71ee0395590
Lluis Ftrack <lluis.casals@ftrack.com>
2023-10-04 12:12:55 +0200

    WIP

--- libs/qt/source/ftrack_qt/widgets/dialogs/__init__.py
+++ libs/qt/source/ftrack_qt/widgets/dialogs/__init__.py
@@ -4,3 +4,6 @@ from ftrack_qt.widgets.dialogs.file_dialog import FileDialog
 from ftrack_qt.widgets.dialogs.scroll_tool_configs_dialog import (
     ScrollToolConfigsDialog,
 )
+from ftrack_qt.widgets.dialogs.tab_tool_configs_dialog import (
+    TabConfigsDialog,
+)
