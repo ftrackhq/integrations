825075636c72c1bda071d22360785d782c2eca6f
henriknorin-ftrack <henrik.norin@backlight.co>
2023-10-18 17:02:00 +0200

    Updated get_version to support running from sources

--- projects/connect-publisher-widget/source/ftrack_connect_publisher_widget/_version.py
+++ projects/connect-publisher-widget/source/ftrack_connect_publisher_widget/_version.py
@@ -9,12 +9,22 @@ def get_version():
     try:
         from importlib.metadata import version
 
-        return version(package_name)
+        result = version(package_name)
     except ImportError:
         # Running on pre-3.8 Python; use importlib-metadata package
         from pkg_resources import get_distribution
 
-        return get_distribution(package_name).version
+        result = get_distribution(package_name).version
+    if result == "0.0.0":
+        # Running from sources, fetch version from pyproject.toml
+        import toml
+
+        path_package = os.path.join(
+            os.path.dirname(os.path.dirname(os.path.dirname(__file__))),
+            "pyproject.toml",
+        )
+        version = toml.load(path_package)["tool"]["poetry"]["version"]
+    return version
 
 
 __version__ = get_version()
