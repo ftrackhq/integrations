5165b44de8e46dadbb3e009d1b9503751491bdd2
Lluis Ftrack <lluis.casals@ftrack.com>
2023-10-20 14:58:01 +0200

    remove black strings

--- libs/qt/source/ftrack_qt/utils/widget/__init__.py
+++ libs/qt/source/ftrack_qt/utils/widget/__init__.py
@@ -9,8 +9,8 @@ from Qt import QtWidgets, QtCore, QtGui
 
 
 def find_parent(widget, class_name):
-    """Recursively find upstream widget having class name
-    containing *class_name*"""
+    '''Recursively find upstream widget having class name
+    containing *class_name*'''
     parent_widget = widget.parentWidget()
     if not parent_widget:
         return
@@ -20,8 +20,8 @@ def find_parent(widget, class_name):
 
 
 def get_main_window_from_widget(widget, class_name):
-    """This function will return the main window of the framework from the
-    given *widget*. The main window is named as main_framework_widget"""
+    '''This function will return the main window of the framework from the
+    given *widget*. The main window is named as main_framework_widget'''
     main_window = widget.window()
     if not main_window:
         return
@@ -34,7 +34,7 @@ def get_main_window_from_widget(widget, class_name):
 
 
 def set_property(widget, name, value):
-    """Update property *name* to *value* for *widget*, and polish afterwards."""
+    '''Update property *name* to *value* for *widget*, and polish afterwards.'''
     widget.setProperty(name, value)
     if widget.style() is not None and shiboken2.isValid(
         widget.style()
@@ -45,7 +45,7 @@ def set_property(widget, name, value):
 
 
 def center_widget(widget, width=None, height=None):
-    """Returns a widget that is *widget* centered horizontally and vertically"""
+    '''Returns a widget that is *widget* centered horizontally and vertically'''
     v_container = QtWidgets.QWidget()
     v_container.setLayout(QtWidgets.QVBoxLayout())
     v_container.layout().addWidget(QtWidgets.QLabel(""), 100)
@@ -65,15 +65,15 @@ def center_widget(widget, width=None, height=None):
 
 
 class InputEventBlockingWidget(QtWidgets.QWidget):
-    """Conditional input event blocking widget"""
+    '''Conditional input event blocking widget'''
 
     def __init__(self, blocker, parent=None):
-        """
+        '''
         Initialize InputEventBlockingWidget
 
         :param blocker: Blocker function that should return true if event should be blocked
         :param parent: The parent dialog or frame
-        """
+        '''
         super(InputEventBlockingWidget, self).__init__(parent=parent)
         self._blocker = blocker
         self._filtered_widget = QtCore.QCoreApplication.instance()
@@ -83,7 +83,7 @@ class InputEventBlockingWidget(QtWidgets.QWidget):
         self._filtered_widget.removeEventFilter(self)
 
     def eventFilter(self, obj, event):
-        """Block *event* on *obj* while if blocker returns True"""
+        '''Block *event* on *obj* while if blocker returns True'''
         retval = False
         if self._blocker() and (isinstance(event, QtGui.QInputEvent)):
             retval = True
