1cea339ddade04de2dac9190244b9942dd3883da
Lluis Casals Marsol <112543804+lluisFtrack@users.noreply.github.com>
2023-10-23 12:52:43 +0000

    fix: exception added (#198)

--- libs/qt/source/ftrack_qt/__init__.py
+++ libs/qt/source/ftrack_qt/__init__.py
@@ -13,15 +13,20 @@ try:
         os.path.dirname(os.path.dirname(os.path.dirname(__file__))),
     )
 except Exception:
-    __version__ = '0.0.0'
+    __version__ = "0.0.0"
 
 logger = logging.getLogger(__name__)
-logger.debug('v{}'.format(__version__))
+logger.debug("v{}".format(__version__))
 
 # DO NOT REMOVE UNUSED IMPORT - important to keep this in order to have resources
 # initialised properly for applying style and providing images & fonts.
-from ftrack_qt_style import (
-    resource,
-)
+try:
+    from ftrack_qt_style import (
+        resource,
+    )
+except Exception:
+    logger.exception(
+        "Styling resource file not found, please install ftrack-qt-style library"
+    )
 
 # TODO: review all the code inside this library, we should standarize docstrings.
