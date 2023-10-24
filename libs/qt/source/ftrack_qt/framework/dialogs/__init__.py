749d244d24bd1ff48546c874aecea930f72297d3
Lluis Ftrack <lluis.casals@ftrack.com>
2023-10-24 13:50:50 +0200

    publisher base created but not discovering it

---
+++ libs/qt/source/ftrack_qt/framework/dialogs/__init__.py
@@ -0,0 +1,5 @@
+# :coding: utf-8
+# :copyright: Copyright (c) 2014-2023 ftrack.
+
+from ftrack_qt.framework.dialogs.publisher_dialog import PublisherDialog
+from ftrack_qt.framework.dialogs.host_selector_dialog import HostSelectorDialog
