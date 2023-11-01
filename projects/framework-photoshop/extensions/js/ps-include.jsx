4c3337edff528082a324955fcac732fb8de16c8a
Lluis Casals Marsol <112543804+lluisFtrack@users.noreply.github.com>
2023-11-01 07:33:05 +0000

    feat: Extensions moved (#215)

---
+++ projects/framework-photoshop/extensions/js/ps-include.jsx
@@ -0,0 +1,13 @@
+/*
+ftrack Photoshop Framework integration CEP interface include functions
+
+Exposed by RPC call event to Python standalone host.
+
+Copyright (c) 2014-2023 ftrack
+*/
+
+// Add additional functions here
+
+/*function helloWorld(output_path, format) {
+    return "hello world";
+}*/
