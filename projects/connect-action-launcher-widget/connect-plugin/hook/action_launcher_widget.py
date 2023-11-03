b5c969386e25cde543bc9cf5bacfe15e52c93d2e
Henrik Norin <112491360+henriknorin-ftrack@users.noreply.github.com>
2023-11-03 10:04:33 +0100

    feat: Backlog/action launcher widget poetry (#216)
    Co-authored-by: Lluis Ftrack <lluis.casals@ftrack.com>

--- projects/connect-action-launcher-widget/connect-plugin/hook/action_launcher_widget.py
+++ projects/connect-action-launcher-widget/connect-plugin/hook/action_launcher_widget.py
@@ -5,17 +5,22 @@ import os
 import sys
 import logging
 
-logger = logging.getLogger('ftrack-connect.widget.ActionLauncherWidget')
-
-cwd = os.path.dirname(__file__)
-sources = os.path.abspath(os.path.join(cwd, '..', 'dependencies'))
-sys.path.append(sources)
-
-import platform
 import ftrack_api
 from ftrack_connect.qt import QtWidgets, QtCore, QtGui
-import qtawesome as qta
 import ftrack_connect.ui.application
+from ftrack_connect.util import get_connect_plugin_version
+
+logger = logging.getLogger(__name__)
+
+cwd = os.path.dirname(__file__)
+connect_plugin_path = os.path.abspath(os.path.join(cwd, '..'))
+
+# Read version number from __version__.py
+__version__ = get_connect_plugin_version(connect_plugin_path)
+
+sources = os.path.abspath(os.path.join(connect_plugin_path, 'dependencies'))
+sys.path.append(sources)
+
 
 from ftrack_connect_action_launcher_widget.actions import Actions
 
@@ -33,8 +38,13 @@ class ActionLauncherWidget(ftrack_connect.ui.application.ConnectWidget):
         layout.addWidget(self.actionsView)
 
 
+def get_version_information(event):
+    '''Return version information for ftrack connect plugin.'''
+    return [dict(name='connect-action-launcher-widget', version=__version__)]
+
+
 def register(session, **kw):
-    '''Register plugin. Called when used as an plugin.'''
+    '''Register plugin. Called when used as a plugin.'''
     # Validate that session is an instance of ftrack_api.Session. If not,
     # assume that register is being called from an old or incompatible API and
     # return without doing anything.
@@ -50,3 +60,10 @@ def register(session, **kw):
         ActionLauncherWidget
     )
     plugin.register(session, priority=20)
+
+    # Enable plugin info in Connect about dialog
+    session.event_hub.subscribe(
+        'topic=ftrack.connect.plugin.debug-information',
+        get_version_information,
+        priority=20,
+    )
