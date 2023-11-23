91f3d3d75c6cb3e91e31fd905ab9d5fef683a039
Henrik Norin <112491360+henriknorin-ftrack@users.noreply.github.com>
2023-11-23 12:53:08 +0100

    fix: Backlog/fix connect installer build (#263)

---
+++ apps/connect/source/ftrack_connect/hook/__init__.py
@@ -0,0 +1,9 @@
+# :coding: utf-8
+# :copyright: Copyright (c) 2014-2023 ftrack
+import os
+
+# Hooks use the ftrack event system. Set the FTRACK_EVENT_PLUGIN_PATH
+# to pick up the default hooks if it has not already been set.
+os.environ.setdefault(
+    'FTRACK_EVENT_PLUGIN_PATH', os.path.realpath(os.path.dirname(__file__))
+)
--- installers/connect-installer/doc/release.rst
+++ installers/connect-installer/doc/release.rst
@@ -5,6 +5,12 @@
 Releases
 ########
 
+.. release:: upcoming
+
+    .. change:: fixed
+
+        Aligned setuptools build with Connect Poetry build.
+
 .. release:: 2.1.1
     :date: 2023-04-27
 
