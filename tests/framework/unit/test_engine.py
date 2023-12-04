b7cb7f1036144cbe53e11646303b5cc68af7bf97
Lluis Ftrack <lluis.casals@ftrack.com>
2023-12-04 11:00:18 +0100

    Move local dep discovery to init file

--- tests/framework/unit/test_engine.py
+++ tests/framework/unit/test_engine.py
@@ -1,29 +1,4 @@
 # test_standard_engine.py
-import sys
-import os
-
-ROOT_INTEGRATIONS_FOLDER = os.path.dirname(
-    os.path.dirname(
-        os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    )
-)
-
-INCLUDE_PACKAGES = [
-    'libs/constants',
-    'libs/framework-core',
-    'libs/framework-engine',
-    'libs/framework-plugin',
-    # QT
-    'libs/framework-widget',
-    'libs/framework-qt',
-    'libs/qt',
-    'libs/qt-style',
-    'libs/utils',
-]
-
-for _package in INCLUDE_PACKAGES:
-    sys.path.append(os.path.join(ROOT_INTEGRATIONS_FOLDER, _package, "source"))
-
 from ftrack_framework_core import registry
 
 import pytest
