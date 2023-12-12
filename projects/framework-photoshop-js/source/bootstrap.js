1e2c211deef7e73fb3bbf2a7666c136d3c6d6987
Henrik Norin <112491360+henriknorin-ftrack@users.noreply.github.com>
2023-12-12 10:05:16 +0100

    feat: Backlog/photoshop tools (#272)
    Co-authored-by: Lluis Casals Marsol <112543804+lluisFtrack@users.noreply.github.com>
    Co-authored-by: Mattias Lagergren <mattias.lagergren@ftrack.com>

--- projects/framework-photoshop-js/source/bootstrap.js
+++ projects/framework-photoshop-js/source/bootstrap.js
@@ -13,15 +13,23 @@ var panel_launchers;
 var context_id = undefined;
 var project_id = undefined;
 
-try {
-    function jsx_callback(){
-        console.log("ps.jsx loaded");
-    }
+function jsx_callback(){
+    console.log("ps.jsx loaded");
+}
 
+try {
     jsx.evalFile('./ps.jsx', jsx_callback);
 } catch (e) {
     error("[INTERNAL ERROR] Failed to load JSX resource "+e+" Details: "+e.stack);
 }
+
+// Load custom extension JSX if exists
+try {
+    jsx.evalFile('./ps-include.jsx', jsx_callback);
+} catch (e) {
+    console.log("[WARNING] Failed to load JSX include resource "+e+" Details: "+e.stack);
+}
+
 
 function initializeIntegration() {
     /* Initialise the Photoshop JS integration part. */
@@ -80,11 +88,11 @@ function initializeSession(env, appVersion) {
         event_manager = new EventManager(session);
 
         //  Subscribe to events
-        event_manager.subscribe.discover_remote_integration(
-            handleIntegrationDiscoverCallback
-        );
         event_manager.subscribe.integration_context_data(
             handleIntegrationContextDataCallback
+        );
+        event_manager.subscribe.remote_integration_rpc(
+            handleRemoteIntegrationRPCCallback
         );
 
         // Settle down - wait for standalone process compile to start listening. Then send
@@ -124,14 +132,6 @@ function handleIntegrationContextDataCallback(event) {
     try {
         if (event.data.remote_integration_session_id != remote_integration_session_id)
             return;
-        if (!connected) {
-            connected = true;
-            showElement("connecting", false);
-            showElement("content", true);
-            alert("ftrack Photoshop Integration successfully initialized\n\nIMPORTANT NOTE: "+
-            "This is an beta and not intended for production use. Please submit bug reports "+
-            "and feedback to support@ftrack.com.");
-        }
         // Build launchers
         if (event.data.panel_launchers !== undefined) {
             panel_launchers = event.data.panel_launchers;
@@ -143,7 +143,7 @@ function handleIntegrationContextDataCallback(event) {
                 let launcher = panel_launchers[idx];
                 let row = launcher_table.insertRow();
                 let cell = row.insertCell();
-                cell.innerHTML = '<button id="launcher_button" onclick="launchTool(\''+launcher.name+'\')"><img id="launcher_image" src="./image/'+launcher.image+'.png" border="0" />&nbsp;'+launcher.label+'</button>';
+                cell.innerHTML = '<button id="launcher_button" onclick="launchTool(\''+launcher.name+'\')"><img id="launcher_image" src="./image/'+launcher.icon+'.png" border="0" />&nbsp;'+launcher.label+'</button>';
                 idx++;
             }
         }
@@ -153,6 +153,18 @@ function handleIntegrationContextDataCallback(event) {
             document.getElementById("context_name").innerHTML = event.data.context_name;
             document.getElementById("context_path").innerHTML = event.data.context_path;
         }
+        if (!connected) {
+            connected = true;
+            showElement("connecting", false);
+            showElement("content", true);
+        } else {
+            // Reply to event to confirm we received the data
+            event_manager.publish_reply(event, prepareEventData(
+                {
+                    "result": true
+                }
+            ));
+        }
     } catch (e) {
         error("[INTERNAL ERROR] Failed setting up panel! "+e+" Details: "+e.stack);
     }
@@ -160,12 +172,103 @@ function handleIntegrationContextDataCallback(event) {
     // TODO: Display received context info
 }
 
-function handleIntegrationDiscoverCallback(event) {
-    /* Tell integration we are still here by sending reply back */
-    if (event.data.remote_integration_session_id !== remote_integration_session_id)
-        return;
-    event_manager.publish_reply(event, prepareEventData({}));
+// Tool launch
+
+function launchTool(tool_name) {
+    // Find dialog name
+    let idx = 0;
+    var dialog_name = undefined;
+    while (idx < panel_launchers.length) {
+        let launcher = panel_launchers[idx];
+        if (launcher.name == tool_name) {
+            dialog_name = launcher.dialog_name;
+            break;
+        }
+        idx++;
+    }
+    event_manager.publish.remote_integration_run_dialog(
+        prepareEventData({
+            "dialog_name": dialog_name
+        })
+    );
 }
+
+// RPC - extendscript API calls
+
+// Whitelisted functions and their mappings, add entrypoints from ps.jsx here
+var RPC_FUNCTION_MAPPING = {
+    hasDocument:"hasDocument",
+    documentSaved:"documentSaved",
+    getDocumentPath:"getDocumentPath",
+    getDocumentData:"getDocumentData",
+    saveDocument:"saveDocument",
+    exportDocument:"exportDocument",
+    openDocument:"openDocument",
+};
+
+function handleRemoteIntegrationRPCCallback(event) {
+    /* Handle RPC calls from standalone process - run function with arguments
+     supplied in event and return the result.*/
+    try {
+        if (event.data.remote_integration_session_id !== remote_integration_session_id)
+            return;
+        let function_name_raw = event.data.function_name;
+        let function_name = RPC_FUNCTION_MAPPING[function_name_raw];
+
+        if (function_name === undefined || function_name.length === 0) {
+            event_manager.publish_reply(event, prepareEventData(
+                {
+                    "error_message": "Unknown RCP function '"+function_name+"'"
+                }
+            ));
+            return;
+        }
+        // Build args, quote strings
+        let s_args = '';
+        let idx = 0;
+        while (idx < event.data.args.length) {
+            let value = event.data.args[idx];
+            if (typeof value === 'string')
+                value = '"' + value + '"';
+            s_args += (s_args.length>0?",":"") + value;
+            idx++;
+        }
+
+        csInterface.evalScript(function_name+'('+s_args+')', function (result) {
+            try {
+                // String is the evalScript type, decode
+                if (result.startsWith("{"))
+                    result = JSON.parse(result);
+                else if (result === "true")
+                    result = true;
+                else if (result === "false")
+                    result = false;
+                event_manager.publish_reply(event, prepareEventData(
+                    {
+                        "result": result
+                    }
+                ));
+            } catch (e) {
+                error_message = "[INTERNAL ERROR] Failed to convert RPC call result! "+e+" Details: "+e.stack;
+                event_manager.publish_reply(event, prepareEventData(
+                    {
+                        "error_message": error_message
+                    }
+                ));
+                error(error_message);
+            }
+        });
+    } catch (e) {
+        error_message = "[INTERNAL ERROR] Failed to run RPC call! "+e+" Details: "+e.stack;
+        event_manager.publish_reply(event, prepareEventData(
+            {
+                "error_message": error_message
+            }
+        ));
+        error(error_message);
+    }
+ }
+
 
 function openContext() {
     let task_url = event_manager.session.serverUrl+"/#slideEntityId="+context_id+"&slideEntityType=task&view=tasks&itemId=projects&entityId="+project_id+"&entityType=show";
---
+++ projects/framework-photoshop/extensions/widgets/image_options.py
@@ -0,0 +1,89 @@
+# :coding: utf-8
+# :copyright: Copyright (c) 2014-2023 ftrack
+
+from functools import partial
+
+from Qt import QtWidgets, QtCore, QtGui
+
+from ftrack_framework_qt.widgets import BaseWidget
+
+
+class ImageExportOptionsWidget(BaseWidget):
+    '''Main class to represent image export options widget on a publish process.'''
+
+    name = 'image_options'
+    ui_type = 'qt'
+
+    on_format_changed = QtCore.Signal(int)
+
+    def __init__(
+        self,
+        event_manager,
+        client_id,
+        context_id,
+        plugin_config,
+        group_config,
+        on_set_plugin_option,
+        on_run_ui_hook,
+        parent=None,
+    ):
+        '''initialise FileExportOptionsWidget with *parent*, *session*, *data*,
+        *name*, *description*, *options* and *context*
+        '''
+
+        self._format_selector = None
+
+        super(ImageExportOptionsWidget, self).__init__(
+            event_manager,
+            client_id,
+            context_id,
+            plugin_config,
+            group_config,
+            on_set_plugin_option,
+            on_run_ui_hook,
+            parent,
+        )
+
+    def pre_build_ui(self):
+        layout = QtWidgets.QVBoxLayout()
+        layout.setContentsMargins(0, 0, 0, 0)
+        layout.setAlignment(QtCore.Qt.AlignTop)
+        self.setLayout(layout)
+        self.setSizePolicy(
+            QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Fixed
+        )
+
+    def build_ui(self):
+        '''Build file extension/format selector'''
+
+        image_format_layout = QtWidgets.QHBoxLayout()
+
+        image_format_layout.addWidget(QtWidgets.QLabel('Image format:'))
+
+        self._extensions = self.plugin_config.get('options').get(
+            'extensions'
+        ) or [".jpg"]
+
+        self._format_selector = QtWidgets.QComboBox()
+        self._format_selector.addItems(self._extensions)
+        image_format_layout.addWidget(self._format_selector, 100)
+
+        self.layout().addLayout(image_format_layout)
+
+    def post_build_ui(self):
+        '''hook events'''
+        self._format_selector.currentIndexChanged.connect(
+            self._on_format_changed
+        )
+
+        # Pre-select extension
+        extension = self.plugin_config.get('options').get('extension')
+        if extension:
+            self._format_selector.setCurrentIndex(
+                self._extensions.index(extension)
+            )
+
+    def _on_format_changed(self, currentIndex):
+        '''Callback when format is changed'''
+        extension = self._extensions[currentIndex]
+        self.set_plugin_option('extension', extension)
