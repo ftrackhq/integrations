dcc830b99eb9d44582093b418c3249c6005e6609
Lluis Ftrack <lluis.casals@ftrack.com>
2023-12-18 11:58:12 +0100

    clean plugin engine and widget dependnecy references

--- tests/framework/manual/standalone_ui_test.py
+++ tests/framework/manual/standalone_ui_test.py
@@ -24,10 +24,7 @@ ROOT_INTEGRATIONS_FOLDER = os.path.dirname(
 
 INCLUDE_PACKAGES = [
     'libs/framework-core',
-    'libs/framework-engine',
-    'libs/framework-plugin',
     # QT
-    'libs/framework-widget',
     'libs/framework-qt' 'libs/qt',
     'libs/qt-style',
     'libs/utils',
