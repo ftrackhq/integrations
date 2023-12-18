3472d5633e86ccf506d61dbceadf71a9cfeabc57
Lluis Ftrack <lluis.casals@ftrack.com>
2023-12-18 13:35:31 +0100

    fix no group bug

--- libs/qt/source/ftrack_qt/utils/widget/__init__.py
+++ libs/qt/source/ftrack_qt/utils/widget/__init__.py
@@ -101,7 +101,7 @@ def build_progress_data(tool_config):
             'label': plugin_config['plugin'].replace('_', ' ').title(),
         }
         tags = plugin_config.get('tags') or []
-        for group in plugin_config.get('parents', []):
+        for group in plugin_config.get('parents') or []:
             if 'options' in group:
                 tags.extend(list(group['options'].values()))
             if 'tags' in group:
