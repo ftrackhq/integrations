07a3db9a8861462cf79e990af7eb5af5f777beb4
henriknorin-ftrack <henrik.norin@backlight.co>
2024-01-11 09:17:19 +0100

    WIP

--- projects/framework-nuke/source/ftrack_framework_nuke/__init__.py
+++ projects/framework-nuke/source/ftrack_framework_nuke/__init__.py
@@ -1,5 +1,83 @@
 # :coding: utf-8
 # :copyright: Copyright (c) 2014-2023 ftrack
 
+import logging
+import time
+import sys
+import os
+import traceback
 
-# TODO: Add support for Nuke
+from Qt import QtWidgets, QtCore
+
+import ftrack_api
+
+from ftrack_constants import framework as constants
+from ftrack_utils.extensions.environment import (
+    get_extensions_path_from_environment,
+)
+from ftrack_framework_core.host import Host
+from ftrack_framework_core.event import EventManager
+from ftrack_framework_core.client import Client
+from ftrack_framework_core import registry
+
+from ftrack_framework_core.configure_logging import configure_logging
+
+from ftrack_qt.utils.decorators import invoke_in_qt_main_thread
+
+# Evaluate version and log package version
+try:
+    from ftrack_utils.version import get_version
+
+    __version__ = get_version(
+        os.path.basename(os.path.dirname(__file__)),
+        os.path.dirname(os.path.dirname(os.path.dirname(__file__))),
+    )
+except Exception:
+    __version__ = '0.0.0'
+
+configure_logging(
+    'ftrack_framework_photoshop',
+    extra_modules=['ftrack_qt'],
+    propagate=False,
+)
+
+logger = logging.getLogger(__name__)
+logger.debug('v{}'.format(__version__))
+
+
+def bootstrap_integration(framework_extensions_path):
+    logger.debug(
+        'Maya integration initialising, extensions path:'
+        f' {framework_extensions_path}'
+    )
+
+    session = ftrack_api.Session(auto_connect_event_hub=False)
+
+    event_manager = EventManager(
+        session=session, mode=constants.event.LOCAL_EVENT_MODE
+    )
+
+    registry_instance = registry.Registry()
+    registry_instance.scan_extensions(paths=framework_extensions_path)
+
+    Host(event_manager, registry=registry_instance)
+
+    client = Client(event_manager, registry=registry_instance)
+
+    # Init tools
+    dcc_config = registry_instance.get_one(
+        name='framework-nuke', extension_type='dcc_config'
+    )['extension']
+
+    logger.debug(f'Read DCC config: {dcc_config}')
+
+    # Create menus
+
+
+# Find and read DCC config
+try:
+    bootstrap_integration(get_extensions_path_from_environment())
+except:
+    # Make sure any exception that happens are logged as there is most likely no console
+    logger.error(traceback.format_exc())
+    raise
