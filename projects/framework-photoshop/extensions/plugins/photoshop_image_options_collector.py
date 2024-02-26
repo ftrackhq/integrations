0a0ce7d7f793308a18a477b13128f741c4360f6c
Henrik Norin <112491360+henriknorin-ftrack@users.noreply.github.com>
2024-02-26 10:49:23 +0100

    Update projects/framework-photoshop/extensions/plugins/photoshop_image_options_collector.py
    Co-authored-by: Lluis Casals Marsol <112543804+lluisFtrack@users.noreply.github.com>

--- projects/framework-photoshop/extensions/plugins/photoshop_image_options_collector.py
+++ projects/framework-photoshop/extensions/plugins/photoshop_image_options_collector.py
@@ -9,7 +9,7 @@ from ftrack_framework_core.exceptions.plugin import PluginExecutionError
 from ftrack_framework_photoshop.rpc_cep import PhotoshopRPCCEP
 
 
-class PhotoshopDocumentCollectorPlugin(BasePlugin):
+class PhotoshopImageOptionsCollectorPlugin(BasePlugin):
     name = 'photoshop_image_options_collector'
 
     def run(self, store):
