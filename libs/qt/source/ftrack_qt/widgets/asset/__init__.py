a9443028cccddf8b0f5573332779b56c97a68118
Lluis Ftrack <lluis.casals@ftrack.com>
2024-03-14 12:14:29 +0100

    filenames aligned

---
+++ libs/qt/source/ftrack_qt/widgets/asset/__init__.py
@@ -0,0 +1,7 @@
+from ftrack_qt.widgets.asset.asset_version_creation_widget import (
+    AssetVersionCreation,
+)
+from ftrack_qt.widgets.asset.asset_version_selection_widget import (
+    AssetVersionSelection,
+)
+from ftrack_qt.widgets.asset.new_asset_input_widget import NewAssetInput
