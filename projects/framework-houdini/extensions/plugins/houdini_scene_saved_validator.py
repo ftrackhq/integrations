46e6c11665eab2b984bb31ecea8dd542a006ff20
seebergs <mattias.seebergs@ftrack.com>
2024-06-04 13:57:21 +0200

    feat: houdini integration (#505)
    Co-authored-by: lorenzo angeli <lorenzo.angeli@backlight.com>
    Co-authored-by: henriknorin-ftrack <henrik.norin@backlight.co>
    Co-authored-by: Henrik Norin <112491360+henriknorin-ftrack@users.noreply.github.com>

---
+++ projects/framework-houdini/extensions/plugins/houdini_scene_saved_validator.py
@@ -0,0 +1,80 @@
+# :coding: utf-8
+# :copyright: Copyright (c) 2024 ftrack
+
+
+from ftrack_utils.paths import get_temp_path
+
+from ftrack_framework_core.plugin import BasePlugin
+from ftrack_framework_core.exceptions.plugin import (
+    PluginExecutionError,
+    PluginValidationError,
+)
+
+import hou
+
+
+class HoudiniSceneSavedValidatorPlugin(BasePlugin):
+    '''
+    Plugin to validate if the scene has been saved.
+    '''
+
+    name = 'houdini_scene_saved_validator'
+
+    def save_to_temp(self, store, extension_format):
+        '''
+        Save the file to a temporary location.
+        '''
+        try:
+            # Save file to a temp file
+            save_path = get_temp_path(filename_extension=extension_format)
+            hou.hipFile.save(save_path, False)
+            self.logger.debug(f"Scene has been saved to: {save_path}.")
+        except Exception as error:
+            raise PluginExecutionError(message=error)
+
+        component_name = self.options.get('component', 'main')
+        store['components'][component_name]['scene_path'] = save_path
+        store['components'][component_name]['scene_saved'] = True
+        store['components'][component_name]['valid_file'] = True
+
+    def save_scene(self, store):
+        '''
+        Save the current scene.
+        '''
+        try:
+            hou.hipFile.save(None, False)
+            self.logger.debug(f"Scene has been saved.")
+        except Exception as error:
+            raise PluginExecutionError(
+                message=f"Error saving the scene: {error}"
+            )
+
+        component_name = self.options.get('component', 'main')
+        store['components'][component_name]['scene_saved'] = True
+        store['components'][component_name]['valid_file'] = True
+
+    def run(self, store):
+        '''
+        Run the validation process for the scene.
+        '''
+        component_name = self.options.get('component', 'main')
+
+        scene_path = store['components'][component_name].get('scene_path')
+        scene_saved = store['components'][component_name].get('scene_saved')
+
+        if not scene_path:
+            # Scene is not saved, save it first.
+            self.logger.warning('Houdini scene has never been saved.')
+            raise PluginValidationError(
+                message='Houdini scene has never been saved, Click fix to save it to a temp file',
+                on_fix_callback=self.save_to_temp,
+                fix_kwargs={'extension_format': 'hip'},
+            )
+        if not scene_saved:
+            self.logger.warning('Houdini scene not saved')
+            raise PluginValidationError(
+                message='Houdini scene not saved, Click fix to save it.',
+                on_fix_callback=self.save_scene,
+            )
+        self.logger.debug("Scene is saved validation passed.")
+        store['components'][component_name]['valid_file'] = True
