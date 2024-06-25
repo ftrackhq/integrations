de7b4dc1c73f402d4431bac679d375887f8f4757
henriknorin-ftrack <henrik.norin@backlight.co>
2024-06-25 11:13:31 +0200

    For testing

--- projects/framework-houdini/extensions/plugins/houdini_scene_loader.py
+++ projects/framework-houdini/extensions/plugins/houdini_scene_loader.py
@@ -11,16 +11,21 @@ class HoudiniSceneLoaderPlugin(BasePlugin):
 
     def run(self, store):
         '''
-        Set the selected camera name to the *store*
+        Load a Houdini scene from component path provided in *store*. Saves the
+        loaded node name to the store.
         '''
 
         component_path = store.get('component_path')
         if not component_path:
             raise PluginExecutionError(f'No component path provided in store!')
 
+        component = self.session.query(
+            f"Component where id={store['entity_id']}"
+        ).first()
+
         try:
             node = hou.node('/obj').createNode(
-                'subnet', context_data['asset_name']
+                'subnet', component['version']['asset']['name']
             )
             node.loadItemsFromFile(component_path.replace('\\', '/'))
             node.setSelected(True)
@@ -32,3 +37,4 @@ class HoudiniSceneLoaderPlugin(BasePlugin):
         self.logger.debug(
             f"Component {component_path} has been loaded to scene."
         )
+        store['loaded_node'] = node.path()
--- projects/framework-houdini/release_notes.md
+++ projects/framework-houdini/release_notes.md
@@ -2,5 +2,6 @@
 
 ## upcoming
 
+* [new] Studio asset load capability, covering reference and import .ma and .mb scenes.
 * [new] Run setup scene frame range plugin on startup.
 * [new] Initial release supporting publish and open of Houdini scenes with thumbnail reviewable.
