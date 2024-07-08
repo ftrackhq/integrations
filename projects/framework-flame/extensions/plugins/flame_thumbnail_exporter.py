f71240911d3f5a9f4da4ef19d0bb6cd9368e6b06
lorenzo angeli <lorenzo.angeli@backlight.com>
2024-07-08 15:16:47 +0200

    start looking into thumbnail exporter make snapshot publisher optional

--- projects/framework-flame/extensions/plugins/flame_thumbnail_exporter.py
+++ projects/framework-flame/extensions/plugins/flame_thumbnail_exporter.py
@@ -1,12 +1,14 @@
 # :coding: utf-8
 # :copyright: Copyright (c) 2014-2023 ftrack
 
-
+import datetime
 from ftrack_utils.paths import get_temp_path
 
 from ftrack_framework_core.plugin import BasePlugin
 from ftrack_framework_core.exceptions.plugin import PluginExecutionError
+from ftrack_framework_flame import presets
 
+import flame
 
 class FlameThumbnailExporterPlugin(BasePlugin):
     name = 'flame_thumbnail_exporter'
@@ -26,4 +28,49 @@ class FlameThumbnailExporterPlugin(BasePlugin):
         except Exception as error:
             raise PluginExecutionError(message=error)
 
-        store['components'][component_name]['exported_path'] = thumbnail_path
+        current_selection = flame.media_panel.selected_entries[0]
+        if not isinstance(current_selection, (flame.PySequence, flame.PyClip)):
+            return
+
+        thumbnail_preset_path = presets.get_preset_path('JPG8')
+        exported_path = get_temp_path(filename_extension='jpg')
+
+        exporter = flame.PyExporter()
+
+        # Set the exporter to use foreground export. By default it will send a
+        # job to Backburner to do the export.
+        #
+        exporter.foreground = True
+
+        # Do the actual export.
+        #
+        exporter.export_between_marks = True
+
+        # Duplicate the clip to avoid modifying the clip itself.
+        #
+        duplicate_clip = flame.duplicate(current_selection)
+        try:
+            # Give the duplicate clip an unique name. We assume here that the preset
+            # we will use have <name> in the destination path.
+            #
+            duplicate_clip.name = current_selection.name + datetime.datetime.now().strftime(
+                "%Y_%m_%d__%H_%M_%S"
+            )
+
+            # Set in mark to current_time and out_mark at current_time + 1 so
+            # only the current frame is exported
+            #
+            duplicate_clip.in_mark = current_selection.current_time.get_value()
+            duplicate_clip.out_mark = current_selection.current_time.get_value() + 1
+
+            exporter.export(
+                current_selection, thumbnail_preset_path, exported_path
+            )
+
+        finally:
+            # Be sure to clean up duplicated clip in case of error during the export
+            #
+            flame.delete(duplicate_clip)
+
+        store['components'][component_name]['exported_path'] = exported_path
+
--- projects/framework-flame/source/ftrack_framework_flame/presets.py
+++ projects/framework-flame/source/ftrack_framework_flame/presets.py
@@ -1,16 +1,34 @@
+import os
 import flame
 
-PRESETS = [
-    {
-        "visibility": flame.PyExporter.PresetVisibility.Autodesk,
-        "type": flame.PyExporter.PresetType.Movie,
-        "dir": "QuickTime",
-        "preset": "QuickTime (8-bit Uncompressed).xml",
-    },
-    {
+_PRESETS = {
+    'JPG8': {
         "visibility": flame.PyExporter.PresetVisibility.Autodesk,
         "type": flame.PyExporter.PresetType.Image_Sequence,
         "dir": "Jpeg",
         "preset": "Jpeg (8-bit).xml",
     },
-]
+    'MOV8': {
+        "visibility": flame.PyExporter.PresetVisibility.Autodesk,
+        "type": flame.PyExporter.PresetType.Movie,
+        "dir": "QuickTime",
+        "preset": "QuickTime (8-bit Uncompressed).xml",
+    }
+}
+
+
+
+def get_preset_path(preset_name):
+    preset = _PRESETS.get(preset_name)
+    if not preset:
+        return
+
+    preset_dir = flame.PyExporter.get_presets_dir(
+        preset["visib"], preset["type"]
+    )
+
+    preset_path = os.path.join(
+        preset_dir, preset["dir"], preset["preset"]
+    )
+
+    return preset_path
