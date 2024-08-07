f5098fa1096419206bf7b2102abbf105352e2450
lorenzo angeli <lorenzo.angeli@backlight.com>
2024-08-07 11:24:16 +0200

    Update wiretap module

--- projects/framework-flame/source/ftrack_framework_flame/wiretap.py
+++ projects/framework-flame/source/ftrack_framework_flame/wiretap.py
@@ -64,12 +64,14 @@ class WiretapApi(object):
     def flame_version(self):
         return flame.get_version()
 
-    def create_project(self, project_name):
+    def create_project(self, project_name, workspace_name='ftrack'):
 
-        xml_settings = self._xml_settings(ftrack_project=project_name)
+        xml_settings = self._xml_settings(project_name=project_name)
 
         if not self._child_node_exists("/projects", project_name, WiretapNodeType.Project):
-            logger.info(f"Project '{project_name}' does not exists. Create it with settings: \n{pprint.pformat(settings)}")
+            logger.info(
+                f"Project '{project_name}' does not exists. Create it with settings: \n{pprint.pformat(xml_settings)}"
+            )
 
             volumes = self.get_volumes()
             logger.debug(f"Framestore found: {volumes}")
@@ -89,20 +91,12 @@ class WiretapApi(object):
                 if not project_node.setMetaData("XML", xml_settings):
                     raise WiretapException(f"Error setting metadata for {project_name}: {project_node.lastError()}")
 
-                workspace_node = self._create_node(project_node, WiretapNodeType.Workspace)
+                workspace_node = self._create_node(project_node, WiretapNodeType.Workspace, workspace_name)
                 self._create_node(workspace_node, WiretapNodeType.Desktop)
 
                 return project_node
 
     def create_user(self, user_name):
-        """
-        Create a Flame Family user.
-
-        :param user_name: The username to create
-        :type user_name: string
-        :return: user node
-        :rtype: WireTapNodeHandle
-        """
         users = WireTapNodeHandle(self._server, "/users")
         user_node = self._create_node(users, WiretapNodeType.User, user_name)
 
@@ -133,8 +127,7 @@ class WiretapApi(object):
     def get_projects(self):
         projects_node = self._get_node_from_path('/projects')
         children = self._get_children(projects_node)
-
-        return children.keys()
+        return list(children.keys())
 
     def get_project(self, project_name):
         return self._get_node_from_path(f"/projects/{project_name}")
@@ -142,8 +135,7 @@ class WiretapApi(object):
     def get_users(self):
         users_node = self._get_node_from_path('/users')
         children = self._get_children(users_node)
-
-        return children.keys()
+        return list(children.keys())
 
     def get_user(self, user_name):
         return self._get_node_from_path(f"/users/{user_name}")
@@ -156,8 +148,7 @@ class WiretapApi(object):
     def get_volumes(self):
         volumes_node = self._get_node_from_path('/volumes')
         children = self._get_children(volumes_node)
-
-        return children.keys()
+        return list(children.keys())
 
     def _create_node(self, parent, node_type, node_name=None):
         node_name = node_type.title() if not node_name else node_name
@@ -216,14 +207,14 @@ class WiretapApi(object):
 
         if not parent_node.getDisplayName(parent_node_name):
             raise WiretapException(f"Couldn't get node name:{ parent_node.lastError()}")
-        # logger.debug("parent node name: %s" % parent_node_name)
+        logger.debug("parent node name: %s" % parent_node_name)
 
         num_children = WireTapInt(0)
         if not parent_node.getNumChildren(num_children):
             raise WiretapException(
                 f"Couldn't get children number for the node {parent_node_name.c_str()}: {parent_node.lastError()}"
             )
-        # logger.debug("parent num children: %s" % int(num_children))
+        logger.debug("parent num children: %s" % int(num_children))
 
         result_node = None
         child_node = WireTapNodeHandle()
@@ -235,27 +226,27 @@ class WiretapApi(object):
             # get child node
             if not parent_node.getChild(child_idx, child_node):
                 raise WiretapException(f"Unable to get child: {parent_node.lastError()}")
-            # logger.debug("child: %s" % child_node)
+            logger.debug("child: %s" % child_node)
 
             # get child node name
             if not child_node.getDisplayName(child_node_name):
                 raise WiretapException(f"Couldn't get node name: {child_node.lastError()}")
-            # logger.debug("child node name: %s" % child_node_name.c_str())
+            logger.debug("child node name: %s" % child_node_name.c_str())
 
             # get child node type
             if not child_node.getNodeTypeStr(child_node_type):
                 raise WiretapException(f"Couldn't get node type: {child_node.lastError()}")
-            # logger.debug("child node type: %s" % child_node_type.c_str())
+            logger.debug("child node type: %s" % child_node_type.c_str())
 
             # check if child match criteria
             if child_node_name.c_str() == node_name and child_node_type.c_str() == node_type:
                 result_node = child_node
-                # logger.debug("node found !")
+                logger.debug("node found !")
                 break
 
             result_node = self._get_node(child_node, node_name, node_type)
             if result_node:
-                # logger.debug("node found !")
+                logger.debug("node found !")
                 return result_node
 
         return result_node
@@ -295,23 +286,22 @@ class WiretapApi(object):
 
         return False
 
-
-    def _xml_settings(self, ftrack_project, partition, width=1920, height=1080,depth='10-bit'):
+    def _xml_settings(self, project_name, volume, fps=25, width=1920, height=1080,depth='10-bit'):
         # https://help.autodesk.com/view/FLAME/2024/ENU/?guid=Flame_API_Wiretap_SDK_Media_and_Metadata_Formats_Project_Node_Metadata_XML_html
 
         ratio = str(float(width)/float(height))
-        fps = str(ftrack_project['custom_attributes']['fps'])
         project_xml_data = f"""
         <Project>
-        <Name>{ftrack_project['name']}</Name>
-        <Description>"Created with ftrack integration for flame: {self.flame_version}"</Description>
+        <Name>{project_name}</Name>
+        <Description>"Created with ftrack integration for flame: "</Description>
+        <Partition>{volume}</Partition>
+        <Version>{self.flame_version}</Version>
         <FrameWidth>{width}<FrameWidth>
         <FrameHeight>{height}<FrameHeight>
         <FrameDepth>{depth}</FrameDepth>
-        <AspectRatio>{ratio}</AspectRatio>
-        <Partition>{partition}</Partition>
+        <AspectRatio>{ratio:.5f}</AspectRatio>
         <FieldDominance>PROGRESSIVE</FieldDominance>
-        <FrameRate>{fps}</FrameRate>
+        <FrameRate>{fps} fps</FrameRate>
         <DefaultStartFrame>1001</DefaultStartFrame>
         <VisualDepth>unknown</VisualDepth>
         </Project>
