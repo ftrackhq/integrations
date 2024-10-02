adc0479632d75795df8020de4460db094d5e2127
lorenzo angeli <lorenzo.angeli@backlight.co>
2024-10-02 14:51:12 +0200

    init blender branch

---
+++ projects/framework-blender/source/framework-blender/utils/__init__.py
@@ -0,0 +1,23 @@
+# :coding: utf-8
+# :copyright: Copyright (c) 2024 ftrack
+import threading
+from functools import wraps
+
+# Dock widget in Blender
+def dock_blender_right(widget):
+    '''Dock *widget* to the right side of Blender.'''
+    pass
+
+
+def run_in_main_thread(f):
+    '''Make sure a function runs in the main Blender thread.'''
+
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        if threading.currentThread().name != 'MainThread':
+            pass
+            # return blender_utils.executeInMainThreadWithResult(f, *args, **kwargs)
+        else:
+            return f(*args, **kwargs)
+
+    return decorated
