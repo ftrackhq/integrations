b1b72d4771e9915157f61d1eeda0c6f718270255
Dennis Weil <dennis.weil@backlight.co>
2024-11-05 14:10:46 +0000

    Properly adding the export_type to the store

--- projects/framework-max/extensions/plugins/max_scene_collector.py
+++ projects/framework-max/extensions/plugins/max_scene_collector.py
@@ -18,14 +18,23 @@ class MaxSceneCollectorPlugin(BasePlugin):
         extension format to be published to the *store*. Also collect the
         scene_name and collect if scene is saved.
         '''
+        export_type = None
+        try:
+            export_type = self.options['export_type']
+        except Exception as error:
+            raise PluginExecutionError(
+                f"Provide export_type and extension_format: {error}"
+            )
+
         scene_name = None
-        scene_saved = False
         try:
             scene_name = os.path.join(rt.maxFilePath, rt.maxFileName)
         except Exception as error:
             raise PluginExecutionError(
                 f"Error retrieving the scene name: {error}"
             )
+
+        scene_saved = False
         try:
             scene_saved = not rt.getSaveRequired()
         except Exception as error:
@@ -33,9 +42,12 @@ class MaxSceneCollectorPlugin(BasePlugin):
                 f"Error Checking if the scene is saved: {error}"
             )
 
+        self.logger.debug(f"Export type set to {export_type}.")
         self.logger.debug(f"Current scene name is: {scene_name}.")
+        self.logger.debug(f"Is current scene saved?: {scene_saved}.")
 
         component_name = self.options.get('component', 'main')
 
+        store['components'][component_name]['export_type'] = export_type
         store['components'][component_name]['scene_name'] = scene_name
         store['components'][component_name]['scene_saved'] = scene_saved
