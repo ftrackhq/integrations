72651493e89270143172a36d512efbc08ca41d8a
Dennis Weil <35080133+dennisweil@users.noreply.github.com>
2024-11-26 10:43:55 +0100

    fix: selection export and playblast creation (#601)

--- projects/framework-blender/extensions/plugins/blender_playblast_exporter.py
+++ projects/framework-blender/extensions/plugins/blender_playblast_exporter.py
@@ -6,6 +6,7 @@ import bpy
 from ftrack_utils.paths import get_temp_path
 
 from ftrack_framework_core.plugin import BasePlugin
+from ftrack_framework_core.exceptions.plugin import PluginExecutionError
 
 
 class BlenderPlayblastExporterPlugin(BasePlugin):
@@ -20,24 +21,46 @@ class BlenderPlayblastExporterPlugin(BasePlugin):
 
         # Set the output file path
         save_path = get_temp_path(filename_extension='.mp4')
+        camera_name = store['components'][component_name]['camera_name']
 
-        scene = bpy.context.scene
-        rd = scene.render
+        bpy.ops.ed.undo_push(message="ftrack: Export Playblast")
+        try:
+            rd = bpy.context.scene.render
 
-        # Set the viewport resolution
-        rd.resolution_x = 1920
-        rd.resolution_y = 1080
+            camera = [
+                obj for obj in bpy.data.objects
+                  if obj.type == 'CAMERA'
+                  and obj.name == camera_name
+            ][0]
 
-        # Set the output format
-        rd.image_settings.file_format = "FFMPEG"
-        # Set output format
-        rd.ffmpeg.format = "MPEG4"
-        # Set the codec
-        rd.ffmpeg.codec = "H264"
+            bpy.context.scene.camera = camera
 
-        # Render the viewport and save the result
-        bpy.context.scene.render.filepath = save_path
-        bpy.ops.render.render(animation=True, use_viewport=True, write_still=True)
-        self.logger.debug(f'Rendering playblast to {save_path}')
+            # Set the viewport resolution
+            rd.resolution_x = 1920
+            rd.resolution_y = 1080
 
-        store['components'][component_name]['exported_path'] = save_path
+            # Set the output format
+            rd.image_settings.file_format = "FFMPEG"
+            # Set output format
+            rd.ffmpeg.format = "MPEG4"
+            # Set the codec
+            rd.ffmpeg.codec = "H264"
+
+            # Render the viewport and save the result
+            bpy.context.scene.render.filepath = save_path
+            self.logger.debug(f'Rendering playblast to {save_path}')
+
+            bpy.ops.render.render(
+                animation=True,
+                use_viewport=False,
+                write_still=True
+            )
+
+            store['components'][component_name]['exported_path'] = save_path
+
+        except Exception as error:
+            raise PluginExecutionError(
+                message=f"Couldn't export playblast to {save_path} due to error:{error}"
+            )
+        finally:
+            bpy.ops.ed.undo()
