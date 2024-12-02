28d2b766bba2c3481a73609897b6ce2e66408a31
Lluis Ftrack <lluis.casals@ftrack.com>
2024-12-02 14:02:13 +0100

    app-launcher base lib

--- app/connect/ftrack/app/connect/__init__.py
+++ app/connect/ftrack/app/connect/__init__.py
@@ -1,17 +1,17 @@
 # :coding: utf-8
 # :copyright: Copyright (c) 2024 ftrack
 
-import click
-from cli import cli
+# Import the CLI tool from the ftrack.app.connect.cli module
+from .cli import cli
 
 # Check if virtual environment is activated
 import os
-import sys
 
-if not os.getenv('VIRTUAL_ENV'):
+if not os.getenv("VIRTUAL_ENV"):
     print(
         "Warning: No virtual environment is active. Please activate your venv before proceeding."
     )
+
 
 if __name__ == "__main__":
     cli()
