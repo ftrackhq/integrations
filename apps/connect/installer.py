d8dfc489d1a1e646c262ccc10b50e76ac2288b1b
Dennis Weil <dennis.weil@backlight.co>
2025-12-01 15:23:43 +0100

    WIP poetry to uv/ruff migration
    - ftrack connect
    - installer
    - projects/rv
    - tools update to build resources

--- apps/connect/installer.py
+++ apps/connect/installer.py
@@ -5,6 +5,7 @@ import os
 import sys
 import logging
 import argparse
+import importlib.util
 
 from ftrack_utils.version import get_version
 from ftrack_app_installer import (
@@ -15,8 +16,8 @@ from ftrack_app_installer import (
 
 
 def setup_installer(bundle_name, version, root_path, entry_file):
-    '''Return the instance of the installer class depending on the current platform'''
-    if sys.platform.startswith('win'):
+    """Return the instance of the installer class depending on the current platform"""
+    if sys.platform.startswith("win"):
         return WindowsAppInstaller(
             bundle_name,
             version,
@@ -24,7 +25,7 @@ def setup_installer(bundle_name, version, root_path, entry_file):
             root_path,
             entry_file,
         )
-    elif sys.platform.startswith('darwin'):
+    elif sys.platform.startswith("darwin"):
         return MacOSAppInstaller(
             bundle_name,
             version,
@@ -32,7 +33,7 @@ def setup_installer(bundle_name, version, root_path, entry_file):
             root_path,
             entry_file,
         )
-    elif sys.platform.startswith('linux'):
+    elif sys.platform.startswith("linux"):
         return LinuxAppInstaller(
             bundle_name,
             version,
@@ -48,27 +49,38 @@ def setup_installer(bundle_name, version, root_path, entry_file):
 def main():
     arguments = sys.argv[1:]
 
-    if sys.platform == "darwin" and getattr(sys, 'frozen', False):
+    if sys.platform == "darwin" and getattr(sys, "frozen", False):
         # Filter out PSN (process serial number) argument passed by OSX.
         arguments = [
-            argument for argument in arguments if '-psn_0_' not in argument
+            argument for argument in arguments if "-psn_0_" not in argument
         ]
 
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        '--codesign',
-        help='Codesign the application (default: True).',
+        "--codesign",
+        help="Codesign the application (default: True).",
         default=True,
-        nargs='?',
-        type=lambda x: (str(x).lower() in ['true', '1', 'yes']),
+        nargs="?",
+        type=lambda x: (str(x).lower() in ["true", "1", "yes"]),
     )
 
     args = parser.parse_args(arguments)
 
     bundle_name = "ftrack Connect"
-    version = get_version("ftrack_connect", os.path.dirname(__file__))
-    root_path = os.path.dirname(os.path.abspath(__file__))
-    entry_file = os.path.join(root_path, "source/ftrack_connect/__main__.py")
+
+    # Find the ftrack_connect package location
+    spec = importlib.util.find_spec("ftrack_connect")
+    if spec is None or spec.origin is None:
+        logging.error("Could not find ftrack_connect package")
+        sys.exit(1)
+
+    package_path = os.path.dirname(spec.origin)
+    # For editable installs, package_path is typically .../source/ftrack_connect
+    # We need to go up two levels to get to the project root
+    source_path = os.path.dirname(package_path)  # .../source
+    root_path = os.path.dirname(source_path)  # project root
+    version = get_version("ftrack_connect", source_path)
+    entry_file = os.path.join(package_path, "__main__.py")
 
     installer = setup_installer(bundle_name, version, root_path, entry_file)
 
