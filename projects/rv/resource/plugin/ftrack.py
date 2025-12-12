10d8fd99ca89ec4b9278be9404102c1f55f83163
lorenzo angeli <lorenzo.angeli@backlight.co>
2025-12-12 10:14:18 +0100

    ensure pyside2/6 compatibility

--- projects/rv/resource/plugin/ftrack.py
+++ projects/rv/resource/plugin/ftrack.py
@@ -10,12 +10,21 @@ import logging
 import platform
 import base64
 
-from PySide2 import QtCore
-from PySide2 import QtGui
-from PySide2 import QtWidgets
-from PySide2 import QtWebEngineWidgets
-from PySide2.QtCore import SIGNAL, SLOT
-
+try:
+    from PySide2 import QtCore
+    from PySide2 import QtGui
+    from PySide2 import QtWidgets
+    from PySide2 import QtWebEngineWidgets
+    from PySide2.QtCore import SIGNAL, SLOT
+except:
+    try:
+        from PySide6 import QtCore
+        from PySide6 import QtGui
+        from PySide6 import QtWidgets
+        from PySide6 import QtWebEngineWidgets
+        from PySide6.QtCore import SIGNAL, SLOT
+    except ImportError:
+        pass
 
 # setup logging
 from ftrack_logging import configure_logging
