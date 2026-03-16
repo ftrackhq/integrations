700835f685960d9f1a0895818ed077e21770d004
Dennis Weil <dennis.weil@backlight.co>
2026-03-16 16:48:03 +0100

    refactor: move app version parsing into ftrack-utils

--- libs/utils/source/ftrack_utils/version/__init__.py
+++ libs/utils/source/ftrack_utils/version/__init__.py
@@ -1,48 +1,61 @@
 # :coding: utf-8
 # :copyright: Copyright (c) 2024 ftrack
 import os
+import re
+import tomllib
+
+from packaging.version import InvalidVersion, Version
+
+
+#: Default expression to match version component of executable path.
+#: Will match last set of numbers in string where numbers may contain a digit
+#: followed by zero or more digits, periods, or the letters 'a', 'b', 'c' or 'v'
+#: E.g. /path/to/x86/some/application/folder/v1.8v2b1/app.exe -> 1.8v2b1
+DEFAULT_VERSION_EXPRESSION = re.compile(r"(?P<version>\d[\d.vabc]*?)[^\d]*$")
+
+
+def parse_application_version(value):
+    """Parse application version into a comparable Version instance.
+
+    Some discovered app paths include non-standard markers such as "v"
+    inside the version string (for example: "1.8v2b1").
+    """
+    candidates = [value, value.replace("v", ".")]
+    for candidate in candidates:
+        try:
+            return Version(candidate)
+        except InvalidVersion:
+            continue
+
+    return Version("0")
 
 
 def get_version(package_name, package_path):
-    '''Return version string for *package_name* at *package_path*'''
-    result = '0.0.0'
+    """Return version string for *package_name* at *package_path*"""
+    result = "0.0.0"
     try:
         from importlib.metadata import version
 
         result = version(package_name)
-    except ImportError:
-        try:
-            # Running on pre 3.8 Python; use importlib-metadata package
-            from pkg_resources import get_distribution
-
-            result = get_distribution(package_name).version
-        except:
-            pass
-    if result == '0.0.0':
-        # Probably running from sources or not able to resolv, fetch version
-        # from pyproject.toml
-        import toml
-
-        path_toml = os.path.join(
-            package_path,
-            "pyproject.toml",
-        )
+    except Exception:
+        path_toml = os.path.join(package_path, "pyproject.toml")
         if os.path.exists(path_toml):
-            result = toml.load(path_toml)["tool"]["poetry"]["version"]
+            with open(path_toml, "rb") as stream:
+                result = tomllib.load(stream)["project"]["version"]
 
     return result
 
 
 def get_connect_plugin_version(connect_plugin_path):
-    '''Return Connect plugin version string for *connect_plugin_path*'''
+    """Return Connect plugin version string for *connect_plugin_path*"""
     result = None
-    path_version_file = os.path.join(connect_plugin_path, '__version__.py')
+    path_version_file = os.path.join(connect_plugin_path, "__version__.py")
     if not os.path.isfile(path_version_file):
         raise FileNotFoundError
     with open(path_version_file) as f:
         for line in f.readlines():
-            if line.startswith('__version__'):
-                result = line.split('=')[1].strip().strip("'")
+            if line.startswith("__version__"):
+                result = line.split("=")[1].strip().strip("'")
                 break
     if not result:
         raise Exception(
