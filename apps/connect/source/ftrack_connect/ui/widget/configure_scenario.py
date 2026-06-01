e209d7ca55c24713829d166827a9869caf85702c
lorenzo angeli <lorenzo.angeli@gmail.com>
2026-06-01 10:53:28 +0200

    add projections to optimize queries

--- apps/connect/source/ftrack_connect/ui/widget/configure_scenario.py
+++ apps/connect/source/ftrack_connect/ui/widget/configure_scenario.py
@@ -1,13 +1,12 @@
 # :coding: utf-8
 # :copyright: Copyright (c) 2014-2023 ftrack
 
-import os
 import webbrowser
 
 try:
-    from PySide6 import QtWidgets, QtCore, QtGui, QtSvg
+    from PySide6 import QtWidgets, QtCore
 except ImportError:
-    from PySide2 import QtWidgets, QtCore, QtGui, QtSvg
+    from PySide2 import QtWidgets, QtCore
 
 import qtawesome as qta
 
@@ -19,13 +18,13 @@ from ftrack_connect import load_fonts_resource
 
 
 class ConfigureScenario(QtWidgets.QWidget):
-    '''Configure scenario widget.'''
+    """Configure scenario widget."""
 
     #: Signal to emit when configuration is completed.
     configuration_completed = QtCore.Signal()
 
     def __init__(self, session):
-        '''Instantiate the configure scenario widget.'''
+        """Instantiate the configure scenario widget."""
         super(ConfigureScenario, self).__init__()
 
         load_fonts_resource()
@@ -35,8 +34,8 @@ class ConfigureScenario(QtWidgets.QWidget):
         # the API.
         try:
             session.query(
-                'Setting where name is "storage_scenario" and '
-                'group is "STORAGE"'
+                "select id from Setting "
+                'where name is "storage_scenario" and group is "STORAGE"'
             ).one()
             can_configure_scenario = True
         except ftrack_api.exception.NoResultFoundError:
@@ -49,7 +48,7 @@ class ConfigureScenario(QtWidgets.QWidget):
 
         layout.addSpacing(100)
 
-        cloud_icon = qta.icon('ftrack.cloud-done')
+        cloud_icon = qta.icon("ftrack.cloud-done")
         icon = QtWidgets.QLabel()
         icon.setPixmap(
             cloud_icon.pixmap(
@@ -58,23 +57,23 @@ class ConfigureScenario(QtWidgets.QWidget):
         )
 
         icon.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
-        icon.setObjectName('icon-label')
+        icon.setObjectName("icon-label")
         layout.addWidget(icon)
 
         layout.addSpacing(50)
 
         label = QtWidgets.QLabel()
-        label.setObjectName('regular-label')
+        label.setObjectName("regular-label")
         text = (
-            'Hi there, Connect needs to be configured so that ftrack can '
-            'store and track your files for you.'
+            "Hi there, Connect needs to be configured so that ftrack can "
+            "store and track your files for you."
         )
 
         if can_configure_scenario is False:
             text += (
-                '<br><br> You do not have the required permission, please ask '
-                'someone with access to system settings in ftrack to '
-                'configure it before you proceed.'
+                "<br><br> You do not have the required permission, please ask "
+                "someone with access to system settings in ftrack to "
+                "configure it before you proceed."
             )
 
         label.setText(text)
@@ -91,13 +90,13 @@ class ConfigureScenario(QtWidgets.QWidget):
 
         layout.addSpacing(20)
 
-        configure_button = QtWidgets.QPushButton(text='Configure now')
-        configure_button.setObjectName('primary')
+        configure_button = QtWidgets.QPushButton(text="Configure now")
+        configure_button.setObjectName("primary")
         configure_button.clicked.connect(self._configure_storage_scenario)
         configure_button.setMinimumHeight(40)
         configure_button.setMaximumWidth(125)
 
-        dismiss_button = QtWidgets.QPushButton(text='Do it later')
+        dismiss_button = QtWidgets.QPushButton(text="Do it later")
         dismiss_button.clicked.connect(self._complete_configuration)
         dismiss_button.setMinimumHeight(40)
         dismiss_button.setMaximumWidth(125)
@@ -112,11 +111,11 @@ class ConfigureScenario(QtWidgets.QWidget):
         layout.addSpacing(20)
 
         label = QtWidgets.QLabel()
-        label.setObjectName('lead-label')
+        label.setObjectName("lead-label")
         label.setText(
-            'If you decide to do this later, some of the functionality in '
-            'ftrack connect and applications started from connect may not '
-            'work as expected until configured.'
+            "If you decide to do this later, some of the functionality in "
+            "ftrack connect and applications started from connect may not "
+            "work as expected until configured."
         )
         label.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
         label.setWordWrap(True)
@@ -131,11 +130,11 @@ class ConfigureScenario(QtWidgets.QWidget):
         layout.addStretch(1)
 
         label = QtWidgets.QLabel()
-        label.setObjectName('green-link')
+        label.setObjectName("green-link")
         label.setText(
             '<a style="color: #935BA2;" '
             'href="https://help.ftrack.com/en/articles/1040436-configuring-file-storage"> '
-            'Learn more about storage scenarios.'
+            "Learn more about storage scenarios."
         )
         label.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
         label.setOpenExternalLinks(True)
@@ -143,20 +142,20 @@ class ConfigureScenario(QtWidgets.QWidget):
         layout.addSpacing(20)
 
         self._subscriber_identifier = session.event_hub.subscribe(
-            'topic=ftrack.storage-scenario.configure-done',
+            "topic=ftrack.storage-scenario.configure-done",
             self._complete_configuration,
         )
         self._session = session
 
     def _complete_configuration(self, event=None):
-        '''Finish configuration.'''
+        """Finish configuration."""
         self._session.event_hub.unsubscribe(self._subscriber_identifier)
         self.configuration_completed.emit()
 
     def _configure_storage_scenario(self):
-        '''Open browser window and go to the configuration page.'''
+        """Open browser window and go to the configuration page."""
         webbrowser.open_new_tab(
-            '{0}/#view=configure_storage_scenario&itemId=newconfigure'.format(
+            "{0}/#view=configure_storage_scenario&itemId=newconfigure".format(
                 self._session.server_url
             )
         )
